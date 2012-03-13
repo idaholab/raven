7de40db20abacba1b1ce310b8a9553bfb072ed67
Cody Permann <cody.permann@inl.gov>
2012-03-13 07:12:55 +0000

    Commiting first version of RAVEN
    r9261

---
+++ Makefile
@@ -0,0 +1,46 @@
+###############################################################################
+################### MOOSE Application Standard Makefile #######################
+###############################################################################
+#
+# Required Environment variables
+# LIBMESH_DIR	- location of the libMesh library
+#
+# Required Make variables
+# APPLICATION_NAME  - the name of this application (all lower case)
+# MOOSE_DIR	- location of the MOOSE framework
+# ELK_DIR	- location of ELK (if enabled)
+#
+# Optional Environment variables
+# CURR_DIR	- current directory (DO NOT MODIFY THIS VARIABLE)
+#
+# Note: Make sure that there is no whitespace after the word 'yes' if enabling
+# an application
+###############################################################################
+CURR_DIR    ?= $(shell pwd)
+ROOT_DIR    ?= $(shell dirname `pwd`)
+
+ifeq ($(MOOSE_DEV),true)
+	MOOSE_DIR ?= $(ROOT_DIR)/devel/moose
+else
+	MOOSE_DIR ?= $(ROOT_DIR)/moose
+endif
+LIBMESH_DIR ?= $(ROOT_DIR)/libmesh
+
+ELK_DIR     ?= $(ROOT_DIR)/elk
+R7_DIR     ?= $(ROOT_DIR)/r7_moose
+RAVEN_DIR   ?= $(ROOT_DIR)/raven
+
+APPLICATION_NAME := RAVEN
+
+DEP_APPS    ?= $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
+#ADDITIONAL_LIBS  += -lgsl
+
+include $(MOOSE_DIR)/build.mk
+# deps
+include $(MOOSE_DIR)/moose.mk
+include $(ELK_DIR)/elk.mk
+include $(R7_DIR)/r7.mk
+include $(RAVEN_DIR)/raven.mk
+
+###############################################################################
+# Additional special case targets should be added here
---
+++ raven.mk
@@ -0,0 +1,72 @@
+RAVEN_SRC_DIRS := $(RAVEN_DIR)/src/*/*
+
+RAVEN_INC_DIRS := $(shell find $(RAVEN_DIR)/include -type d -not -path "*/.svn*")
+RAVEN_INCLUDE  := $(foreach i, $(RAVEN_INC_DIRS), -I$(i))
+
+libmesh_INCLUDE := $(RAVEN_INCLUDE) $(libmesh_INCLUDE)
+
+RAVEN_LIB := $(RAVEN_DIR)/libRAVEN-$(METHOD)$(libext)
+
+RAVEN_APP := $(RAVEN_DIR)/RAVEN-$(METHOD)
+
+# source files
+RAVEN_srcfiles    := $(shell find $(RAVEN_SRC_DIRS) -name *.C)
+RAVEN_csrcfiles   := $(shell find $(RAVEN_SRC_DIRS) -name *.c)
+RAVEN_fsrcfiles   := $(shell find $(RAVEN_SRC_DIRS) -name *.f)
+RAVEN_f90srcfiles := $(shell find $(RAVEN_SRC_DIRS) -name *.f90)
+# object files
+RAVEN_objects	:= $(patsubst %.C, %.$(obj-suffix), $(RAVEN_srcfiles))
+RAVEN_objects	+= $(patsubst %.c, %.$(obj-suffix), $(RAVEN_csrcfiles))
+RAVEN_objects += $(patsubst %.f, %.$(obj-suffix), $(RAVEN_fsrcfiles))
+RAVEN_objects += $(patsubst %.f90, %.$(obj-suffix), $(RAVEN_f90srcfiles))
+
+RAVEN_app_objects := $(patsubst %.C, %.$(obj-suffix), $(RAVEN_DIR)/src/main.C)
+
+all:: $(RAVEN_LIB)
+
+# build rule for lib RAVEN
+ifeq ($(enable-shared),yes)
+# Build dynamic library
+$(RAVEN_LIB): $(RAVEN_objects)
+	@echo "Linking "$@"..."
+	@$(libmesh_CC) $(libmesh_CXXSHAREDFLAG) -o $@ $(RAVEN_objects) $(libmesh_LDFLAGS)
+else
+# Build static library
+ifeq ($(findstring darwin,$(hostos)),darwin)
+$(RAVEN_LIB): $(RAVEN_objects)
+	@echo "Linking "$@"..."
+	@libtool -static -o $@ $(RAVEN_objects)
+else
+$(RAVEN_LIB): $(RAVEN_objects)
+	@echo "Linking "$@"..."
+	@$(AR) rv $@ $(RAVEN_objects)
+endif
+endif
+
+# include RAVEN dep files
+-include $(RAVEN_DIR)/src/*/*.d
+
+
+# how to build RAVEN application
+ifeq ($(APPLICATION_NAME),RAVEN)
+all:: RAVEN
+
+RAVEN: $(RAVEN_APP)
+
+$(RAVEN_APP): $(moose_LIB) $(elk_LIB) $(r7_LIB) $(RAVEN_LIB) $(RAVEN_app_objects)
+	@echo "Linking "$@"..."
+	@$(libmesh_CXX) $(libmesh_CXXFLAGS) $(RAVEN_app_objects) -o $@ $(RAVEN_LIB) $(r7_LIB) $(elk_LIB) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS)
+
+-include $(RAVEN_DIR)/src/*.d
+endif
+
+
+clean::
+	@rm -fr $(RAVEN_APP)
+	@rm -fr $(RAVEN_LIB)
+	@find . \( -name "*~" -or -name "*.o" -or -name "*.d" -or -name "*.pyc" \
+                -or -name "*.gcda" -or -name "*.gcno" -or -name "*.gcov" \) -exec rm '{}' \;
+	@rm -fr *.mod
+
+cleanall::
+	make -C $(RAVEN_DIR) clean
