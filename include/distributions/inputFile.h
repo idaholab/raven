4d27fbafe2e51c6f79b10c46b3f8cb95047f339e
Joshua Cogliati <joshua.cogliati@inl.gov>
2012-05-16 10:56:09 +0000

    Moving function definitions out of .h file into .C file.
    r10627

--- include/distributions/inputFile.h
+++ include/distributions/inputFile.h
@@ -17,27 +17,8 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
 
-static void LoadData(double** data, int dimensionality, int cardinality, string filename) {
-  int x, y;
-
-  ifstream in(filename.c_str());
-
-  if (!in) {
-    cout << "Cannot open file.\n";
-    return;
-  }
-
-  for (y = 0; y < cardinality; y++) {
-    for (x = 0; x < dimensionality; x++) {
-      in >> data[y][x];
-    }
-  }
-
-  in.close();
-}
-
+void LoadData(double** data, int dimensionality, int cardinality, std::string filename);
 
 
 #endif /* INPUTFILE_H_ */
