d73c60109eea8aa3565614102a832471d3e1334d
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2012-07-09 09:35:01 +0000

    Syntax for Distributions
    r11834

---
+++ include/distributions/DistributionContainer.h
@@ -0,0 +1,112 @@
+/*
+ * DistributionContainer.h
+ *
+ *  Created on: Jul 6, 2012
+ *      Author: alfoa
+ */
+
+#ifndef DISTRIBUTIONCONTAINER_H_
+#define DISTRIBUTIONCONTAINER_H_
+
+#include "distribution_1D.h"
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class DistributionContainer;
+
+class DistributionContainer{
+     public:
+	 /*
+	  * Constructor(empty)
+	  */
+	 DistributionContainer();
+	 /*
+	  * Destructor
+	  */
+	 virtual ~DistributionContainer();
+     /*
+      * Function to construct on the fly this class through the action system
+      * @
+      * @
+      */
+     void constructDistributionContainer(std::string DistAlias, distribution_type type, double xmin, double xmax, double param1, double param2);
+     /*
+      * Function to construct on the fly this class through the action system
+      * @
+      * @
+      */
+     void constructDistributionContainerCustom(std::string DistAlias, distribution_type type, std::vector < double > dist_x, std::vector< double > dist_y, custom_dist_fit_type fit_type);
+
+     /*
+      * Function to get the enum of the distribution called DistAlias
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      */
+     distribution_type getType (std::string DistAlias);
+     /*
+      * Function to get the lower limit of the interval over which the distribution is defined
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      */
+     double getMin (std::string DistAlias);		// return limits of the interval over which the distribution is defined
+     /*
+      * Function to get the upper limit of the interval over which the distribution is defined
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      */
+     double getMax(std::string DistAlias);
+     /*
+      * Function to get the first characteristic parameter of the distribution (ex. "Mu")
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      */
+     double getParamater1(std::string DistAlias);	// return _parameter1
+     /*
+      * Function to get the second characteristic parameter of the distribution (ex. "Sigma")
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      */
+     double getParameter2(std::string DistAlias); // return _parameter1
+     /*
+      * Function to change on the fly the first characteristic parameter of the distribution (ex. "Mu")
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      * @ newParameter1, value of the parameter
+      */
+     void changeParameter1(std::string DistAlias, double newParameter1);	// to change on the fly paramter1
+     /*
+      * Function to change on the fly the second characteristic parameter of the distribution (ex. "Sigma")
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      * @ newParameter2, value of the parameter
+      */
+     void changeParameter2(std::string DistAlias, double newParameter2);	// to change on the fly paramter1
+     /*
+      * Function to get the pdf value of the distribution called "DistAlias"
+      * as function of the position x within [_xMin , xMax]
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      * @ x, position
+      */
+     double pdfCalc(std::string DistAlias, double x);	// return pdf value of the distribution _type as function of the position x within [_xMin , xMax]
+     /*
+      * Function to get the cdf value of the distribution called "DistAlias"
+      * as function of the position x within [_xMin , xMax]
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      * @ x, position
+      */
+     double cdfCalc(std::string DistAlias, double x);	// return cdf value of the distribution _type as function of the position x within [_xMin , xMax]
+     /*
+      * Function to get a random number distributed accordingly to the distribution
+      * given a random number [0,1]
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      */
+     double randGen(std::string DistAlias);   // return a random number distributed accordingly to the distribution given a random number [0,1]
+
+     protected:
+     std::map < std::string, int > _vector_pos_map;
+     std::vector < distribution_1D > _distribution_cont;
+     /*
+      * Function to get the position in the internal mapping
+      * @ DistAlias, alias of the distribution from which retrieving the parameter
+      */
+     int getPosition(std::string DistAlias);
+
+};
+
+
+#endif /* DISTRIBUTIONCONTAINER_H_ */
---
+++ src/distributions/DistributionContainer.C
@@ -0,0 +1,210 @@
+/*
+ * DistributionContainer.C
+ *
+ *  Created on: Jul 6, 2012
+ *      Author: alfoa
+ */
+#include "DistributionContainer.h"
+
+#include "distribution_1D.h"
+#include <iostream>
+using namespace std;
+
+class DistributionContainer;
+
+DistributionContainer::DistributionContainer()
+{
+}
+DistributionContainer::~DistributionContainer()
+{
+}
+void
+DistributionContainer::constructDistributionContainer(std::string DistAlias, distribution_type type, double xmin, double xmax, double param1, double param2){
+	_distribution_cont.push_back(distribution_1D(type, xmin, xmax, param1, param2));
+	_vector_pos_map[DistAlias]=_distribution_cont.size()-1;
+}
+void
+DistributionContainer::constructDistributionContainerCustom(std::string DistAlias, distribution_type type, std::vector< double > dist_x, std::vector< double > dist_y, custom_dist_fit_type fit_type){
+
+//  comment out when the constructor has been implemented
+
+//	_distribution_cont.push_back(distribution_1D(type, dist_x, dist_y, fit_type));
+//	_vector_pos_map[DistAlias]=_distribution_cont.size()-1;
+}
+
+distribution_type
+DistributionContainer::getType(std::string DistAlias){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].getType();
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+double
+DistributionContainer::getMin(std::string DistAlias){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].getMin();
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+double
+DistributionContainer::getMax(std::string DistAlias){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].getMax();
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+double
+DistributionContainer::getParamater1(std::string DistAlias){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].getParamater1();
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+double
+DistributionContainer::getParameter2(std::string DistAlias){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].getParameter2();
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+void
+DistributionContainer::changeParameter1(std::string DistAlias,double newParameter1){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	_distribution_cont[position].changeParameter1(newParameter1);
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    }
+
+}
+
+void
+DistributionContainer::changeParameter2(std::string DistAlias,double newParameter2){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	_distribution_cont[position].changeParameter2(newParameter2);
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    }
+
+}
+
+double
+DistributionContainer::pdfCalc(std::string DistAlias, double x){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].pdfCalc(x);
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+double
+DistributionContainer::cdfCalc(std::string DistAlias, double x){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].cdfCalc(x);
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+double
+DistributionContainer::randGen(std::string DistAlias){
+    int position;
+
+    position = getPosition(DistAlias);
+
+    if(position != -1){
+    	return _distribution_cont[position].randGen();
+    }
+    else{
+    	std::cerr << " ERROR: distribution " << DistAlias << "not present in the mapping";
+    	return DISTRIBUTION_ERROR;
+    }
+
+}
+
+int DistributionContainer::getPosition(std::string DistAlias){
+	std::map <std::string, int>::iterator p;
+
+	p = _vector_pos_map.find(DistAlias);
+	if(p != _vector_pos_map.end()){
+       return (p->second);
+	}
+	else{
+		return -1;
+	}
+}
+
+
+
+
+
+
+
