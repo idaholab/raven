9a25c4c1840a03496a5cb56e89839520e89eaef9
John Peterson <jw.peterson@inl.gov>
2013-01-18 11:46:11 +0000

    Updating Raven to work with automake libmesh, could not test Python3 parts.
    Added a gitignore file for Raven.
    
    r15947

--- raven.mk
+++ raven.mk
@@ -51,7 +51,7 @@ RAVEN_INCLUDE  := $(foreach i, $(RAVEN_INC_DIRS), -I$(i))
 
 libmesh_INCLUDE := $(RAVEN_INCLUDE) $(libmesh_INCLUDE)
 
-RAVEN_LIB := $(RAVEN_DIR)/libRAVEN-$(METHOD)$(libext)
+RAVEN_LIB := $(RAVEN_DIR)/libRAVEN-$(METHOD).la
 
 RAVEN_APP := $(RAVEN_DIR)/RAVEN-$(METHOD)
 
@@ -60,13 +60,12 @@ RAVEN_srcfiles    := $(shell find $(RAVEN_SRC_DIRS) -name *.C)
 RAVEN_csrcfiles   := $(shell find $(RAVEN_SRC_DIRS) -name *.c)
 RAVEN_fsrcfiles   := $(shell find $(RAVEN_SRC_DIRS) -name *.f)
 RAVEN_f90srcfiles := $(shell find $(RAVEN_SRC_DIRS) -name *.f90)
+
 # object files
 RAVEN_objects	:= $(patsubst %.C, %.$(obj-suffix), $(RAVEN_srcfiles))
 RAVEN_objects	+= $(patsubst %.c, %.$(obj-suffix), $(RAVEN_csrcfiles))
 RAVEN_objects += $(patsubst %.f, %.$(obj-suffix), $(RAVEN_fsrcfiles))
 RAVEN_objects += $(patsubst %.f90, %.$(obj-suffix), $(RAVEN_f90srcfiles))
-
-RAVEN_app_objects := $(patsubst %.C, %.$(obj-suffix), $(RAVEN_DIR)/src/main.C)
 
 # plugin files
 RAVEN_plugfiles   := $(shell find $(RAVEN_DIR)/plugins/ -name *.C 2>/dev/null)
@@ -80,30 +79,32 @@ RAVEN_plugins     += $(patsubst %.c, %-$(METHOD).plugin, $(RAVEN_cplugfiles))
 RAVEN_plugins     += $(patsubst %.f, %-$(METHOD).plugin, $(RAVEN_fplugfiles))
 RAVEN_plugins     += $(patsubst %.f90, %-$(METHOD).plugin, $(RAVEN_f90plugfiles))
 
+# RAVEN main
+RAVEN_main_src    := $(RAVEN_DIR)/src/main.C
+RAVEN_app_objects := $(patsubst %.C, %.$(obj-suffix), $(RAVEN_main_src))
+
+# dependency files
+RAVEN_deps := $(patsubst %.C, %.$(obj-suffix).d, $(RAVEN_srcfiles)) \
+              $(patsubst %.c, %.$(obj-suffix).d, $(RAVEN_csrcfiles)) \
+              $(patsubst %.C, %.$(obj-suffix).d, $(RAVEN_main_src))
+
+# If building shared libs, make the plugins a dependency, otherwise don't.
+ifeq ($(libmesh_shared),yes)
+  RAVEN_plugin_deps := $(RAVEN_plugins)
+else
+  RAVEN_plugin_deps :=
+endif
+
 all:: $(RAVEN_LIB)
 
-# build rule for lib RAVEN
-ifeq ($(enable-shared),yes)
-# Build dynamic library
-$(RAVEN_LIB): $(RAVEN_objects) $(RAVEN_plugins)
+$(RAVEN_LIB): $(RAVEN_objects) $(RAVEN_plugin_deps)
 	@echo "Linking "$@"..."
-	@$(libmesh_CC) $(libmesh_CXXSHAREDFLAG) -o $@ $(RAVEN_objects) $(libmesh_LDFLAGS)
-else
-# Build static library
-ifeq ($(findstring darwin,$(hostos)),darwin)
-$(RAVEN_LIB): $(RAVEN_objects)
-	@echo "Linking "$@"..."
-	@libtool -static -o $@ $(RAVEN_objects)
-else
-$(RAVEN_LIB): $(RAVEN_objects)
-	@echo "Linking "$@"..."
-	@$(AR) rv $@ $(RAVEN_objects)
-endif
-endif
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+	  $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(RAVEN_objects) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(RAVEN_DIR)
+	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $(RAVEN_LIB) $(RAVEN_DIR)
 
 # include RAVEN dep files
--include $(RAVEN_DIR)/src/*/*.d
-
+-include $(RAVEN_deps)
 
 # how to build RAVEN application
 ifeq ($(APPLICATION_NAME),RAVEN)
@@ -113,35 +114,70 @@ RAVEN_MODULES = $(RAVEN_DIR)/python_modules
 
 $(RAVEN_DIR)/src/executioners/PythonControl.$(obj-suffix): $(RAVEN_DIR)/src/executioners/PythonControl.C
 	@echo "Override PythonControl Compile"
-	$(libmesh_CXX) $(libmesh_CPPFLAGS) $(libmesh_CXXFLAGS) $(PYTHON_INCLUDE) -DRAVEN_MODULES='"$(RAVEN_MODULES)"' -MMD -MF $@.d $(libmesh_INCLUDE) -c $< -o $@ 
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=compile --quiet \
+          $(libmesh_CXX) $(libmesh_CPPFLAGS) $(libmesh_CXXFLAGS) $(PYTHON_INCLUDE) -DRAVEN_MODULES='"$(RAVEN_MODULES)"' $(libmesh_INCLUDE) -MMD -MF $@.d -MT $@ -c $< -o $@
+
+# TODO[JWP]: Should this use libtool to make a platform-independent shared library?
+#            I could not test it because I don't have python3.
+$(RAVEN_DIR)/python_modules/_distribution1D.so : $(RAVEN_DIR)/python_modules/distribution1D.i \
+                                                 $(RAVEN_DIR)/src/distributions/distribution_1D.C \
+                                                 $(RAVEN_DIR)/src/distributions/DistributionContainer.C \
+                                                 $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C
+# Swig
+	swig -c++ -python -py3 -I$(RAVEN_DIR)/../moose/include/base/ $(libmesh_INCLUDE) \
+          -I$(RAVEN_DIR)/../moose/include/utils/ -I$(RAVEN_DIR)/include/distributions/ \
+          -I$(RAVEN_DIR)/include/utilities/ -I$(RAVEN_DIR)/include/base/ \
+          $(RAVEN_DIR)/python_modules/distribution1D.i
+# Compile
+	$(libmesh_CXX) $(libmesh_CPPFLAGS) $(libmesh_CXXFLAGS) $(PYTHON_INCLUDE) -fPIC \
+          $(libmesh_INCLUDE) -I$(RAVEN_DIR)/include/distributions/ \
+          -I$(RAVEN_DIR)/include/utilities/ -I$(RAVEN_DIR)/include/base/ \
+          -I$(RAVEN_DIR)/../moose/include/base/ -I$(RAVEN_DIR)/../moose/include/utils/ \
+          $(RAVEN_DIR)/python_modules/distribution1D_wrap.cxx \
+          $(RAVEN_DIR)/src/distributions/*.C \
+          $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C \
+          $(RAVEN_DIR)/../moose/src/base/MooseObject.C -shared -o $(RAVEN_DIR)/python_modules/_distribution1D.so $(EXTRA_MOOSE_LIBS) $(PYTHON_LIB)
 
 
-$(RAVEN_DIR)/python_modules/_distribution1D.so : $(RAVEN_DIR)/python_modules/distribution1D.i  $(RAVEN_DIR)/src/distributions/distribution_1D.C $(RAVEN_DIR)/src/distributions/DistributionContainer.C $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C
-	swig -c++ -python -py3 -I$(RAVEN_DIR)/../moose/include/base/ $(libmesh_INCLUDE) -I$(RAVEN_DIR)/../moose/include/utils/ -I$(RAVEN_DIR)/include/distributions/ -I$(RAVEN_DIR)/include/utilities/ -I$(RAVEN_DIR)/include/base/ $(RAVEN_DIR)/python_modules/distribution1D.i
-	$(libmesh_CXX) $(libmesh_CPPFLAGS) $(libmesh_CXXFLAGS) $(PYTHON_INCLUDE) -fPIC $(libmesh_INCLUDE) -I$(RAVEN_DIR)/include/distributions/ -I$(RAVEN_DIR)/include/utilities/ -I$(RAVEN_DIR)/include/base/ -I$(RAVEN_DIR)/../moose/include/base/ -I$(RAVEN_DIR)/../moose/include/utils/ $(RAVEN_DIR)/python_modules/distribution1D_wrap.cxx $(RAVEN_DIR)/src/distributions/*.C $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C $(RAVEN_DIR)/../moose/src/base/MooseObject.C -shared -o $(RAVEN_DIR)/python_modules/_distribution1D.so $(EXTRA_MOOSE_LIBS) $(PYTHON_LIB)
-
-
-$(RAVEN_DIR)/python_modules/_raventools.so : $(RAVEN_DIR)/python_modules/raventools.i  $(RAVEN_DIR)/src/tools/batteries.C $(RAVEN_DIR)/src/tools/dieselGenerator.C $(RAVEN_DIR)/src/tools/pumpCoastdown.C $(RAVEN_DIR)/src/tools/decayHeat.C $(RAVEN_DIR)/src/tools/powerGrid.C $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C
+$(RAVEN_DIR)/python_modules/_raventools.so : $(RAVEN_DIR)/python_modules/raventools.i \
+                                             $(RAVEN_DIR)/src/tools/batteries.C \
+                                             $(RAVEN_DIR)/src/tools/dieselGenerator.C \
+                                             $(RAVEN_DIR)/src/tools/pumpCoastdown.C \
+                                             $(RAVEN_DIR)/src/tools/decayHeat.C \
+                                             $(RAVEN_DIR)/src/tools/powerGrid.C \
+                                             $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C
+# Swig
 	swig -c++ -python -py3 -I$(RAVEN_DIR)/include/tools/  -I$(RAVEN_DIR)/include/utilities/ $(RAVEN_DIR)/python_modules/raventools.i
-	$(libmesh_CXX) $(libmesh_CPPFLAGS) $(libmesh_CXXFLAGS) $(PYTHON_INCLUDE) -fPIC -I$(RAVEN_DIR)/include/tools/ -I$(RAVEN_DIR)/include/utilities/ $(RAVEN_DIR)/python_modules/raventools_wrap.cxx $(RAVEN_DIR)/src/tools/*.C $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C -shared -o $(RAVEN_DIR)/python_modules/_raventools.so $(PYTHON_LIB)
+# Compile
+	$(libmesh_CXX) $(libmesh_CPPFLAGS) $(libmesh_CXXFLAGS) $(PYTHON_INCLUDE) -fPIC \
+          -I$(RAVEN_DIR)/include/tools/ -I$(RAVEN_DIR)/include/utilities/ \
+          $(RAVEN_DIR)/python_modules/raventools_wrap.cxx \
+          $(RAVEN_DIR)/src/tools/*.C \
+          $(RAVEN_DIR)/src/utilities/Interpolation_Functions.C -shared -o $(RAVEN_DIR)/python_modules/_raventools.so $(PYTHON_LIB)
 
 
 RAVEN: $(RAVEN_APP) $(PYTHON_MODULES)
 
 $(RAVEN_APP): $(moose_LIB) $(elk_MODULES) $(r7_LIB) $(RAVEN_LIB) $(RAVEN_app_objects)
 	@echo "Linking "$@"..."
-	@$(libmesh_CXX) $(libmesh_CXXFLAGS) $(RAVEN_app_objects) -o $@ $(RAVEN_LIB) $(r7_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS) $(PYTHON_LIB)
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+          $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(RAVEN_app_objects) $(RAVEN_LIB) $(r7_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS) $(PYTHON_LIB)
 
--include $(RAVEN_DIR)/src/*.d
 endif
 
-delete_list := $(RAVEN_APP) $(RAVEN_LIB)
+delete_list := $(RAVEN_APP) $(RAVEN_LIB) $(RAVEN_DIR)/libRAVEN-$(METHOD).*
 
 clean::
-	@rm -f $(RAVEN_DIR)/python_modules/_distribution1D.so $(RAVEN_DIR)/python_modules/_raventools.so $(RAVEN_DIR)/python_modules/distribution1D_wrap.cxx $(RAVEN_DIR)/python_modules/raventools_wrap.cxx $(RAVEN_DIR)/python_modules/distribution1D.py
+	@rm -f $(RAVEN_DIR)/python_modules/_distribution1D.so \
+          $(RAVEN_DIR)/python_modules/_raventools.so \
+          $(RAVEN_DIR)/python_modules/distribution1D_wrap.cxx \
+          $(RAVEN_DIR)/python_modules/raventools_wrap.cxx \
+          $(RAVEN_DIR)/python_modules/distribution1D.py
 
 clobber::
-	@rm -f $(RAVEN_DIR)/python_modules/_distribution1D.so $(RAVEN_DIR)/python_modules/distribution1D_wrap.cxx $(RAVEN_DIR)/python_modules/distribution1D.py
+	@rm -f $(RAVEN_DIR)/python_modules/_distribution1D.so \
+          $(RAVEN_DIR)/python_modules/distribution1D_wrap.cxx \
+          $(RAVEN_DIR)/python_modules/distribution1D.py
 
 cleanall:: 
 	make -C $(RAVEN_DIR) clean 
