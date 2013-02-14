c9f33f301ab24c84084fb6caaa0b3f1b754c47cf
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2013-02-14 13:17:34 +0000

    CSV parser for Sampling Classes
    r16824

--- scripts/Csv_loader.py
+++ scripts/Csv_loader.py
@@ -40,6 +40,9 @@ class CsvLoader:
       time_end = False
       time_float = [float(x) for x in time]
       
+    inDict  = inParamDict
+    outDict = outParamDict       
+    
     #we now load the csv file
     with open(filein,'rb') as f:
       reader = csv.DictReader(f)
@@ -47,12 +50,12 @@ class CsvLoader:
       if(self.all_out_param):
           self.field_names = reader.fieldnames
       else:
-          self.field_names = outParamDict.keys()
+          self.field_names = outDict.keys()
           self.field_names.insert(0, 'time')
       
       row = reader.next()
       # Put the input params values into the inParamDict
-      for key, value in inParamDict.items():
+      for key, value in inDict.items():
         try:
           value = float(row[key])
         except:
@@ -68,27 +71,32 @@ class CsvLoader:
             actual_time   = float(actual_dict['time'])
             # This loop add interpolated values into the outParamDict
             if(not self.all_out_param):
-              for key, value in outParamDict.items():
+              for key, value in outDict.items():
                 try:
                   actual_value = float(row[key])
                   previous_value =  float(self.previous_dict[key])
-                  outParamDict[key] = (actual_value-previous_value)/(actual_time-previous_time)*(time_float-previous_time)
+                  outDict[key] = (actual_value-previous_value)/(actual_time-previous_time)*(time_float-previous_time)
                 except:
                   raise("ERROR: the parameter "+ key +" has not been found")
               break
             else:
-              outParamDict = self.actual_dict        
+              outDict = self.actual_dict        
               break
       #if time == end => we have to get the last row => it's in the actual_dict
       if time_end:
         if(not self.all_out_param):
-          for key, value in outParamDict.items():
+          for key, value in outDict.items():
             try:
-              outParamDict[key] = float(self.actual_dict[key])
+              outDict[key] = float(self.actual_dict[key])
             except:
               raise("ERROR: the parameter "+ key +" has not been found")
         else:
-          outParamDict = self.actual_dict
+          outDict = self.actual_dict
+          
+    for key in outDict:
+      outParamDict[key] = np.array(outDict[key], dtype=float)
+    for key in inDict:
+      inParamDict[key] = np.array(inDict[key], dtype=float)           
     return
   def csvLoaderForTimePointSet(self,filesin,time,inParamDict,outParamDict):
     '''
@@ -110,6 +118,9 @@ class CsvLoader:
       # convert the time in float
       time_end = False
       time_float = [float(x) for x in time]
+      
+    inDict  = inParamDict
+    outDict = outParamDict    
     
     for i in range(len(filesin)): 
       #we now load the csv file
@@ -119,14 +130,14 @@ class CsvLoader:
         if(self.all_out_param):
             self.field_names = reader.fieldnames
         else:
-            self.field_names = outParamDict.keys()
+            self.field_names = outDict.keys()
             self.field_names.insert(0, 'time')
       
         row = reader.next()
         # Put the input params values into the inParamDict
-        for key, value in inParamDict.items():
+        for key, value in inDict.items():
           try:
-            inParamDict[key].append(float(row[key]))
+            inDict[key].append(float(row[key]))
           except:
             print("ERROR: the parameter " + key + " has not been found")
         #loop over rows    
@@ -144,26 +155,174 @@ class CsvLoader:
                   try:
                     actual_value = float(row[key])
                     previous_value =  float(self.previous_dict[key])
-                    outParamDict[key].append(float((actual_value-previous_value)/(actual_time-previous_time)*
+                    outDict[key].append(float((actual_value-previous_value)/(actual_time-previous_time)*
                                                    (time_float-previous_time)))
                   except:
                     raise("ERROR: the parameter "+ key +" has not been found")
                 break
               else:
                 for key in self.actual_dict.keys():
-                  outParamDict[key].append(self.actual_dict[key])        
+                  outDict[key].append(self.actual_dict[key])        
                 break
         #if time == end => we have to get the last row => it's in the actual_dict
         if time_end:
           if(not self.all_out_param):
-            for key, value in outParamDict.items():
+            for key, value in outDict.items():
               try:
-                outParamDict[key].append(float(self.actual_dict[key]))
+                outDict[key].append(float(self.actual_dict[key]))
               except:
                 raise("ERROR: the parameter "+ key +" has not been found")
           else:
             for key in self.actual_dict.keys():
-              outParamDict[key].append(float(self.actual_dict[key]))   
+              outDict[key].append(float(self.actual_dict[key]))   
+    #convert the lists in working dicts into numpy arrays
+    for key in outDict:
+      outParamDict[key] = np.array(outDict[key], dtype=float)
+    for key in inDict:
+      inParamDict[key] = np.array(inDict[key], dtype=float)    
+    return
+  def csvLoaderForHistory(self,filein,time_filter=None,inParamDict,outParamDict):
+    '''
+    filein = file name
+    time_filter   = time_filter
+    inParamDict = parameters to be picked up (dictionary of values)
+    outParamDict = parameters to be picked up (dictionary of lists)   
+    '''
+    inDict  = inParamDict
+    outDict = outParamDict
+    
+    if(inDict['all']):
+      inDict['all'] = None
+    else:
+      inputParamN = len(inDict.keys())
+    if(outDict['all']):
+      self.all_out_param  = True
+      outParamDict['all'] = None            
+    else:
+      self.all_out_param  = False
+
+    if time_filter:
+      time_all = False
+      time_float = [float(x) for x in time_filter]
+    else:
+      # WE HAVE TO TAKE A DECISION REGARDING THE FILTERING
+      time_all = True
+      time_float = -1.0
+   
+    #we now load the csv file
+    with open(filein,'rb') as f:
+      reader = csv.DictReader(f)
+      reader.next
+      if(self.all_out_param):
+        self.field_names = reader.fieldnames
+      else:
+        self.field_names = outParamDict.keys()
+        self.field_names.insert(0, 'time')
+      
+      row = reader.next()
+      # Put the input params values into the inParamDict
+      for key, value in inDict.items():
+        try:
+          inDict[key]=(float(row[key]))
+        except:
+          print("ERROR: the parameter " + key + " has not been found")
+      #loop over rows    
+      for row in reader:
+          self.previous_dict = self.actual_dict
+          self.actual_dict   = row  
+          if(not self.all_out_param):
+            for key, value in outDict.items():
+              try:
+                outDict[key].append(float(row[key]))
+              except:
+                raise("ERROR: the parameter "+ key +" has not been found")
+            break
+          else:
+            for key in row.keys():
+              outDict[key].append(float(row[key]))        
+            break
+    #convert the lists in working dicts into numpy arrays
+    for key in outDict:
+      outParamDict[key] = np.array(outDict[key], dtype=float)
+    for key in inDict:
+      inParamDict[key] = np.array(inDict[key], dtype=float)        
+    return          
+  def csvLoaderForHistories(self,filesin,time_filters=None,inParamDict,outParamDict):
+    '''
+    filein = file name
+    time_filter   = time_filter
+    inParamDict = parameters to be picked up (dictionary of dictionaries of lists))
+    outParamDict = parameters to be picked up (dictionary of dictionaries of lists)   
+    '''
+    inDict  = {}
+    outDict = {}
+    # firstly we read the first file to grep information regarding the number of time-step
+    self.parseFilesToGrepDimensions(filesin, NtimeSteps, maxNumOfParams, NSamples)
+
+    inDict  = inParamDict
+    outDict = outParamDict
+    if(inDict['all']):
+      inputParamN = maxNumOfParams
+    else:
+      inputParamN = len(inDict.keys())
+    if(outDict['all']):
+      outputParamN = maxNumOfParams
+    else:
+      outputParamN = len(outDict.keys())  
+    # now we "allocate" the numpy matrix
+    data_work_in = np.zeros(shape=(inputParamN,NtimeSteps,NSamples))
+    data_work_out = np.zeros(shape=(outputParamN,NtimeSteps,NSamples))
+    
+    for i in range(NSamples):
+      self.csvLoaderForHistory(filein[i],time_filter,inDict,outDict)
+      index = 0
+      for key in inDict:
+        data_work_in[index,:,i] = float(inDict[key])
+        index = index + 1
+      index = 0  
+      for key in outDict:
+        data_work_out[index,:,i] = float(outDict[key])
+        index = index + 1    
+    #construct the output dictionaries
+    key_index = 0
+    for key in inDict:
+      if(str(key) != 'all'):
+        inParamDict[key] = data_work_in[key_index,:,:]
+        key_index = key_index + 1
+    key_index = 0    
+    for key in outDict:
+      if(str(key) != 'all'):
+        outParamDict[key] = data_work_out[key_index,:,:]
+        key_index = key_index + 1      
     return
 
+  def parseFilesToGrepDimensions(self,filesin,NtimeSteps,maxNumOfParams,NSamples):      
+    '''
+    filesin = file names
+    NtimeSteps   = maxNumberOfTs
+    maxNumOfParams = max number of parameters
+    NSamples = number of Samples   
+    '''    
+    NSamples       = len(filesin)
+    maxNumOfParams = 0
+    NtimeSteps     = 0  
+    for i in range(filesin):    
+      with open(filesin[i],'rb') as f:
+        reader = csv.DictReader(f)
+        reader.next
+        if(len(reader.fieldnames) > maxNumOfParams):
+          maxNumOfParams = len(reader.fieldnames)
+        
+        countTimeSteps = 1  
+        row = reader.next()
+        for row in reader:
+          countTimeSteps = countTimeSteps + 1   
+        
+        if(countTimeSteps>NtimeSteps):
+          NtimeSteps = countTimeSteps
+    return      
 
+      
+      
+      
+    
