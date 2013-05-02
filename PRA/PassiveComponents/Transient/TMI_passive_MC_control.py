2f1f9838fd25a720c82f876ad176345a1bf309fd
Diego Mandelli <diego.mandelli@inl.gov>
2013-05-02 09:53:29 +0000

    added passive components
    r18612

---
+++ PRA/PassiveComponents/Transient/TMI_passive_MC_control.py
@@ -0,0 +1,27 @@
+import sys
+import math
+import distribution1D
+import raventools
+
+# initialize distribution container
+distcont  = distribution1D.DistributionContainer.Instance()
+
+
+def initial_function(monitored, controlled, auxiliary):
+
+    return
+
+def control_function(monitored, controlled, auxiliary):
+  
+    
+	numberWeeks = 
+	pointPerWeek = 
+	
+	for i in range (numberWeeks*pointPerWeek):
+	
+	######################
+	#### First Cycle #####
+	######################
+  
+  return 
+
---
+++ PRA/PassiveComponents/Transient/pressureRupture.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+def pressureRupture(a,type):
+   "calculate the rupture pressure for states C and L"
+   
+   h     = 0.038    # in m
+   H     = 0.3048   # in m
+   sigmF = 333      # in MPa
+   
+   if type=1:   # circum type
+     b=a/0.1
+   else:
+     b=a
+     
+   M = 1-math.exp(-0.157&*b/(math.sqrt(H*(h-a)/2)))
+   
+   pressureF = 4*sigmaF*h/H*(1-a/h*M)
+   
+   return pressureF
+
