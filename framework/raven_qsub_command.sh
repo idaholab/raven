ab9c8ad8bd45e5349e15c7c039dbf28415e293ff
Joshua Cogliati <joshua.cogliati@inl.gov>
2013-10-24 14:37:43 +0000

    Making mpiqsub work on cluster. First, it needed to get the correct mpiexec so raven_qsub_command.sh was changed to load the moose environment. Secondly, some of the inputs were out of date.
    r22444

--- framework/raven_qsub_command.sh
+++ framework/raven_qsub_command.sh
@@ -7,9 +7,11 @@ cd $PBS_O_WORKDIR
 if [ `echo $MODULEPATH | grep -c '/apps/projects/moose/modulefiles'` -eq 0 ]; then   export MODULEPATH=$MODULEPATH:/apps/projects/moose/modulefiles; fi
 
 #module load python/2.7
+eval `/apps/local/modules/bin/modulecmd bash load moose-dev-gcc python/3.2`
 
 export PYTHONPATH=$HOME/raven_libs/pylibs/lib/python2.7/site-packages
 
 which python
+which mpiexec
 echo $COMMAND
 $COMMAND
