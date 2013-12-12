007f963670a1f9b3b540a2ff89e92734d4ee177b
Joshua Cogliati <joshua.cogliati@inl.gov>
2013-12-12 16:10:59 +0000

    Adding test of truncated normal.
    r23458

--- framework/TestDistributions.py
+++ framework/TestDistributions.py
@@ -52,7 +52,7 @@ checkAnswer("uniform ppf(0.0)",uniform.ppf(0.0),1.0)
 checkAnswer("uniform ppf(0.5)",uniform.ppf(0.5),2.0)
 checkAnswer("uniform ppf(1.0)",uniform.ppf(1.0),3.0)
 
-print(uniform.rvs(5))
+print(uniform.rvs(5),uniform.rvs())
 
 #Test Normal
 
@@ -63,6 +63,7 @@ normalElement.append(createElement("sigma",text="2.0"))
 normal = Distributions.Normal()
 normal.readMoreXML(normalElement)
 normal.initializeDistribution()
+
 checkAnswer("normal cdf(0.0)",normal.cdf(0.0),0.308537538726)
 checkAnswer("normal cdf(1.0)",normal.cdf(1.0),0.5)
 checkAnswer("normal cdf(2.0)",normal.cdf(2.0),0.691462461274)
@@ -71,7 +72,27 @@ checkAnswer("normal ppf(0.1)",normal.ppf(0.1),-1.56310313109)
 checkAnswer("normal ppf(0.5)",normal.ppf(0.5),1.0)
 checkAnswer("normal ppf(0.9)",normal.ppf(0.9),3.56310313109)
 
-print(normal.rvs(5))
+print(normal.rvs(5),normal.rvs())
+
+#Test Truncated Normal 
+
+truncNormalElement = ET.Element("truncnorm")
+truncNormalElement.append(createElement("mean",text="1.0"))
+truncNormalElement.append(createElement("sigma",text="2.0"))
+truncNormalElement.append(createElement("lowerBound",text="-1.0"))
+truncNormalElement.append(createElement("upperBound",text="3.0"))
+
+truncNormal = Distributions.Normal()
+truncNormal.readMoreXML(truncNormalElement)
+truncNormal.initializeDistribution()
+
+checkAnswer("truncNormal cdf(0.0)",truncNormal.cdf(0.0),0.219546787406)
+checkAnswer("truncNormal cdf(1.0)",truncNormal.cdf(1.0),0.5)
+checkAnswer("truncNormal cdf(2.0)",truncNormal.cdf(2.0),0.780453212594)
+
+checkAnswer("truncNormal ppf(0.1)",truncNormal.ppf(0.1),-0.498029197939)
+checkAnswer("truncNormal ppf(0.5)",truncNormal.ppf(0.5),1.0)
+checkAnswer("truncNormal ppf(0.9)",truncNormal.ppf(0.9),2.49802919794)
 
 #Test Gamma
 
@@ -92,7 +113,7 @@ checkAnswer("gamma ppf(0.1)",gamma.ppf(0.1),0.210721031316)
 checkAnswer("gamma ppf(0.5)",gamma.ppf(0.5),1.38629436112)
 checkAnswer("gamma ppf(0.9)",gamma.ppf(0.9),4.60517018599)
 
-print(gamma.rvs(5))
+print(gamma.rvs(5),gamma.rvs())
 
 #Test Beta
 
@@ -114,7 +135,7 @@ checkAnswer("beta ppf(0.1)",beta.ppf(0.1),0.63095734448)
 checkAnswer("beta ppf(0.5)",beta.ppf(0.5),0.870550563296)
 checkAnswer("beta ppf(0.9)",beta.ppf(0.9),0.979148362361)
 
-print(beta.rvs(5))
+print(beta.rvs(5),beta.rvs())
 
 #Test Triangular
 
@@ -135,7 +156,7 @@ checkAnswer("triangular ppf(0.1)",triangular.ppf(0.1),1.09544511501)
 checkAnswer("triangular ppf(0.5)",triangular.ppf(0.5),2.44948974278)
 checkAnswer("triangular ppf(0.9)",triangular.ppf(0.9),3.36754446797)
 
-print(triangular.rvs(5))
+print(triangular.rvs(5),triangular.rvs())
 
 #Test Poisson
 
@@ -154,7 +175,7 @@ checkAnswer("poisson ppf(0.1)",poisson.ppf(0.1),2.0)
 checkAnswer("poisson ppf(0.5)",poisson.ppf(0.5),4.0)
 checkAnswer("poisson ppf(0.9)",poisson.ppf(0.9),7.0)
 
-print(poisson.rvs(5))
+print(poisson.rvs(5),poisson.rvs())
 
 #Test Binomial
 
