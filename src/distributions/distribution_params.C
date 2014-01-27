a7b995f43a511cbd12a7ba60bfd74050ee7833f9
Joshua Cogliati <joshua.cogliati@inl.gov>
2014-01-27 13:55:30 +0000

    Updating to remove old built_by_action.
    r24230

--- src/distributions/distribution_params.C
+++ src/distributions/distribution_params.C
@@ -23,9 +23,9 @@ InputParameters validParams<distribution>(){
    params.addParam<unsigned int>("seed", _defaultSeed ,"RNG seed");
    params.addRequiredParam<std::string>("type","distribution type");
    params.addParam<unsigned int>("truncation", 1 , "Type of truncation"); // Truncation types: 1) pdf_prime(x) = pdf(x)*c   2) [to do] pdf_prime(x) = pdf(x)+c
-   params.addPrivateParam<std::string>("built_by_action", "add_distribution");
    params.addParam<unsigned int>("force_distribution", 0 ,"force distribution to be evaluated at: if (0) Don't force distribution, (1) xMin, (2) Mean, (3) xMax");
 
+   params.registerBase("distribution");
    return params;
 }
 
