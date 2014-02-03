959c4441ee1bfed6b81bcff37da142c00d802603
Joshua Cogliati <joshua.cogliati@inl.gov>
2014-02-03 12:46:32 +0000

    Adding a makefile that can be used to generate python modules. This splits out the python module ability to separate makefile if needed.
    r24483

---
+++ config.mk
@@ -0,0 +1,63 @@
+PYTHON3_HELLO := $(shell python3 -c "print('HELLO')" 2>/dev/null)
+PYTHON2_HELLO := $(shell python -c "print 'HELLO'" 2>/dev/null)
+
+SWIG_VERSION := $(shell swig -version 2>/dev/null)
+PYTHON_CONFIG_WHICH := $(shell which python-config 2>/dev/null)
+
+UNAME := $(shell uname)
+
+ifneq ($(PYTHON_CONFIG_WHICH),)
+	PYTHON2_INCLUDE=$(shell python-config --includes)
+	PYTHON2_LIB=$(shell python-config --ldflags)
+endif
+
+ifeq ($(PYTHON3_HELLO),HELLO)
+        PYTHON_INCLUDE = $(shell $(RAVEN_DIR)/scripts/find_flags.py include) #-DPy_LIMITED_API 
+        PYTHON_LIB = $(shell $(RAVEN_DIR)/scripts/find_flags.py library) #-DPy_LIMITED_API 
+ifeq ($(findstring SWIG Version 2,$(SWIG_VERSION)),)
+	CONTROL_MODULES = 
+	PYTHON_MODULES = 
+else
+	CONTROL_MODULES = $(RAVEN_DIR)/control_modules/_distribution1D.so $(RAVEN_DIR)/control_modules/_raventools.so 
+	PYTHON_MODULES = $(RAVEN_DIR)/python_modules/_distribution1Dpy2.so $(RAVEN_DIR)/python_modules/_distribution1Dpy3.so
+endif
+
+else
+ifeq ($(PYTHON2_HELLO),HELLO)
+ifeq ($(PYTHON_CONFIG_WHICH),)
+	PYTHON_INCLUDE = -DNO_PYTHON3_FOR_YOU
+	PYTHON_LIB = -DNO_PYTHON3_FOR_YOU
+	CONTROL_MODULES = 
+	PYTHON_MODULES = 
+else
+	PYTHON_INCLUDE=$(PYTHON2_INCLUDE)
+	PYTHON_LIB=$(PYTHON2_LIB)
+	CONTROL_MODULES=
+	PYTHON_MODULES = 
+#CONTROL_MODULES=$(RAVEN_DIR)/control_modules/_distribution1D.so $(RAVEN_DIR)/control_modules/_raventools.so
+endif
+else
+#Python3 not found.
+	PYTHON_INCLUDE = -DNO_PYTHON3_FOR_YOU
+	PYTHON_LIB = -DNO_PYTHON3_FOR_YOU
+	CONTROL_MODULES = 
+	PYTHON_MODULES = $(RAVEN_DIR)/python_modules/_distribution1Dpy2.so
+endif
+endif
+
+RAVEN_LIB_INCLUDE_DIR := $(RAVEN_DIR)/contrib/include
+
+ifeq  ($(UNAME),Darwin)
+raven_shared_ext := dylib
+else
+raven_shared_ext := so
+endif
+
+HAS_DYNAMIC := $(shell $(libmesh_LIBTOOL) --config | grep build_libtool_libs | cut -d'=' -f2 )
+
+ifeq ($(HAS_DYNAMIC),no)  
+ifdef CONTROL_MODULES
+$(warning RAVEN modules must be compiled with shared libmesh libraries)
+	CONTROL_MODULES = 
+endif
+endif
--- raven.mk
+++ raven.mk
@@ -1,68 +1,3 @@
-PYTHON3_HELLO := $(shell python3 -c "print('HELLO')" 2>/dev/null)
-PYTHON2_HELLO := $(shell python -c "print 'HELLO'" 2>/dev/null)
-
-SWIG_VERSION := $(shell swig -version 2>/dev/null)
-PYTHON_CONFIG_WHICH := $(shell which python-config 2>/dev/null)
-
-UNAME := $(shell uname)
-
-ifneq ($(PYTHON_CONFIG_WHICH),)
-	PYTHON2_INCLUDE=$(shell python-config --includes)
-	PYTHON2_LIB=$(shell python-config --ldflags)
-endif
-
-ifeq ($(PYTHON3_HELLO),HELLO)
-        PYTHON_INCLUDE = $(shell $(RAVEN_DIR)/scripts/find_flags.py include) #-DPy_LIMITED_API 
-        PYTHON_LIB = $(shell $(RAVEN_DIR)/scripts/find_flags.py library) #-DPy_LIMITED_API 
-ifeq ($(findstring SWIG Version 2,$(SWIG_VERSION)),)
-	CONTROL_MODULES = 
-else
-	CONTROL_MODULES = $(RAVEN_DIR)/control_modules/_distribution1D.so $(RAVEN_DIR)/control_modules/_raventools.so $(RAVEN_DIR)/python_modules/_distribution1Dpy2.so $(RAVEN_DIR)/python_modules/_distribution1Dpy3.so
-endif
-
-else
-ifeq ($(PYTHON2_HELLO),HELLO)
-ifeq ($(PYTHON_CONFIG_WHICH),)
-	PYTHON_INCLUDE = -DNO_PYTHON3_FOR_YOU
-	PYTHON_LIB = -DNO_PYTHON3_FOR_YOU
-	CONTROL_MODULES = 
-else
-	PYTHON_INCLUDE=$(PYTHON2_INCLUDE)
-	PYTHON_LIB=$(PYTHON2_LIB)
-	CONTROL_MODULES=
-#CONTROL_MODULES=$(RAVEN_DIR)/control_modules/_distribution1D.so $(RAVEN_DIR)/control_modules/_raventools.so
-endif
-else
-#Python3 not found.
-	PYTHON_INCLUDE = -DNO_PYTHON3_FOR_YOU
-	PYTHON_LIB = -DNO_PYTHON3_FOR_YOU
-	CONTROL_MODULES = $(RAVEN_DIR)/python_modules/_distribution1Dpy2.so
-endif
-endif
-
-RAVEN_LIB_INCLUDE_DIR := $(RAVEN_DIR)/contrib/include
-
-ifeq  ($(UNAME),Darwin)
-raven_shared_ext := dylib
-else
-raven_shared_ext := so
-endif
-
-HAS_DYNAMIC := $(shell $(libmesh_LIBTOOL) --config | grep build_libtool_libs | cut -d'=' -f2 )
-
-ifeq ($(HAS_DYNAMIC),no)  
-ifdef CONTROL_MODULES
-$(warning RAVEN modules must be compiled with shared libmesh libraries)
-	CONTROL_MODULES = 
-endif
-endif
-
-
-#ifeq ($(UNAME),Darwin)
-EXTRA_MOOSE_LIBS = -L$(MOOSE_DIR) -lmoose-$(METHOD) -L$(RAVEN_DIR) -lRAVEN-$(METHOD) -L$(R7_DIR) -lr7_moose-$(METHOD) -L$(ELK_DIR)/lib -lelk-$(METHOD) -lphase_field-$(METHOD) -lnavier_stokes-$(METHOD) -lheat_conduction-$(METHOD) -lsolid_mechanics-$(METHOD) -ltensor_mechanics-$(METHOD) -llinear_elasticity-$(METHOD) -lchemical_reactions-$(METHOD) -lfluid_mass_energy_balance-$(METHOD) -lmisc-$(METHOD) -lcontact-$(METHOD) $(libmesh_LIBS)
-#else
-#	EXTRA_MOOSE_LIBS = 
-#endif
 
 
 RAVEN_INC_DIRS := $(shell find $(RAVEN_DIR)/include -type d -not -path "*/.svn*")
@@ -197,7 +132,7 @@ $(RAVEN_DIR)/control_modules/_raventools.so : $(RAVEN_DIR)/control_modules/raven
 	ln -s libraventools.$(raven_shared_ext) $(RAVEN_MODULES)/_raventools.so
 
 
-RAVEN: $(RAVEN_APP) $(CONTROL_MODULES)
+RAVEN: $(RAVEN_APP) $(CONTROL_MODULES) $(PYTHON_MODULES)
 
 $(RAVEN_APP): $(moose_LIB) $(elk_MODULES) $(r7_LIB) $(RAVEN_LIB) $(RAVEN_app_objects)
 	@echo "Linking "$@"..."
@@ -215,7 +150,8 @@ clean::
           $(RAVEN_DIR)/control_modules/raventools_wrap.cxx \
           $(RAVEN_DIR)/control_modules/distribution1D.py \
           $(RAVEN_DIR)/control_modules/libdistribution1D.* \
-          $(RAVEN_DIR)/control_modules/*.so*
+          $(RAVEN_DIR)/control_modules/*.so* \
+          $(RAVEN_DIR)/python_modules/*.so*
 
 clobber::
 	@rm -f $(RAVEN_DIR)/control_modules/_distribution1D.so \
