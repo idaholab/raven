a4998ce5fa3642726f50ead6aefa72ded17ecb16
Joshua Cogliati <joshua.cogliati@inl.gov>
2014-03-05 14:39:19 +0000

    Start of tests for Grid and LHS tests for Raven. (These cause not implement errors when run, but are useful to see progress of Grid and LHS for Raven)
    r25320

---
+++ tests/framework/LHStestRaven/simple_flow_3eqn_control.py
@@ -0,0 +1,25 @@
+
+
+def initial_function(monitored, controlled, auxiliary):
+    print("monitored",monitored,"controlled",controlled,"auxiliary",auxiliary)
+    mult = 1.0
+    controlled.pipe_Area = mult*controlled.pipe_Area
+    controlled.pipe_Dh = mult*controlled.pipe_Dh
+    controlled.pipe_Hw = mult*controlled.pipe_Hw
+    controlled.pipe_Tw = mult*controlled.pipe_Tw
+    controlled.pipe_aw = mult*controlled.pipe_aw
+    controlled.pipe_f = mult*controlled.pipe_f
+
+    return
+
+def control_function(monitored, controlled, auxiliary):
+    print("monitored",monitored,"controlled",controlled,"auxiliary",auxiliary)
+    mult = 1.0
+    controlled.pipe_Area = mult*controlled.pipe_Area
+    controlled.pipe_Dh = mult*controlled.pipe_Dh
+    controlled.pipe_Hw = mult*controlled.pipe_Hw
+    controlled.pipe_Tw = mult*controlled.pipe_Tw
+    controlled.pipe_aw = mult*controlled.pipe_aw
+    controlled.pipe_f = mult*controlled.pipe_f
+    return
+
