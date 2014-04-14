208b49c5077c8d9171656bed76b34b48dfd26af0
Joshua Cogliati <joshua.cogliati@inl.gov>
2014-04-14 12:43:53 +0000

    Check for needed modules for RavenPython tests.
    r26432

--- scripts/TestHarness/testers/RavenPython.py
+++ scripts/TestHarness/testers/RavenPython.py
@@ -1,6 +1,7 @@
 from util import *
 from Tester import Tester
 from CSVDiffer import CSVDiffer
+import RavenUtils
 import os, subprocess
 
 class RavenPython(Tester):
@@ -8,7 +9,7 @@ class RavenPython(Tester):
     output_swig = subprocess.Popen(["swig","-version"],stdout=subprocess.PIPE).communicate()[0]
   except OSError:
     output_swig = "Failed"
-    
+
   has_swig2 = "Version 2.0" in output_swig
 
 
@@ -16,7 +17,7 @@ class RavenPython(Tester):
     params = Tester.getValidParams()
     params.addRequiredParam('input',"The python file to use for this test.")
     if os.environ.get("CHECK_PYTHON3","0") == "1":
-      params.addParam('python_command','python3','The command to use to run python')  
+      params.addParam('python_command','python3','The command to use to run python')
     else:
       params.addParam('python_command','python','The command to use to run python')
     params.addParam('requires_swig2', False, "Requires swig2 for test")
@@ -34,7 +35,10 @@ class RavenPython(Tester):
   def checkRunnable(self, option):
     if self.specs['requires_swig2'] and not RavenPython.has_swig2:
       return (False, 'skipped (No swig 2.0 found)')
-
+    missing = RavenUtils.checkForMissingModules()
+    if len(missing) > 0:
+      return (False,'skipped (Missing python modules: '+" ".join(missing)+
+              " PYTHONPATH="+os.environ.get("PYTHONPATH","")+')')
     return (True, '')
 
   def processResults(self, moose_dir,retcode, options, output):
