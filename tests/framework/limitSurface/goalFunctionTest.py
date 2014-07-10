1ae390ea4c7a75fc1c1b1be9642e0b7b82fa9e30
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2014-07-10 09:32:59 +0000

    added regression test for Limit Surface Postprocessor
    r28499

---
+++ tests/framework/limitSurface/goalFunctionTest.py
@@ -0,0 +1,11 @@
+'''
+Created on NO NEED TO KNOW
+
+@author: alfoa
+'''
+import numpy as np
+import math
+
+def __residuumSign(self):
+  if self.z < 9.0 : return 1.0
+  else             : return -1.0
