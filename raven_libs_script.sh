7611dfcf41fede246a66aa0949f5d750d1c5ad59
Joshua Cogliati <joshua.cogliati@inl.gov>
2014-07-21 07:05:30 +0000

    Only build python3 libs if CROW_USE_PYTHON3 is true.
    Otherwise there is not much point.
    
    r28712

--- raven_libs_script.sh
+++ raven_libs_script.sh
@@ -1,8 +1,9 @@
+
 #!/bin/bash
 cd `dirname $0`
 ./backend_raven_libs_script.sh
 
-if test ""`which python3-config` != ""
+if test ""`which python3-config` != "" -a ""$CROW_USE_PYTHON3 == TRUE
 then
     if test "`uname -sr | sed 's/\..*//'`" = "Darwin 13"
     then
