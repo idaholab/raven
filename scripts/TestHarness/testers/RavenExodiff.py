74b8b8918bfcf5a7bf248db6473189156338d26a
Joshua Cogliati <joshua.cogliati@inl.gov>
2014-08-12 14:28:51 +0000

    Fix from Jason Miller to use stdout for checking if python-config exists.
    r29158

--- scripts/TestHarness/testers/RavenExodiff.py
+++ scripts/TestHarness/testers/RavenExodiff.py
@@ -19,7 +19,7 @@ class RavenExodiff(Exodiff):
     has_python2 = py2_output.startswith("HELLO")
 
     try:
-        py_cfg_output = subprocess.Popen(["python-config","--help"],stderr=subprocess.PIPE).communicate()[1]
+        py_cfg_output = subprocess.Popen(["python-config","--help"],stdout=subprocess.PIPE).communicate()[0]
     except OSError:
         py_cfg_output = "Failed"
 
