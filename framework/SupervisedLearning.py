8fb2ae396f61275b8a620165715da8a03727f315
P Talbot <taoyiliang@gmail.com>
2015-02-10 11:41:05 -0700

    yet more debugging

--- framework/SupervisedLearning.py
+++ framework/SupervisedLearning.py
@@ -355,6 +355,7 @@ class GaussPolynomialRom(NDinterpolatorRom):
     print('DEBUG scipy tests')
     for i in range(1,5):
       print('DEBUG  ',self.quads.values()[0](i))
+    print('DEBUG scipy',scipy.__version__)
     #local evals
     #if len(self.features)==1:
     #  tests=[(0),(0.2),(0.5),(0.7),(1.0)]
