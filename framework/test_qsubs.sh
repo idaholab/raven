76c3fc93591b21a76ba672fe6c7beb891f447ca9
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-02-25 11:20:18 -0700

    Initial simple test to run a qsub, wait for it, and the return.

---
+++ framework/test_qsubs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -Rvf ../inputs/mpi_driver_test/FirstMRun/
+
+
+JOB=`python Driver.py ../inputs/mpi_driver_test/test_mpiqsub.xml | tail -n 1`
+
+JOB_NUMBER=`echo $JOB|cut -d '.' -f 1`
+echo $JOB_NUMBER
+
+qsub -W depend=afterany:$JOB_NUMBER -W block=true script
+
+ls ../inputs/mpi_driver_test/FirstMRun/
+
+
