fe49add24799e7ff350eb6e212846c5789efccc9
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-02-26 12:27:11 -0700

    Adding a quick test for mpiqsub.

--- framework/test_qsubs.sh
+++ framework/test_qsubs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -Rvf ../inputs/mpi_driver_test/FirstMRun/
+rm -Rf ../inputs/mpi_driver_test/FirstMRun/
 
 
 JOB=`python Driver.py ../inputs/mpi_driver_test/test_mpiqsub.xml | tail -n 1`
@@ -10,6 +10,6 @@ echo $JOB_NUMBER
 
 qsub -W depend=afterany:$JOB_NUMBER -W block=true script
 
-ls ../inputs/mpi_driver_test/FirstMRun/
+ls ../inputs/mpi_driver_test/FirstMRun/*eqn.csv
 
 
