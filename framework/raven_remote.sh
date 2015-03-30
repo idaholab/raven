c8936bd51562adb322d36a496d29e7290ff7f518
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-03-30 09:32:11 -0600

    Updating pbsdsh method to work on falcon.
    Also updating test_pbs.xml command to newer syntax.

--- framework/raven_remote.sh
+++ framework/raven_remote.sh
@@ -5,18 +5,26 @@ echo `pwd`
 echo PBS_O_WORKDIR $PBS_O_WORKDIR
 hostname
 
-source /etc/profile  
-if [ `echo $MODULEPATH | grep -c '/apps/projects/moose/modulefiles'` -eq 0 ]; then   export MODULEPATH=$MODULEPATH:/apps/projects/moose/modulefiles; fi
+source /etc/profile
+AVAIL_MODULES=`module avail 2>&1`
+#echo AVAIL_MODULES $AVAIL_MODULES
+echo $AVAIL_MODULES | grep 'raven-devel-gcc';
+if test 0 -eq $?;
+then
+    echo found raven-devel-gcc
+    module load pbs raven-devel-gcc
+else
+    echo no raven-devel-gcc
+     if [ `echo $MODULEPATH | grep -c '/apps/projects/moose/modulefiles'` -eq 0 ]; then   export MODULEPATH=$MODULEPATH:/apps/projects/moose/modulefiles; fi
 
-echo "$MODULEPATH"
-module load moose-dev-gcc pbs python/3.2
+     echo "$MODULEPATH"
+     module load moose-dev-gcc pbs python/3.2
+
+     which python3
+fi
 
 #Disable MVAPICH2 sched_setaffinity
 export MV2_ENABLE_AFFINITY=0
-
-#cd $PBS_O_WORKDIR
-
-which python3
 
 echo $@
 
