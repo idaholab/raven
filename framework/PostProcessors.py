b69ee5cdec738d0d08737c5cc3a151656d9b2d83
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-04-09 08:53:43 -0600

    Adds the ability to calculate extra f_z statistics.
    These statistics are somewhat timeconsuming to calculate, so they
    are not calculated by default.

--- framework/PostProcessors.py
+++ framework/PostProcessors.py
@@ -329,6 +329,7 @@ class ComparisonStatistics(BasePostProcessor):
     #self.dataPulls = [] #List of data references that will be used
     #self.referenceData = [] #List of reference (experimental) data
     self.methodInfo = {} #Information on what stuff to do.
+    self.f_z_stats = False
 
   def inputToInternal(self,currentInput):
     return [(currentInput)]
@@ -358,6 +359,9 @@ class ComparisonStatistics(BasePostProcessor):
           self.methodInfo['num_bins'] = int(outer.attrib['num_bins'])
         if 'bin_method' in outer.attrib:
           self.methodInfo['bin_method'] = outer.attrib['bin_method'].lower()
+      if outer.tag == 'fz':
+        self.f_z_stats =  (outer.text.lower() in utils.stringsThatMeanTrue())
+
 
 
   def run(self, Input): # inObj,workingDir=None):
@@ -447,7 +451,7 @@ class ComparisonStatistics(BasePostProcessor):
           utils.printCsv(csv,'"'+key+'"',dataStats[key])
         print("data_stats",dataStats)
         graphData.append((dataStats, cdfFunc, pdfFunc,str(dataPull)))
-      mathUtils.printGraphs(csv, graphData)
+      mathUtils.printGraphs(csv, graphData, self.f_z_stats)
       for i in range(len(graphData)):
         dataStat = graphData[i][0]
         def delist(l):
