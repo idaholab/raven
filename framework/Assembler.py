18ccb1ce494a92f4fe10f7195a88442afcd95b6c
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2015-05-05 07:41:50 -0600

    added a line

--- framework/Assembler.py
+++ framework/Assembler.py
@@ -62,6 +62,7 @@ class Assembler(MessageHandler.MessageUser):
       self.assemblerDict[key] =  []
       for interface in value:
         self.assemblerDict[key].append([interface[0],interface[1],interface[2],initDict[interface[0]][interface[2]]])
+  
   def _readMoreXML(self,xmlNode):
     self.type = xmlNode.tag
     if 'name' in xmlNode.attrib: self.name = xmlNode.attrib['name']
