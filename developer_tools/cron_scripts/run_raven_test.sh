b97630b0d360b3a60048dcdbb30a4c33beaa5837
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-05-08 09:41:25 -0600

    Adding variables for the parts that will change.

--- developer_tools/cron_scripts/run_raven_test.sh
+++ developer_tools/cron_scripts/run_raven_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Change these to whatever is correct locally
+RAVEN_HOME=$HOME/raven/falcon/fal_pack/
+RESULT_EMAIL=joshua.cogliati@inl.gov
+
 DATE_STRING=`date '+%F_%R'`
 echo "Started" > $HOME/raven/logs/cron_started_${DATE_STRING}
 
@@ -12,6 +16,6 @@ eval `/usr/bin/modulecmd bash load pbs raven-devel-gcc` #2>&1 | tee -a $HOME/rav
 
 env 2>&1 | tee -a $HOME/raven/logs/cron_output_${DATE_STRING}
 
-$HOME/raven/falcon/fal_pack/raven/work_in_progress/cron_work/checkout_build_and_test.sh 2>&1 | tee -a $HOME/raven/logs/cron_output_${DATE_STRING}
+$RAVEN_HOME/raven/developer_tools/cron_scripts/checkout_build_and_test.sh 2>&1 | tee -a $HOME/raven/logs/cron_output_${DATE_STRING}
 
-tail -n 500 $HOME/raven/logs/cron_output_${DATE_STRING} | mailx -s "RAVEN cron output ${DATE_STRING}" joshua.cogliati@inl.gov
+tail -n 500 $HOME/raven/logs/cron_output_${DATE_STRING} | mailx -s "RAVEN cron output ${DATE_STRING}" $RESULT_EMAIL
