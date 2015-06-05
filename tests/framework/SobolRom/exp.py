52da2733f29e3c82eb2c744f256fa5c479726ca1
talbpaul <talbotpne@gmail.com>
2015-06-05 15:31:42 -0600

    removing files I accidentally added

--- tests/framework/SobolRom/exp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def eval(inp,exp):
-  #return sum(n**exp for n in inp)
-  return np.exp(-sum(inp))
-
-def run(self,Input):
-  self.ans = eval((self.x1,self.x2,self.x3),1)
-  self.ans2 = eval((self.x1,self.x2,self.x3),2)
