5f28834d0a013b3ca7832340d4da592352363f5b
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-06-09 16:02:52 -0600

    Only build amsc if module is out of date.

--- amsc.mk
+++ amsc.mk
@@ -2,7 +2,10 @@ mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 MY_DIR :=  $(patsubst %/,%,$(dir $(mkfile_path)))
 
 AMSC_srcfiles := $(shell find $(MY_DIR)/src/contrib -name "*.cpp" -not -name main.C)
-amsc:: $(MY_DIR)/src/contrib/amsc.i $(AMSC_srcfiles)
+AMSC_MODULE = $(MY_DIR)/src/contrib/_amsc.so
+
+amsc :: $(AMSC_MODULE)
+$(AMSC_MODULE) : $(MY_DIR)/src/contrib/amsc.i $(AMSC_srcfiles)
 	@echo "Building "$@"..."
 	(cd $(MY_DIR) && if test `uname` != "Darwin"; then unset CXX; fi && python $(MY_DIR)/setup.py build_ext build install --install-platlib=$(MY_DIR)/src/contrib)
 	@echo "Done"
