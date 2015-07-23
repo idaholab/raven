f85bced641bf15a691e27749614e46aeeac6ae17
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-07-23 15:18:28 -0600

    Handle revisions better by using the submodule ids.

--- developer_tools/packaging/raven_framework_git.sh
+++ developer_tools/packaging/raven_framework_git.sh
@@ -3,24 +3,16 @@ SCRIPT_DIRNAME=`dirname $0`
 SCRIPT_DIR=`(cd $SCRIPT_DIRNAME; pwd)`
 CHECKOUT_DIR=${CHECKOUT_DIR:=$HOME/raven_framework_checkout_git}
 RAVEN_DIR=${RAVEN_DIR:=$SCRIPT_DIR/../..}
-PROJECT_DIR=$RAVEN_DIR/..
+PULL_DIRECTORY=$RAVEN_DIR/..
 MAIN_DIR=$CHECKOUT_DIR/
 mkdir -p $MAIN_DIR
 cd $MAIN_DIR
 RAVEN_BRANCH=${RAVEN_BRANCH:=master}
 RAVEN_REV=${RAVEN_REV:=$RAVEN_BRANCH}
-CROW_BRANCH=${CROW_BRANCH:=$RAVEN_BRANCH}
-#CROW_REV=${CROW_REV:=$CROW_BRANCH}
-MOOSE_BRANCH=${MOOSE_BRANCH:=master}
-#MOOSE_REV=${MOOSE_REV:=$MOOSE_BRANCH}
-
-echo using branches MOOSE: $MOOSE_BRANCH RAVEN: $RAVEN_BRANCH
 
 rm -Rf raven
-git clone --branch $RAVEN_BRANCH --shared --no-checkout $PROJECT_DIR/raven
+git clone --branch $RAVEN_BRANCH --shared --no-checkout $PULL_DIRECTORY/raven
 cd raven
-#git clone --branch $MOOSE_BRANCH --shared --no-checkout $PROJECT_DIR/moose
-#git clone --branch $CROW_BRANCH --shared --no-checkout $PROJECT_DIR/crow
 
 cd $MAIN_DIR/raven
 git checkout $RAVEN_REV -- framework
@@ -48,12 +40,12 @@ CROW_REV=`git submodule status crow | tr -d - | cut -d ' ' -f 1`
 MOOSE_REV=`git submodule status moose | tr -d - | cut -d ' ' -f 1`
 
 cd $MAIN_DIR/raven/
-git clone --branch $CROW_BRANCH --shared --no-checkout $PROJECT_DIR/crow
+git clone --shared --no-checkout $PULL_DIRECTORY/crow
 cd crow
 git checkout $CROW_REV -- .
 
 cd $MAIN_DIR/raven/
-git clone --branch $MOOSE_BRANCH --shared --no-checkout $PROJECT_DIR/moose
+git clone --shared --no-checkout $PULL_DIRECTORY/moose
 cd moose
 git checkout $MOOSE_REV -- framework/scripts
 git checkout $MOOSE_REV -- framework/Makefile
@@ -67,3 +59,5 @@ git checkout $MOOSE_REV -- COPYING
 git checkout $MOOSE_REV -- EXPORT_CONTROL
 git checkout $MOOSE_REV -- LICENSE
 git checkout $MOOSE_REV -- README.md
+
+echo using revisions MOOSE: $MOOSE_REV RAVEN: $RAVEN_REV CROW: $CROW_REV
