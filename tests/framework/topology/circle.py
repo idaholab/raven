30984fe2bf697faf6099fca51b78e8cb18c93828
Dan Maljovec <maljovec@cs.utah.edu>
2015-08-13 15:39:43 -0600

    Adding a simple test case for the circular limit surface in 2D.

---
+++ tests/framework/topology/circle.py
@@ -0,0 +1,14 @@
+'''
+Created on 8/13/15
+
+@author: maljdan
+'''
+import numpy as np
+import math
+
+def eval(inp):
+  retVal = math.sqrt(inp[0]*inp[0] + inp[1]*inp[1])
+  return float('%.8f' % retVal)
+
+def run(self,Input):
+  self.y = eval((self.x1,self.x2))
