241b773ec0bcafd7fa8653f1cc77351569a31741
P Talbot <taoyiliang@gmail.com>
2015-09-11 12:08:59 -0600

    sorted index sets, sparse grids

--- tests/framework/IndexSetTest/polynomial.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def eval(inp,exp):
-  return sum(n**exp for n in inp)
-  #return np.exp(-sum(inp)/len(inp))
-
-def run(self,Input):
-  self.ans = eval((self.x1,self.x2),1)
-  self.ans2 = eval((self.x1,self.x2),2)
