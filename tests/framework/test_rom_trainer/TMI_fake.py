3957b1b41f0a4f73eabd634aa009870754dab098
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-10-13 09:45:44 -0600

    Updating test_multi_target_rom to use external models.

--- tests/framework/test_rom_trainer/TMI_fake.py
+++ tests/framework/test_rom_trainer/TMI_fake.py
@@ -7,6 +7,8 @@ def run(self, Input):
   DeltaTimeScramToAux = Input["DeltaTimeScramToAux"]
   DG1recoveryTime = Input["DG1recoveryTime"]
   self.CladTempThreshold = numpy.zeros(number_of_steps)
+  self.UpperPlenumEnergy= numpy.zeros(number_of_steps)
   for i in range(len(self.time)):
     self.time[i] = 0.25*i
     self.CladTempThreshold[i] = self.time[i]*50.0 + DeltaTimeScramToAux*200.0 + DG1recoveryTime*500.0
+    self.UpperPlenumEnergy[i] = self.time[i]*5.0 + DeltaTimeScramToAux*30.0 + DG1recoveryTime*40.0 + DeltaTimeScramToAux*DG1recoveryTime*5.0
