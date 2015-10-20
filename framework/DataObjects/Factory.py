c0b2fc85a926f914ce7b93f6c96cd97ea6b7ad46
Dan Maljovec <maljovec@cs.utah.edu>
2015-10-20 16:17:35 -0600

    Adding docstrings and fixing errors caused by the Factory being unable to locate the other files of the submodule. This is a place where we could use wildcard imports to save new development in this file. In a perfect world, the Factory should not need to be updated when each new class is added.

--- framework/DataObjects/Factory.py
+++ framework/DataObjects/Factory.py
@@ -11,36 +11,58 @@ if not 'xrange' in dir(__builtins__):
   xrange = range
 #End compatibility block for Python 3-------------------------------------------
 
-from Data import Data
-from Point import Point
-from PointSet import PointSet
-from History import History
-from HistorySet import HistorySet
+################################################################################
+from DataObjects.Data import Data
+from DataObjects.Point import Point
+from DataObjects.PointSet import PointSet
+from DataObjects.History import History
+from DataObjects.HistorySet import HistorySet
+## [ Add new class here ]
+################################################################################
+## Alternatively, to fully automate this file:
+# from DataObjects import *
+################################################################################
 
 """
  Interface Dictionary (factory) (private)
 """
-# Make sure you include your new class in the imports above, as long as that is
-#  done, then this machinery will handle its appropriate generation for use
-#  by the factory
+# This machinery will automatically populate the "knownTypes" given the
+# imports defined above.
 __base = 'Data'
 __interFaceDict = {}
 
 for classObj in eval(__base).__subclasses__():
   __interFaceDict[classObj.__name__] = classObj
 
-__knownTypes = __interFaceDict.keys()
-
 def knownTypes():
-  return __knownTypes
+  """
+  Returns a list of strings that define the types of instantiable objects for
+  this base factory.
+  """
+  return __interFaceDict.keys()
 
 def returnInstance(Type,caller):
+  """
+  Attempts to create and return an instance of a particular type of object
+  available to this factory.
+  @ In, Type, string should be one of the knownTypes.
+  @ In, caller, the object requesting the instance
+                (used for error/debug messaging).
+  @ Out, subclass object constructed with no arguments
+  """
   try:
     return __interFaceDict[Type]()
   except KeyError:
     caller.raiseAnError(NameError,__name__+': unknown '+__base+' type '+Type)
 
 def returnClass(Type,caller):
+  """
+  Attempts to return a particular class type available to this factory.
+  @ In, Type, string should be one of the knownTypes.
+  @ In, caller, the object requesting the class
+                (used for error/debug messaging).
+  @ Out, reference to the subclass
+  """
   try:
     return __interFaceDict[Type]
   except KeyError:
