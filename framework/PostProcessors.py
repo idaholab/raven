b6c38743124aefdf16c82a0f4b39f3dcd55ef55a
Andrea Alfonsi <alfoa@falcon2.inel.gov>
2015-12-18 10:53:23 -0700

    added a small comment

--- framework/PostProcessors.py
+++ framework/PostProcessors.py
@@ -1533,6 +1533,7 @@ class BasicStatistics(BasePostProcessor):
       sumWeightsList, sumSquareWeightsList = np.zeros(featuresNumber, dtype = np.result_type(feature, np.float64)), np.zeros(featuresNumber, dtype = np.result_type(feature, np.float64))
       for myIndex in range(featuresNumber): sumWeightsList[myIndex], sumSquareWeightsList[myIndex] = np.sum(weights[myIndex][:]), np.sum(np.square(weights[myIndex][:]))
       diff = X - np.atleast_2d(np.average(X, axis = 1 - axis, weights = w)).T
+      # I personally think this approach could be correct. I did not cranch the equations since I di not have time so I am not 100% sure. Andrea
       factList = np.zeros(featuresNumber, dtype = np.result_type(feature, np.float64))
       if not self.biased: factList[:] = sumWeightsList[:] / ((sumWeightsList[:]*sumWeightsList[:] - sumSquareWeightsList[:]))
       else              : factList[:] = 1.0 / sumWeightsList[:]
