2cae5948f1dfd9e99d8e9039810ab90d454e9db2
Paul W. Talbot <talbotpne@gmail.com>
2016-01-04 14:12:52 -0700

    re-golded for updated external model variables

--- tests/framework/CodeInterfaceTests/GenericInterface/poly_inp.py
+++ tests/framework/CodeInterfaceTests/GenericInterface/poly_inp.py
@@ -1,30 +1,33 @@
 import numpy as np
+import os
 
-def eval(x,y):
+def eval(x,y,z):
   dat=[]
   c = 0
   for i in [0.3,0.5,0.7,1.0]:
     for j in [1.3,1.5,1.7,2.0]:
       c+=1
-      dat.append([c,i,j,x,y,(i-x)*(j-y)])
+      dat.append([c,i,j,x,y,z,(i-x)*(j-y)+z])
   return dat
 
 def run(xin,yin,out):
   inx = file(xin,'r')
   iny = file(yin,'r')
+  if not os.path.isfile('dummy.e'):
+    raise IOError('Missing dummy exodus file "dummy.e"!')
   for line in inx:
     if line.startswith('x ='):
       x=float(line.split('=')[1])
-      break
+    elif line.startswith('z ='):
+      z=float(line.split('=')[1])
   for line in iny:
     if line.startswith('y ='):
       y=float(line.split('=')[1])
-      break
 
-  dat = eval(x,y)
+  dat = eval(x,y,z)
 
   outf = file(out+'.csv','w')
-  outf.writelines('step,i,j,x,y,poly\n')
+  outf.writelines('step,i,j,x,y,z,poly\n')
   for e in dat:
     outf.writelines(','.join(str(i) for i in e)+'\n')
   outf.close()
