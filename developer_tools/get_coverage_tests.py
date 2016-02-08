12577b5dbb9fe7194a93cbf8d446aa6f8bf4cf7a
Paul W. Talbot <talbotpne@gmail.com>
2016-02-08 15:46:51 -0700

    improved options for conversion scripts included

--- developer_tools/get_coverage_tests.py
+++ developer_tools/get_coverage_tests.py
@@ -1,8 +1,14 @@
 import os
+import sys
+
+raven_dir = os.getcwd().split('raven')[0]+'raven'
+test_dir = os.path.join(raven_dir,'tests')
+
 skip_these = ['test_rom_trainer.xml','../../framework/TestDistributions.py']
 do_tests = []
 testsFilenames = []
-for root, dirs, files in os.walk("."):
+
+for root, dirs, files in os.walk(test_dir):
   for filename in files:
     if filename == "tests":
       testsFilenames.append((root,os.path.join(root, filename)))
@@ -11,6 +17,14 @@ for root,testFilename in testsFilenames:
   for line in testsFile:
     if line.strip().startswith('input'):
       newtest = line.split('=')[1].strip().strip("'")
-      if newtest not in skip_these: do_tests.append(os.path.join(root,newtest))
+      if newtest not in skip_these and newtest.endswith('.xml'): do_tests.append(os.path.join(root,newtest))
 
 print ' '.join(do_tests)
+
+def returnTests():
+  """
+    Provides a python way to obtain test list instead of screen print
+    @ In, None
+    @ Out, None
+  """
+  return do_tests
