d61e2f036028b407971b998c3bf2802326d85692
Congjian Wang <wangc@fn601604.fn.inl.gov>
2016-02-18 14:20:25 -0700

    fix typo

--- framework/Samplers.py
+++ framework/Samplers.py
@@ -296,7 +296,7 @@ class Sampler(utils.metaclass_insert(abc.ABCMeta,BaseType),Assembler):
       self.variables2distributionsMapping[key]['reducedDim'] = reducedDim  # the dimension of variable in the transformed space
       self.variables2distributionsMapping[key]['totDim'] = max(self.distributions2variablesIndexList[distName]) # We will reset the value if the node <variablesTransformation> exist in the raven input file
       if not self.variablesTransformationDict:
-        if self.variables2distributionsMapping[key]['totDim'] != len(self.distributions2variablesIndexList[distname]):
+        if self.variables2distributionsMapping[key]['totDim'] != len(self.distributions2variablesIndexList[distName]):
           self.raiseAnError(IOError,'The "dim" assigned to the variables insider Sampler are not correct! the "dim" should start from 1, and end with the full dimension of given distribution')
 
     #Checking the variables transformation
