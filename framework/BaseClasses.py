54792fb67e34d80ac6523d993b715e019e8b80d8
Talbot, Paul William <talbotpne@gmail.com>
2016-02-23 09:49:14 -0700

    Removed debug return comment

--- framework/BaseClasses.py
+++ framework/BaseClasses.py
@@ -56,7 +56,6 @@ class BaseType(MessageHandler.MessageUser):
         @ In, node, ET.Element, the node to search for replacement
         @ Out, None
       """
-      #return
       if node.text is not None and node.text.strip() != '':
         textEntries = list(t.strip() for t in node.text.split(','))
         for t,text in enumerate(textEntries):
