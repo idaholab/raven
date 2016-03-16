8f371ee98758395c7c29d708a125bd4275aad753
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2016-03-16 11:43:53 -0600

    Updating make_docs and the Makefiles to return errors if failures.

--- doc/make_docs.sh
+++ doc/make_docs.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+SCRIPT_NAME=`readlink $0`
+if test -x "$SCRIPT_NAME";
+then
+    SCRIPT_DIRNAME=`dirname $SCRIPT_NAME`
+else
+    SCRIPT_DIRNAME=`dirname $0`
+fi
+SCRIPT_DIR=`(cd $SCRIPT_DIRNAME; pwd)`
+cd $SCRIPT_DIR
+
 for DIR in user_manual qa_docs tests; do
-    (cd $DIR && make)
+    cd $DIR
+    if make; then
+        echo Successfully made docs in $DIR
+    else
+        echo Failed to make docs in $DIR
+        exit -1
+    fi
+    cd $SCRIPT_DIR
 done
 
+rm -Rvf pdfs
 mkdir pdfs
 for DOC in qa_docs/raven_sdd.pdf qa_docs/test_plan.pdf qa_docs/requirements.pdf  user_manual/raven_user_manual.pdf tests/analytic_tests.pdf; do
     cp $DOC pdfs/
