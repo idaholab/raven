090ae101fe7811aba72118927c21f7f5874b8871
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2016-04-10 21:36:28 -0600

    initial ensemble

--- framework/Samplers.py
+++ framework/Samplers.py
@@ -5152,13 +5152,218 @@ class AdaptiveSobol(Sobol,AdaptiveSparseGrid):
     for sub in self.useSet.keys():
       self._finalizeSubset(sub)
     AdaptiveSparseGrid._writeConvergencePoint(self,runPoint)
+#
+#
+#
+#
+#
+#
+#
+#
+class EnsembleForwardSampler(StaticSampler):
+  """
+    Ensemble Forward sampler. This sampler is aimed to combine Forward Sampling strategies
+  """
+  def __init__(self):
+    """
+      Default Constructor that will initialize member variables with reasonable
+      defaults or empty lists/dictionaries where applicable.
+      @ In, None
+      @ Out, None
+    """
+    StaticSampler.__init__(self)
+    self.acceptableSamplers   = ['MonteCarlo','Stratified','Grid','FactorialDesign','ResponseSurfaceDesign']
+    self.printTag             = 'SAMPLER EnsembleForward'
+    self.instanciatedSamplers = {}
+    self.listOfCombinationOfPoints = []
 
+  def localInputAndChecks(self,xmlNode):
+    """
+      Class specific xml inputs will be read here and checked for validity.
+      @ In, xmlNode, xml.etree.ElementTree.Element, The xml element node that will be checked against the available options specific to this Sampler.
+      @ Out, None
+    """
+    StaticSampler.readSamplerInit(self,xmlNode)
+    for child in xmlNode:
+      if child.tag in self.acceptableSamplers:
+        child.attrib['name'] = child.tag
+        self.instanciatedSamplers[child.tag] = returnInstance(child.tag,self)
+        #FIXME the variableGroups needs to be fixed
+        self.instanciatedSamplers[child.tag].readXML(child,self.messageHandler,variableGroups={},globalAttributes=self.globalAttributes)
+      elif child.tag in knownTypes():
+        self.raiseAnError(IOError,"Sampling strategy "+child.tag+" is not usable in "+self.type+" Sampler. Available are "+",".join(self.acceptableSamplers))
+      else:
+        self.raiseAnError(IOError,"XML node "+ child.tag + " unknown. Check the Manual!")
+  
+  def _localWhatDoINeed(self):
+    """
+      This method is a local mirror of the general whatDoINeed method.
+      It is implmented here because this Sampler requests special objects
+      @ In, None
+      @ Out, needDict, dict, dictionary of objects needed
+    """
+    needDict = StaticSampler._localWhatDoINeed(self)
+    for combSampler in self.instanciatedSamplers.values():
+      preneedDict = combSampler.whatDoINeed()
+      for key,value in preneedDict.items():
+        if key not in needDict.keys(): needDict[key] = []
+        needDict[key] = needDict[key] + value
+    return needDict 
 
-#
-#
-#
-#
+  def _localGenerateAssembler(self,initDict):
+    """
+      It is used for sending to the instanciated class, which is implementing the method, the objects that have been requested through "whatDoINeed" method
+      It is an abstract method -> It must be implemented in the derived class!
+      @ In, initDict, dict, dictionary ({'mainClassName(e.g., Databases):{specializedObjectName(e.g.,DatabaseForSystemCodeNamedWolf):ObjectInstance}'})
+      @ Out, None
+    """
+    availableDist = initDict['Distributions']
+    availableFunc = initDict['Functions']
+    for combSampler in self.instanciatedSamplers.values():
+      combSampler._generateDistributions(availableDist,availableFunc)
+    self.raiseADebug("Distributions initialized!")
+    
+  def localInitialize(self):
+    """
+      Initialize the EnsembleForwardSampler sampler. It call the localInitialize method of all the Samplers defined in this input
+      @ In, None
+      @ Out, None
+    """
+    self.limit = 1
+    samplersCombinations = []
+    cnt = 0
+    for samplingStrategy in self.instanciatedSamplers.keys():
+      self.instanciatedSamplers[samplingStrategy].initialize(externalSeeding=self.initSeed,solutionExport=None)
+      samplersCombinations.append([])
+      self.limit *= self.instanciatedSamplers[samplingStrategy].limit
+      while self.instanciatedSamplers[samplingStrategy].amIreadyToProvideAnInput():
+        self.instanciatedSamplers[samplingStrategy].counter +=1
+        self.instanciatedSamplers[samplingStrategy].localGenerateInput(None,None)
+        self.instanciatedSamplers[samplingStrategy].inputInfo['prefix'] = self.instanciatedSamplers[samplingStrategy].counter
+        samplersCombinations[cnt].append(copy.deepcopy(self.instanciatedSamplers[samplingStrategy].inputInfo))
+      cnt+=1
 
+    self.raiseAMessage('Number of Combined Samples are ' + str(self.limit) + '!')
+    combinations = list(itertools.product(*samplersCombinations))
+    print("a")
+    
+#     for nPoint in range(self.pointByVar-1): self.sampledCoordinate[nPoint]= [tempFillingCheck[mappingIdVarName[varName]][nPoint] for varName in self.axisName]
+#     if self.restartData:
+#       self.counter+=len(self.restartData)
+#       self.raiseAMessage('Number of points from restart: %i' %self.counter)
+#       self.raiseAMessage('Number of points needed:       %i' %(self.limit-self.counter))
+
+  def localGenerateInput(self,model,myInput):
+    """
+      Function to select the next most informative point for refining the limit
+      surface search.
+      After this method is called, the self.inputInfo should be ready to be sent
+      to the model
+      @ In, model, model instance, an instance of a model
+      @ In, myInput, list, a list of the original needed inputs for the model (e.g. list of files, etc.)
+      @ Out, None
+    """
+    varCount = 0
+    self.inputInfo['distributionName'] = {} #Used to determine which distribution to change if needed.
+    self.inputInfo['distributionType'] = {} #Used to determine which distribution type is used
+    weight = 1.0
+    for varName in self.axisName:
+      # new implementation for ND LHS
+      if not "<distribution>" in varName:
+        if self.variables2distributionsMapping[varName]['totDim']>1 and self.variables2distributionsMapping[varName]['reducedDim'] == 1:    # to avoid double count of weight for ND distribution; I need to count only one variable instaed of N
+          if self.variablesTransformationDict:
+            distName = self.variables2distributionsMapping[varName]['name']
+            for distVarName in self.distributions2variablesMapping[distName]:
+              for kkey in utils.first(distVarName.keys()).strip().split(','):
+                self.inputInfo['distributionName'][kkey] = self.toBeSampled[varName]
+                self.inputInfo['distributionType'][kkey] = self.distDict[varName].type
+            ndCoordinate = np.zeros(len(self.distributions2variablesMapping[distName]))
+            dxs = np.zeros(len(self.distributions2variablesMapping[distName]))
+            centerCoordinate = np.zeros(len(self.distributions2variablesMapping[distName]))
+            positionList = self.distributions2variablesIndexList[distName]
+            for var in self.distributions2variablesMapping[distName]:
+              # if the varName is a comma separated list of strings the user wants to sample the comma separated variables with the same sampled value => link the value to all comma separated variables
+              variable = utils.first(var.keys()).strip()
+              position = utils.first(var.values())
+              upper = self.gridEntity.returnShiftedCoordinate(self.gridEntity.returnIteratorIndexes(),{variable:self.sampledCoordinate[self.counter-1][varCount]+1})[variable]
+              lower = self.gridEntity.returnShiftedCoordinate(self.gridEntity.returnIteratorIndexes(),{variable:self.sampledCoordinate[self.counter-1][varCount]})[variable]
+              varCount += 1
+              if self.gridInfo[variable] == 'CDF':
+                coordinate = lower + (upper-lower)*Distributions.random()
+                ndCoordinate[positionList.index(position)] = self.distDict[variable].inverseMarginalDistribution(coordinate,variable)
+                dxs[positionList.index(position)] = self.distDict[variable].inverseMarginalDistribution(max(upper,lower),variable)-self.distDict[variable].inverseMarginalDistribution(min(upper,lower),variable)
+                centerCoordinate[positionList.index(position)] = (self.distDict[variable].inverseMarginalDistribution(upper,variable)+self.distDict[variable].inverseMarginalDistribution(lower,variable))/2.0
+                for kkey in variable.strip().split(','):
+                  self.values[kkey] = ndCoordinate[positionList.index(position)]
+                  self.inputInfo['upper'][kkey] = self.distDict[variable].inverseMarginalDistribution(max(upper,lower),variable)
+                  self.inputInfo['lower'][kkey] = self.distDict[variable].inverseMarginalDistribution(min(upper,lower),variable)
+              elif self.gridInfo[variable] == 'value':
+                dxs[positionList.index(position)] = max(upper,lower) - min(upper,lower)
+                centerCoordinate[positionList.index(position)] = (upper + lower)/2.0
+                coordinateCdf = self.distDict[variable].marginalCdf(lower) + (self.distDict[variable].marginalCdf(upper) - self.distDict[variable].marginalCdf(lower))*Distributions.random()
+                coordinate = self.distDict[variable].inverseMarginalDistribution(coordinateCdf,variable)
+                ndCoordinate[positionList.index(position)] = coordinate
+                for kkey in variable.strip().split(','):
+                  self.values[kkey] = coordinate
+                  self.inputInfo['upper'][kkey] = max(upper,lower)
+                  self.inputInfo['lower'][kkey] = min(upper,lower)
+            self.inputInfo['ProbabilityWeight-'+varName.replace(",","!")] = self.distDict[varName].cellIntegral(centerCoordinate,dxs)
+            weight *= self.inputInfo['ProbabilityWeight-'+varName.replace(",","!")]
+            self.inputInfo['SampledVarsPb'][varName] = self.distDict[varName].pdf(ndCoordinate)
+          else:
+            if self.gridInfo[varName] == 'CDF':
+              upper = self.gridEntity.returnShiftedCoordinate(self.gridEntity.returnIteratorIndexes(),{varName:self.sampledCoordinate[self.counter-1][varCount]+1})[varName]
+              lower = self.gridEntity.returnShiftedCoordinate(self.gridEntity.returnIteratorIndexes(),{varName:self.sampledCoordinate[self.counter-1][varCount]})[varName]
+              varCount += 1
+              coordinate = lower + (upper-lower)*Distributions.random()
+              gridCoordinate, distName =  self.distDict[varName].ppf(coordinate), self.variables2distributionsMapping[varName]['name']
+              for distVarName in self.distributions2variablesMapping[distName]:
+                for kkey in utils.first(distVarName.keys()).strip().split(','):
+                  self.inputInfo['distributionName'][kkey], self.inputInfo['distributionType'][kkey], self.values[kkey] = self.toBeSampled[varName], self.distDict[varName].type, np.atleast_1d(gridCoordinate)[distVarName.values()[0]-1]
+              # coordinate stores the cdf values, we need to compute the pdf for SampledVarsPb
+              self.inputInfo['SampledVarsPb'][varName] = self.distDict[varName].pdf(np.atleast_1d(gridCoordinate).tolist())
+              weight *= max(upper,lower) - min(upper,lower)
+              self.inputInfo['ProbabilityWeight-'+varName.replace(",","!")] = max(upper,lower) - min(upper,lower)
+            else: self.raiseAnError(IOError,"Since the globalGrid is defined, the Stratified Sampler is only working when the sampling is performed on a grid on a CDF. However, the user specifies the grid on " + self.gridInfo[varName])
+      if ("<distribution>" in varName) or self.variables2distributionsMapping[varName]['totDim']==1:   # 1D variable
+        # if the varName is a comma separated list of strings the user wants to sample the comma separated variables with the same sampled value => link the value to all comma separated variables
+        upper = self.gridEntity.returnShiftedCoordinate(self.gridEntity.returnIteratorIndexes(),{varName:self.sampledCoordinate[self.counter-1][varCount]+1})[varName]
+        lower = self.gridEntity.returnShiftedCoordinate(self.gridEntity.returnIteratorIndexes(),{varName:self.sampledCoordinate[self.counter-1][varCount]})[varName]
+        varCount += 1
+        if self.gridInfo[varName] =='CDF':
+          coordinate = lower + (upper-lower)*Distributions.random()
+          ppfValue = self.distDict[varName].ppf(coordinate)
+          ppfLower = self.distDict[varName].ppf(min(upper,lower))
+          ppfUpper = self.distDict[varName].ppf(max(upper,lower))
+          weight *= self.distDict[varName].cdf(ppfUpper) - self.distDict[varName].cdf(ppfLower)
+          self.inputInfo['ProbabilityWeight-'+varName.replace(",","-")] = self.distDict[varName].cdf(ppfUpper) - self.distDict[varName].cdf(ppfLower)
+          self.inputInfo['SampledVarsPb'][varName]  = self.distDict[varName].pdf(ppfValue)
+        elif self.gridInfo[varName] == 'value':
+          coordinateCdf = self.distDict[varName].cdf(min(upper,lower)) + (self.distDict[varName].cdf(max(upper,lower))-self.distDict[varName].cdf(min(upper,lower)))*Distributions.random()
+          if coordinateCdf == 0.0: self.raiseAWarning(IOError,"The grid lower bound and upper bound in value will generate ZERO cdf value!!!")
+          coordinate = self.distDict[varName].ppf(coordinateCdf)
+          weight *= self.distDict[varName].cdf(max(upper,lower)) - self.distDict[varName].cdf(min(upper,lower))
+          self.inputInfo['ProbabilityWeight-'+varName.replace(",","-")] = self.distDict[varName].cdf(max(upper,lower)) - self.distDict[varName].cdf(min(upper,lower))
+          self.inputInfo['SampledVarsPb'][varName] = self.distDict[varName].pdf(coordinate)
+        for kkey in varName.strip().split(','):
+          self.inputInfo['distributionName'][kkey] = self.toBeSampled[varName]
+          self.inputInfo['distributionType'][kkey] = self.distDict[varName].type
+          if self.gridInfo[varName] =='CDF':
+            self.values[kkey] = ppfValue
+            self.inputInfo['upper'][kkey] = ppfUpper
+            self.inputInfo['lower'][kkey] = ppfLower
+          elif self.gridInfo[varName] =='value':
+            self.values[kkey] = coordinate
+            self.inputInfo['upper'][kkey] = max(upper,lower)
+            self.inputInfo['lower'][kkey] = min(upper,lower)
+
+    self.inputInfo['PointProbability'] = reduce(mul, self.inputInfo['SampledVarsPb'].values())
+    self.inputInfo['ProbabilityWeight' ] = weight
+    self.inputInfo['SamplerType'] = 'Stratified'
+#
+#
+#
+#
 """
  Interface Dictionary (factory) (private)
 """
@@ -5176,6 +5381,7 @@ __interFaceDict['SparseGridCollocation'   ] = SparseGridCollocation
 __interFaceDict['AdaptiveSparseGrid'      ] = AdaptiveSparseGrid
 __interFaceDict['Sobol'                   ] = Sobol
 __interFaceDict['AdaptiveSobol'           ] = AdaptiveSobol
+__interFaceDict['EnsembleForward'         ] = EnsembleForwardSampler
 __knownTypes = list(__interFaceDict.keys())
 
 def knownTypes():
