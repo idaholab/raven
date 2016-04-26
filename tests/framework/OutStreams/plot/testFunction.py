88e4aa218e59e48fa1554bbbb343e2e673d40dec
Dan Maljovec <maljovec@cs.utah.edu>
2016-04-26 11:32:50 -0600

    Moving IO files into an Outstreams directory.

---
+++ tests/framework/OutStreams/plot/testFunction.py
@@ -0,0 +1,14 @@
+'''
+Created on 4/26/16
+
+@author: maljdan
+'''
+import numpy as np
+import math
+
+def eval(inp):
+  retVal = math.exp(- ((inp[0] - .5)**2 + (inp[1] - .5)**2)/.125)
+  return float('%.8f' % retVal)
+
+def run(self,Input):
+  self.Z = eval(((self.X),(self.Y-1)))
