95e4199239e658e7fd334e99861111c941141d5b
Paul W. Talbot <talbotpne@gmail.com>
2016-04-26 11:09:58 -0600

    hdf5 fix, but validate xml failing

---
+++ scripts/conversionScripts/hdf5_readwrite.py
@@ -0,0 +1,32 @@
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as pxml
+import os
+
+def convert(tree,fileName=None):
+  """
+    Converts input files to be compatible with merge request ???.
+    Changes list of <variable> nodes to <variables> nodes.
+    @ In, tree, xml.etree.ElementTree.ElementTree object, the contents of a RAVEN input file
+    @ In, fileName, the name for the raven input file
+    @Out, tree, xml.etree.ElementTree.ElementTree object, the modified RAVEN input file
+  """
+  simulation = tree.getroot()
+  if simulation.tag!='Simulation' and simulation.tag!='ExternalModel': return tree #this isn't an input file
+  dbNodes = None
+  if simulation.tag=='Simulation':
+    dbNodes = simulation.find('Databases')
+  elif simulation.tag=='Databases': #externalNode case
+    dbNodes = simulation
+  if dbNodes is not None:
+    for dbNode in dbNodes:
+      if 'filename' in dbNode.attrib.keys() and 'readMode' not in dbNode.attrib.keys():
+        dbNode.attrib['readMode'] = 'read'
+      else:
+        dbNode.attrib['readMode'] = 'overwrite'
+  return tree
+
+
+if __name__=='__main__':
+  import convert_utils
+  import sys
+  convert_utils.standardMain(sys.argv,convert)
