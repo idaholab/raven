9e1e68358b7a6a7e708c38f06bf283cbf9d69c9a
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2016-10-09 16:52:01 -0600

    added Workshop material

---
+++ Workshop/IRUG2016_workshop/inputs/goalFunctionTest.py
@@ -0,0 +1,14 @@
+'''
+Created on NO NEED TO KNOW
+
+@author: alfoa
+'''
+import numpy
+import math
+
+def __residuumSign(self):
+  if self.cntrlvar_67 >= self.failureTemp: 
+    return -1
+  else: 
+    return 1
+
