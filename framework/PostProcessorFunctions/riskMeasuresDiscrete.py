18648b981311a5b545ff2edf23c1a74f4f4f4e6b
mandd <mandd@inl603893.inl.gov>
2016-11-03 13:23:50 -0600

    edits

--- framework/PostProcessorFunctions/riskMeasuresDiscrete.py
+++ framework/PostProcessorFunctions/riskMeasuresDiscrete.py
@@ -10,7 +10,10 @@ if not 'xrange' in dir(__builtins__):
   xrange = range
 #End compatibility block for Python 3----------------------------------------------------------------
 
+#External Modules------------------------------------------------------------------------------------
 import numpy as np
+import copy
+#External Modules End--------------------------------------------------------------------------------
 
 from PostProcessorInterfaceBaseClass import PostProcessorInterfaceBase
 
@@ -46,7 +49,7 @@ class RiskMeasuresDiscrete(PostProcessorInterfaceBase):
       elif child.tag == 'variables':
         self.variables = child.text.split(',')
       elif child.tag == 'target':
-        self.target = child.text.split(',')
+        self.target = child.text
       elif child.tag !='method':
         self.raiseAnError(IOError, 'RiskMeasuresDiscrete Interfaced Post-Processor ' + str(self.name) + ' : XML node ' 
                           + str(child) + ' is not recognized')
@@ -62,35 +65,91 @@ class RiskMeasuresDiscrete(PostProcessorInterfaceBase):
      @ Out, outputDic, dict, dictionary which contains the risk measures
 
     """
-    #print(inputDic['data']['input'])
-    print(inputDic['metadata'])
-    
-    if 'metadata' in inputDic.keys():
-      pbPresent = 'ProbabilityWeight' in inputDic['metadata'][0].keys() if 'metadata' in inputDic.keys() else False
+        
+    if 'metadata' in inputDic.keys():     
+      if 'ProbabilityWeight' in inputDic['metadata'][0].keys():
+        pbPresent = True
+      else:
+        pbPresent = False
+    else:
+      pbPresent = False
+
     if not pbPresent:
-      pbWeights['realization'] = np.asarray([1.0 / len(inputDic['targets'][self.parameters['targets'][0]])]*len(inputDic['targets'][self.parameters['targets'][0]]))
+      pbWeights= np.asarray([1.0 / len(inputDic['targets'][self.parameters['targets'][0]])]*len(inputDic['targets'][self.parameters['targets'][0]]))
     else: 
-      pbWeights['realization'] = inputDic['metadata']['ProbabilityWeight']/np.sum(inputDic['metadata']['ProbabilityWeight'])
-    print(pbWeights['realization'])
+      pbWeights = inputDic['metadata'][0]['ProbabilityWeight']/np.sum(inputDic['metadata'][0]['ProbabilityWeight'])
+
+    N = pbWeights.size
+
+    # 1.0 = failure
+    # 0.0 = OK
+    
+    outputDic = {}
+    outputDic['data'] = {}
+    outputDic['data']['output'] = {}
+    outputDic['data']['input']  = {}
+    
+    print('======= Risk Measures =============')
+
+    
     for variable in self.variables:
       R0     = 0.0
       Rminus = 0.0
       Rplus  = 0.0
+
+      data=np.zeros((3,N))
+      data[0] = pbWeights
+      data[1] = inputDic['data']['output'][variable]
+      data[2] = inputDic['data']['output'][self.target]
       
       ''' Calculate R0, Rminus, Rplus '''
-      R0 = np.inner()
+      minusValues = np.argwhere(data[1,:] == 0.0)
+      plusValues  = np.argwhere(data[1,:] == 1.0)
+      
+      dataMinus = np.delete(data, minusValues, axis=1)
+      dataPlus  = np.delete(data, plusValues , axis=1)
+      
+      indexMinus = np.argwhere(dataMinus[2,:] == 1.0)
+      indexPlus  = np.argwhere(dataPlus[2,:]  == 1.0)
+      indexZero  = np.argwhere(data[2,:]      == 1.0)
+      
+      dataReducedMinus = np.delete(dataMinus, indexMinus, axis=1)
+      dataReducedPlus  = np.delete(dataPlus,  indexMinus, axis=1)
+      dataReducedZero  = np.delete(data,      indexZero,  axis=1)
+      
+      Rminus = np.sum(dataReducedMinus[0,:])
+      Rplus  = np.sum(dataReducedPlus[0,:])
+      R0     = np.sum(dataReducedZero[0,:])
+      
+      print('--> ' + str(variable) + 'Rminus = ' + str(Rminus))
+      print('--> ' + str(variable) + 'Rplus  = ' + str(Rplus))
+      print('--> ' + str(variable) + 'R0     = ' + str(R0))
       
       ''' Calculate RRW, RAW, FV, B '''
       RRW = R0/Rminus
       RAW = Rplus/R0
       FV  = (R0-Rminus)/R0
       B   = Rplus + Rminus
+            
+      if 'RRW' in self.measures:
+        outputDic['data']['output'][variable + '_RRW'] = np.asanyarray([RRW])
+        print('--> ' + str(variable) + 'RRW = ' + str(RRW))
+      if 'RAW' in self.measures:
+        outputDic['data']['output'][variable + '_RAW'] = np.asanyarray([RAW])
+        print('--> ' + str(variable) + 'RAW = ' + str(RAW))
+      if 'FV' in self.measures:
+        outputDic['data']['output'][variable + '_FV']  = np.asanyarray([FV])
+        print('--> ' + str(variable) + 'FV = ' + str(FV))
+      if 'B' in self.measures:
+        outputDic['data']['output'][variable + '_B']   = np.asanyarray([B])
+        print('--> ' + str(variable) + 'B  = ' + str(B))
       
-      outputDic[variable + '_RRW'] = RRW
-      outputDic[variable + '_RAW'] = RAW
-      outputDic[variable + '_FV']  = FV
-      outputDic[variable + '_B']   = B
+      outputDic['data']['input'][variable + '_avg'] = np.asanyarray([np.sum(dataMinus[0,:])])
       
+      
+   
+    outputDic['metadata'] = copy.deepcopy(inputDic['metadata'])   
+   
     return outputDic
 
 
