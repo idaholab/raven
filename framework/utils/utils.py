75c3d4113cd739567dbea47e13b9e6266e0c7f49
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2016-12-04 21:31:56 -0700

    modified bison test

--- framework/utils/utils.py
+++ framework/utils/utils.py
@@ -818,6 +818,6 @@ def sizeMatch(var,sizeToCheck):
     @ Out, sizeMatched, bool, is the size ok?
   """
   sizeMatched = True
-  if len(np.atleast_1d(var)) != sizeToCheck: sizeMatched = False
+  if len(numpy.atleast_1d(var)) != sizeToCheck: sizeMatched = False
   return sizeMatched
 
