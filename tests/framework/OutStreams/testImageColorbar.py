ad331deedd6fa97a743e0a9271b15ff73caa61a1
Dan Maljovec <maljovec@cs.utah.edu>
2017-01-22 22:56:08 -0700

    Updating the test descriptions for two outstream tests.

--- tests/framework/OutStreams/testImageColorbar.py
+++ tests/framework/OutStreams/testImageColorbar.py
@@ -2,6 +2,26 @@
 Created on 8/1/16
 
 @author: maljdan
+
+<TestInfo>
+  <name>framework.colorbar_plot</name>
+  <author>maljdan</author>
+  <created>2016-08-01</created>
+  <classesTested> </classesTested>
+  <description>
+     This test the online generation of plots (colorbar plot).
+     It can not be considered part of the active code but of the regression test
+     system. This file is a near identical copy of testImageGeneration.py and at
+     some point, we should see if the interface can be generalized to handle
+     image file diffs more generically. Right now, the number of image file
+     tests is small, but in order to be scalable, we need a better solution.
+  </description>
+  <revisions>
+    <revision author="maljdan" date="2016-05-04">Fixing the test for the compare executable to test the gold image against itself, if this returns a non-zero code, then the version of imageMagick cannot be used to get a valid difference. Also, I am removing the difference image and instead doing null: to remove the output file when using compare.</revision>
+    <revision author="alfoa" date="2017-01-21">Adding this test description.</revision>
+  </revisions>
+</TestInfo>
+
 (possibly promote to a diff class at some point, but relies on an external
  application, namely ImageMagick) This test was created in reference to issue
 #639
