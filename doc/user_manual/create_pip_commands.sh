9ee9941ab8a4251b3dcb81f1db9fd0e62a10425f
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2017-03-08 16:05:21 -0700

    Adding description of using PIP.

---
+++ doc/user_manual/create_pip_commands.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat > pip_commands.txt <<EOF
+#Create a new virtual environment in directory raven_libs
+virtualenv raven_libs
+#Use the virtual environment
+source raven_libs/bin/activate
+#Use pip to install needed libraries
+EOF
+python ../../scripts/TestHarness/testers/RavenUtils.py --pip-install | fold -s -w60 | sed 's/$/\\/' | python -c 'import sys; sys.stdout.write(sys.stdin.read()[:-2])'>> pip_commands.txt
+
