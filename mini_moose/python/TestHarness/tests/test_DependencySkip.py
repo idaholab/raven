56ec55b0461fa165f2f2c11943aa7c4e18f02f77
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2017-04-05 15:51:57 -0600

    The tests were causing the rest of the tests to fail.

--- mini_moose/python/TestHarness/tests/test_DependencySkip.py
+++
@@ -1,9 +0,0 @@
-from TestHarnessTestCase import TestHarnessTestCase
-class TestHarnessTester(TestHarnessTestCase):
-    def testDependencySkip(self):
-        """
-        Test skipping a test if its prereq is also skipped
-        """
-        output = self.runTests('-i', 'depend_skip_tests')
-        self.assertIn('skipped (always skipped)', output)
-        self.assertIn('skipped (skipped dependency)', output)
