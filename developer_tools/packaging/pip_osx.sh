4c4505a5f74d30ec7ab1f7cf4153529cad1cb2e8
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2017-04-12 16:25:48 -0600

    Build Python.

--- developer_tools/packaging/pip_osx.sh
+++ developer_tools/packaging/pip_osx.sh
@@ -5,12 +5,21 @@ BUILD_DIR="$HOME/build_raven_libs"
 INSTALL_DIR="$HOME/raven_libs"
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
-curl -O -- https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
-PATH="$PATH:$HOME/Library/Python/2.7/bin"
-pip install virtualenv --user
-virtualenv $INSTALL_DIR
-source $INSTALL_DIR/bin/activate
+curl -O -L -- https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
+tar -xvzf Python-2.7.13.tgz
+cd Python-2.7.13/
+./configure --prefix="$INSTALL_DIR"
+make
+make install
+
+PATH="$INSTALL_DIR/bin:$PATH"
+which python
+
+python -m ensurepip
+cd $BUILD_DIR
+pip install virtualenv
+#virtualenv $INSTALL_DIR
+#source $INSTALL_DIR/bin/activate
 pip install numpy==1.11.0 h5py==2.6.0 scipy==0.17.1 scikit-learn==0.17.1 matplotlib==1.5.1
 
 
