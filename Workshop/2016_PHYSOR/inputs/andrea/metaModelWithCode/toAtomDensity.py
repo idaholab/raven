560f3c6a795ca56811f11aea172c566b623f1ad0
mandd <diego.mandelli@inl.gov>
2017-05-08 14:17:03 -0600

    deleted old files, moved beamer files, add inputStrcuture presentation

--- Workshop/2016_PHYSOR/inputs/andrea/metaModelWithCode/toAtomDensity.py
+++
@@ -1,6 +0,0 @@
-
-def run(self, Input):
-  self.densA = self.A*1000.0*6.02214199E+23/235.0
-  self.densB = self.B*1000.0*6.02214199E+23/238.0
-  self.densC = self.C*1000.0*6.02214199E+23/150.0
-  self.densD = self.D*1000.0*6.02214199E+23/100.0
