b5b22570c3f70bfa661148b36e5569ac4df52a80
Paul W. Talbot <paul.talbot@inl.gov>
2017-05-17 11:09:00 -0600

    update based on comments

--- scripts/establish_conda_env.sh
+++ scripts/establish_conda_env.sh
@@ -11,15 +11,15 @@ conda_install_or_create ()
     `python $SCRIPT_DIR/TestHarness/testers/RavenUtils.py --conda-install`
   else
     echo ... No RAVEN environment located, creating it ...
-    try_using_raven_conda
+    try_using_raven_environment
     `python $SCRIPT_DIR/TestHarness/testers/RavenUtils.py --conda-create`
   fi
 }
 
 try_using_raven_environment ()
 {
-    #First check home directory for a script, then /opt/raven_libs
-    if test -e $HOME/.raven/environments/raven_libs_profile;
+  #First check home directory for a script, then /opt/raven_libs
+  if test -e $HOME/.raven/environments/raven_libs_profile;
     then
   source $HOME/.raven/environments/raven_libs_profile
     else
@@ -36,7 +36,7 @@ then
   conda_install_or_create
 else
   echo conda not initially located, checking for RAVEN conda ...
-  try_using_raven_conda
+  try_using_raven_environment
   if which conda 2> /dev/null;
   then
     echo conda located, checking environments ...
