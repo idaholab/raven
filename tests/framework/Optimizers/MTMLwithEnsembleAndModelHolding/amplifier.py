81b0fa0f99d29a1853baa297788a7e04f3d0dbcd
alfoa <andrea.alfonsi@inl.gov>
2017-07-26 13:56:36 -0600

    added test

---
+++ tests/framework/Optimizers/MTMLwithEnsembleAndModelHolding/amplifier.py
@@ -0,0 +1,9 @@
+# takes input parameters ans and z
+# returns value in "objAns" amplifying the self.ans times (self.z+1.0)
+
+def run(self,Inputs):
+
+  self.objAns = self.ans*(self.z+1.0)
+  print(self.ans,self.z,self.objAns)
+
+
