fixed gradient

tests/framework/Optimizers/Stochastic/stoch_parabola.py
@@ -17,7 +17,9 @@ def random(scale=0.5,loc=-1.0):
   return scale*(2.*np.random.rand()+loc)
 
 def evaluate(values):
-  return base(values) + random()
+  ran = random()/10.0
+  print(ran)
+  return base(values) + ran
 
 def run(self,Input):
   """