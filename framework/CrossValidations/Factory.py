73a03ce948a7ab32b7f6c86c962ccd8efb4ea635
Congjian Wang <congjian.wang@inl.gov>
2017-09-05 14:06:33 -0600

    initial implementation of cross validation

---
+++ framework/CrossValidations/Factory.py
@@ -0,0 +1,80 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Created on Jul 18 2016
+
+@author: mandd
+"""
+#for future compatibility with Python 3-----------------------------------------
+from __future__ import division, print_function, unicode_literals, absolute_import
+import warnings
+warnings.simplefilter('default',DeprecationWarning)
+if not 'xrange' in dir(__builtins__):
+  xrange = range
+#End compatibility block for Python 3-------------------------------------------
+
+################################################################################
+from utils import utils
+from .CrossValidation import CrossValidation
+from .SklCrossValidation import SciKitLearn
+## [ Add new class here ]
+################################################################################
+## Alternatively, to fully automate this file:
+# from OutStreamManagers import *
+################################################################################
+
+"""
+  Interface Dictionary (factory) (private)
+"""
+# This machinery will automatically populate the "knownTypes" given the
+# imports defined above.
+__base = 'CrossValidation'
+__interFaceDict = {}
+
+for classObj in utils.getAllSubclasses(eval(__base)):
+  __interFaceDict[classObj.__name__] = classObj
+
+def knownTypes():
+  """
+    Returns a list of strings that define the types of instantiable objects for
+    this base factory.
+    @ In, None
+    @ Out, knownTypes, list, list of known types
+  """
+  return __interFaceDict.keys()
+
+def returnInstance(Type,caller):
+  """
+    Attempts to create and return an instance of a particular type of object
+    available to this factory.
+    @ In, Type, string, string should be one of the knownTypes.
+    @ In, caller, instance, the object requesting the instance (used for error/debug messaging).
+    @ Out, returnInstance, instance, instance of OutStreamManager subclass, a subclass object constructed with no arguments
+  """
+  try:
+    return __interFaceDict[Type]()
+  except KeyError:
+    caller.raiseAnError(NameError,__name__+': unknown '+__base+' type '+Type)
+
+def returnClass(Type,caller):
+  """
+    Attempts to return a particular class type available to this factory.
+    @ In, Type, string, string should be one of the knownTypes.
+    @ In, caller, instance, the object requesting the class (used for error/debug messaging).
+    @ Out, returnClass, class, reference to the subclass
+  """
+  try:
+    return __interFaceDict[Type]
+  except KeyError:
+    caller.raiseAnError(NameError,__name__+': unknown '+__base+' type '+Type)
---
+++ framework/CrossValidations/SklCrossValidation.py
@@ -0,0 +1,106 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Created on August 2017
+
+@author: wangc
+"""
+#for future compatibility with Python 3--------------------------------------------------------------
+from __future__ import division, print_function, unicode_literals, absolute_import
+import warnings
+warnings.simplefilter('default',DeprecationWarning)
+#End compatibility block for Python 3----------------------------------------------------------------
+
+#External Modules------------------------------------------------------------------------------------
+import numpy as np
+from utils import utils
+if int(sklearn.__version__.split(".")[1]) > 17:
+  from sklearn import model_selection as cross_validation
+else:
+  from sklearn import cross_validation
+#External Modules End--------------------------------------------------------------------------------
+
+#Internal Modules------------------------------------------------------------------------------------
+from .CrossValidation import CrossValidation
+#Internal Modules End--------------------------------------------------------------------------------
+
+class SciKitLearn(CrossValidation):
+  """
+    SciKitLearn inteface for Cross validation methods
+  """
+  availImpl = {}  # dictionary of available cross validation methods {mainClass:(classPointer, output Type (float))}
+  availImpl['KFold'                  ] = (cross_validation.KFold,                  'float')
+  availImpl['StratifiedKFold'        ] = (cross_validation.StratifiedKFold,        'float')
+  availImpl['LeaveOneOut'            ] = (cross_validation.LeaveOneOut,            'float')
+  availImpl['LeavePOut'              ] = (cross_validation.LeavePOut,              'float')
+  availImpl['LeaveOneLabelOut'       ] = (cross_validation.LeaveOneLabelOut,       'float')
+  availImpl['LeavePLabelOut'         ] = (cross_validation.LeavePLabelOut,         'float')
+  availImpl['ShuffleSplit'           ] = (cross_validation.ShuffleSplit,           'float')
+  availImpl['LabelKFold'             ] = (cross_validation.LabelKFold,             'float')
+  availImpl['LabelShuffleSplit'      ] = (cross_validation.LabelShuffleSplit,      'float')
+  availImpl['StratifiedShuffleSplit' ] = (cross_validation.StratifiedShuffleSplit, 'float')
+
+  def __init__(self, messageHandler, **kwargs):
+    """
+      Constructor for SciKitLearn class
+      @ In, messageHandler, MessageHandler, Message handler object
+      @ In, kwargs, dict, arguments for the SciKitLearn
+      @ Out, None
+    """
+    CrossValidation.__init__(self, messageHandler, **kwargs)
+    self.printTag = 'SciKitLearn Cross Validation'
+
+    if 'SKLtype' not in self.initOptionDict.keys():
+      self.raiseAnError(IOError, 'Missing XML node: Cross validation from SciKitLearn requires keyword "SKLtype"')
+    self.SKLType = self.initOptionDict['SKLtype']
+    self.initOptionDict.pop('SKLtype')
+
+    if self.SKLType not in self.__class__.availImpl.keys():
+      self.raiseAnError(IOError, 'Unknow SKLtype ', self.SKLType, ' from cross validation ', self.name)
+
+    self.__class__.returnType = self.__class__.availImpl[self.SKLType][1]
+
+    for key, value in self.initOptionDict.items():
+      try:
+        self.initOptionDict[key] = ast.literal_eval(value)
+      except:
+        pass
+
+    self.CVInstance = self.__class__.availImpl[self.SKLType][0](**self.initOptionDict)
+    self.outputDict = {}
+
+  def reset(self):
+    """
+      Used to reset private variables
+      @ In, None
+      @ Out, None
+    """
+    self.CVInstance = None
+
+  def getCrossValidationType(self):
+    """
+      This method is used to return the type of cross validation to be employed
+      @ In, None
+      @ Out, None
+    """
+    return self.SKLType
+
+  def generateTrainTestIndices(self):
+    """
+      generate train/test indices
+      @ In, None
+      @ Out, Object, instance of cross validation
+    """
+    return self.CVInstance
+
