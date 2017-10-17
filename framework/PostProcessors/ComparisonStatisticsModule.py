c6b9dfd479f786111369efd2aec849855d8a9684
Andrea Alfonsi - INL <andrea.alfonsi@inl.gov>
2017-10-16 23:01:53 -0600

    Update ComparisonStatisticsModule.py

--- framework/PostProcessors/ComparisonStatisticsModule.py
+++ framework/PostProcessors/ComparisonStatisticsModule.py
@@ -183,7 +183,7 @@ def __processData(data, methodInfo):
                                 (abs(skewness) ** (2.0 / 3.0) + ((4.0 - math.pi) / 2.0) ** (2.0 / 3.0)))
   delta       = delta_func(skewness)
   # see https://en.wikipedia.org/wiki/Skew_normal_distribution (Estimation)
-  delta_alpha = delta_func(min(0.9952717,ret["skewness"]))
+  delta_alpha = delta_func(min(0.9952717,skewness))
   delta       = math.copysign(delta, skewness)
   delta_alpha = math.copysign(delta_alpha, skewness)
   alpha       = delta_alpha / math.sqrt(1.0 - delta_alpha ** 2)
