8ca9f54875a79177f484320577b824af63703e4b
Paul Talbot <paul.talbot@inl.gov>
2017-12-04 12:22:26 -0700

    pylint problems addressed (#451)
    * pylint problems addressed
    
    * Update TestXPointSet.py
    
    * malformed comment blocks in 2 tests
    
    * new unordered CSV differ, tested on test_Lorentz

--- framework/DataObjects/XDataObject.py
+++ framework/DataObjects/XDataObject.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""
+  Base class for both in-memory and in-disk data structures.
+"""
 #For future compatibility with Python 3
 from __future__ import division, print_function, unicode_literals, absolute_import
 import warnings
