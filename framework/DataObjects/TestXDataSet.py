37adc191c6c1f3a21d758ce914f644f88b1c8362
Diego Mandelli <diego.mandelli@inl.gov>
2017-12-07 19:24:12 -0700

    Mods for new dataObject (#463)
    * edits
    
    * edits
    
    * comments review-1
    
    * edits
    
    * asDictionary works
    
    * added full loop cycle test

--- framework/DataObjects/TestXDataSet.py
+++ framework/DataObjects/TestXDataSet.py
@@ -716,6 +716,34 @@ checkArray('load from dict "b"[3]',data.asDataset().isel(True,RAVEN_sample_ID=3)
 rlz = data.realization(index=2)
 checkFloat('load from dict rlz 2 "a"',rlz['a'],1.2)
 checkArray('load from dict rlz 2 "b"',rlz['b'].values,[1.2,1.21,1.22],float)
+# Here I am testing the functionality that converts the dataObject into a dict
+convertedDict = data.asDataset(outType='dict')
+# check that the dictionary entries are the same
+checkArray('asDict "a"',seed['a'],convertedDict['data']['a'],float)
+checkArray('asDict "b[0]"',convertedDict['data']['b'][0],seed['b'][0],float)
+checkArray('asDict "b[4]"',convertedDict['data']['b'][4],seed['b'][4],float)
+checkArray('asDict "b[9]"',convertedDict['data']['b'][9],seed['b'][9],float)
+checkArray('asDict "t[0]"',convertedDict['data']['t'][0],seed['t'][0],float)
+checkArray('asDict "t[4]"',convertedDict['data']['t'][4],seed['t'][4],float)
+checkArray('asDict "t[9]"',convertedDict['data']['t'][9],seed['t'][9],float)
+checkSame('asDict dims "a"',convertedDict['dims']['a'],[])
+checkSame('asDict dims "b"',convertedDict['dims']['b'],['t'])
+# TODO check metadata?
+# double-check there's no errors using this to construct a new dataset (full loop)
+xml = createElement('DataSet',attrib={'name':'test'})
+xml.append(createElement('Input',text='a'))
+xml.append(createElement('Output',text='b'))
+xml.append(createElement('Index',attrib={'var':'t'},text='b'))
+dataRe = XDataSet.DataSet()
+dataRe.messageHandler = mh
+dataRe._readMoreXML(xml)
+dataRe.load(convertedDict['data'],style='dict',dims=convertedDict['dims'])
+# use exact same tests as originally loading from dict, but for dataRe
+checkArray('load from dict "a"',dataRe.asDataset()['a'].values,seed['a'],float)
+checkArray('load from dict "b"[3]',dataRe.asDataset().isel(True,RAVEN_sample_ID=3)['b'].dropna('t').values,seed['b'][3],float)
+rlz = dataRe.realization(index=2)
+checkFloat('load from dict rlz 2 "a"',rlz['a'],1.2)
+checkArray('load from dict rlz 2 "b"',rlz['b'].values,[1.2,1.21,1.22],float)
 ######################################
 #        REMOVING VARIABLES          #
 ######################################
