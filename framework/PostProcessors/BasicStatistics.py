bdb2cefb3a78eb2ef3d464978552696def7bbf5d
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2017-12-14 16:38:19 -0700

    Fixed LimitSurface Postprocessors, added conversion script, added documentation

--- framework/PostProcessors/BasicStatistics.py
+++ framework/PostProcessors/BasicStatistics.py
@@ -169,7 +169,7 @@ class BasicStatistics(PostProcessor):
     if type(currentInput).__name__ =='dict':
       if 'targets' not in currentInput.keys() and 'timeDepData' not in currentInput.keys():
         self.raiseAnError(IOError, 'Did not find targets or timeDepData in input dictionary')
-      return currentInput
+      return [currentInput]
     if currentInput.type not in ['PointSet','HistorySet']:
       self.raiseAnError(IOError, self, 'BasicStatistics postprocessor accepts PointSet and HistorySet only! Got ' + currentInput.type)
 
