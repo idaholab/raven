537fd291854f3e6d0b0cce9a4d7cfbb764662c1f
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2017-12-16 12:57:02 -0700

    fixed reading from CSV of HistorySet (correctly considering the pivotParameter)

--- framework/DataObjects/XHistorySet.py
+++ framework/DataObjects/XHistorySet.py
@@ -155,13 +155,10 @@ class HistorySet(DataSet):
     for i,sub in enumerate(subFiles):
       # first time create structures
       subDat = pd.read_csv(sub)
-      if set(self.indexes) not in set(self._outputs):
-        if 'time' in self.indexes:
-          self.raiseAnError(IOError,'Importing HistorySet from .csv: the pivot parameter "time" has not been found in the .csv file. Check that the '
-                                    'correct pivot parameter has been specified in the dataObject or make sure the pivot parameter is included in the .csv files')
-        else:
-          self.raiseAnError(IOError, 'Importing HistorySet from .csv: the specified pivot parameter ' + str(self.indexes) + '  has not been found in the .csv file')
-      for out in self._outputs:
+      if len(set(subDat.keys()).intersection(self.indexes)) != len(self.indexes):
+        self.raiseAnError(IOError,'Importing HistorySet from .csv: the pivot parameters "'+', '.join(self.indexes)+'" have not been found in the .csv file. Check that the '
+                                  'correct <pivotParameter> has been specified in the dataObject or make sure the <pivotParameter> is included in the .csv files')
+      for out in self._outputs+self.indexes:
         data[out][i] = subDat[out].values # TODO dtype?
 
     self.load(data,style='dict',dims=self.getDimensions())
