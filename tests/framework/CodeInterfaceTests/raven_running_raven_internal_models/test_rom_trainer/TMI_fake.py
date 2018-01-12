a0da1c17f931c31475fc2e37d20d1f91b9b262df
Paul W. Talbot <paul.talbot@inl.gov>
2018-01-11 20:19:41 -0600

    stash

--- tests/framework/CodeInterfaceTests/raven_running_raven_internal_models/test_rom_trainer/TMI_fake.py
+++ tests/framework/CodeInterfaceTests/raven_running_raven_internal_models/test_rom_trainer/TMI_fake.py
@@ -19,9 +19,12 @@ def run(self, Input):
   self.time = numpy.zeros(number_of_steps)
   DeltaTimeScramToAux = Input["DeltaTimeScramToAux"]
   DG1recoveryTime = Input["DG1recoveryTime"]
+  print('DEBUGG run DTA:',DeltaTimeScramToAux,type(DeltaTimeScramToAux))
+  print('DEBUGG run DG1:',DG1recoveryTime,type(DG1recoveryTime))
   self.CladTempThreshold = numpy.zeros(number_of_steps)
   self.UpperPlenumEnergy= numpy.zeros(number_of_steps)
   for i in range(len(self.time)):
     self.time[i] = 0.25*i
     self.CladTempThreshold[i] = self.time[i]*50.0 + DeltaTimeScramToAux*200.0 + DG1recoveryTime*500.0
     self.UpperPlenumEnergy[i] = self.time[i]*5.0 + DeltaTimeScramToAux*30.0 + DG1recoveryTime*40.0 + DeltaTimeScramToAux*DG1recoveryTime*5.0
+  print('OUTGOING:',self.CladTempThreshold)
