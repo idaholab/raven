7c58f908b0ac7722c91fd88adcb380ec29b73d13
Congjian Wang <congjian.wang@inl.gov>
2018-02-03 10:23:45 -0700

    fix cluster tests (#566)
    * fix cluster tests
    
    * fix parallelPP test

--- tests/cluster_tests/test_pbspro.sh
+++ tests/cluster_tests/test_pbspro.sh
@@ -119,7 +119,7 @@ rm -Rf InternalParallelPostProcessorLS/*.csv
 
 python ../../../framework/Driver.py test_internal_parallel_PP_LS.xml ../pbspro_mpi.xml ../cluster_runinfo.xml
 
-wait_lines 'InternalParallelPostProcessorLS/*.csv' 6 parallelPP
+wait_lines 'InternalParallelPostProcessorLS/*.csv' 4 parallelPP
 
 cd ..
 
