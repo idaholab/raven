861d4dcefb58929acb03ce0035079d0710084c54
Paul W. Talbot <paul.talbot@inl.gov>
2018-02-06 15:07:39 -0700

    cleanup

--- framework/Models/Dummy.py
+++ framework/Models/Dummy.py
@@ -139,9 +139,6 @@ class Dummy(Model):
     missing = list(var for var,val in inputDict.items() if val is None)
     if len(missing) != 0:
       self.raiseAnError(IOError,'Input values for variables {} not found while preparing the input for model "{}"!'.format(missing,self.name))
-    #for var,val in inputDict.values():
-    #  if val is None:
-    #    self.raiseAnError(IOError,'While preparing the input for the model '+self.type+' with name '+self.name+' found a None input variable '+ str(inputDict.items()))
     #the inputs/outputs should not be store locally since they might be used as a part of a list of input for the parallel runs
     #same reason why it should not be used the value of the counter inside the class but the one returned from outside as a part of the input
 
