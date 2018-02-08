43ee9e8794b6fca987c032c364d83f7349c909fa
alfoa <andrea.alfonsi@inl.gov>
2018-02-08 15:35:19 -0700

    ok

--- framework/SupervisedLearning.py
+++ framework/SupervisedLearning.py
@@ -2985,7 +2985,7 @@ class PolyExponential(superVisedLearning):
     self.pivotValues = targetVals[0,:index,pivotParamIndex]
     # the targets are the coefficients
     expTermCoeff = np.concatenate( (aij,bij), axis=1)    
-    what = 1
+    what = 2
     if what == 1:
       # now that we have the coefficients, we can construct the polynomial expansion whose targets are the just computed coefficients
       self.model = make_pipeline(PolynomialFeatures(self.polyExpParams['polyOrder']), linear_model.Ridge())
@@ -3006,18 +3006,38 @@ class PolyExponential(superVisedLearning):
         coeff_str+="(" + ",".join(self.features)+"):"
         self.raiseAMessage(coeff_str)
         self.raiseAMessage("      "+" ".join([str(elm) for elm in coefficients[l]]))
-  if what == 2:
-    #svr
-    dualCoefficients = []
-    supportVectors = []
-    rho = []
-    self.model = [svm.SVR(kernel='rbf', degree=3, gamma='auto', tol=1e-3, C=1.0) for _ in range(self.polyExpParams['expTerms']*2)]
-    for cnt, model in enumerate(self.model):
-      model.fit(featureVals, expTermCoeff[:,cnt])
-      dualCoefficients.append(model.dual_coef_)
-      supportVectors.append(model.support_vectors_)
-      rho.append(model.intercept_)
-  
+    if what == 2:
+      #svr
+      from sklearn import preprocessing
+      from sklearn.model_selection import GridSearchCV
+      self.scaler = preprocessing.StandardScaler().fit(featureVals)
+      X_scaled = self.scaler.transform(featureVals)  
+      fileObject = open("support_vectors.csv","w+")
+      parameters = {'kernel':('linear', 'rbf','poly'), 
+                    'C':[1.0,2.0,5.0,7.0,10.0,15.0,20.,25., 30],
+                    'gamma':[0.8,1.0,1.4,1.8,2.4,3.6,5.0,7.5,10.],
+                    'epsilon':[0.05,0.1,0.2, 0.3, 0.5]}
+      
+      #self.model = [svm.SVR(kernel='rbf', tol=1e-3, C=30,verbose=True) for _ in range(self.polyExpParams['expTerms']*2)]
+      self.model = [None for _ in range(self.polyExpParams['expTerms']*2)]
+      for cnt in range(len(self.model )):
+      #for cnt, model in enumerate(self.model):
+        clf = GridSearchCV(svm.SVR(), parameters)
+        search = clf.fit(X_scaled, expTermCoeff[:,cnt])
+        model = search.best_estimator_
+        self.model[cnt] = search.best_estimator_
+        #model.fit(X_scaled, expTermCoeff[:,cnt])
+        if cnt < self.polyExpParams['expTerms']:
+          coeff_str = "a_"+str(cnt+1)    
+        else:
+          coeff_str = "b_"+str((cnt-self.polyExpParams['expTerms'])+1)
+        fileObject.write("term"+","+"nSV"+",rho\n")
+        fileObject.write(coeff_str+","+str(model.dual_coef_.shape[-1])+","+str(model.intercept_[0])+"\n")
+        fileObject.write("dual_coef,"+",".join(self.features)+"\n")
+        for j in range(model.dual_coef_.shape[-1]):
+          strSupportVectors = [str(elm) for elm in model.support_vectors_[j,:]]
+          fileObject.write(str(model.dual_coef_[0,j]) +","+",".join(strSupportVectors)+"\n")
+      fileObject.close() 
 
   def __evaluateLocal__(self,featureVals):
     """
@@ -3025,9 +3045,9 @@ class PolyExponential(superVisedLearning):
       @ Out, returnEvaluation , dict, dictionary of values for each target (and pivot parameter)
     """
     if isinstance(self.model, list):
-      evaluation = []
-      for cnt,model in range(len(self.model)):
-        evaluation.append(model.predict(featureVals))
+      evaluation = np.zeros((len(featureVals),len(self.model)))
+      for cnt,model in enumerate(self.model):
+        evaluation[:,cnt] = model.predict(self.scaler.transform(featureVals) )
     else:
       evaluation = self.model.predict(featureVals)
     returnEvaluation = {}
