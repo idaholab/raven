dd6ceaffd2799018d2a3116e09aa455939fa27d5
Derek Stucki <derek.stucki@inl.gov>
2018-02-27 10:49:21 -0700

    Added setup.py and a build script for hit.

---
+++ scripts/TestHarness/hit-windows/build_hit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HIT_DIR=../../../moose/framework/contrib/hit
+MOOSE_PYTHON_DIR=../../../moose/python
+if [[ $VSVARSALL ]]
+then
+    SETUPTOOLS_COMPILER_FLAG='--compiler=msvc'
+fi
+cp setup.py $HIT_DIR
+pushd $HIT_DIR
+python setup.py build_ext --inplace $SETUPTOOLS_COMPILER_FLAG
+popd
+if [[ -f ${HIT_DIR}/hit.so ]]
+then
+    mv ${HIT_DIR}/hit.so $MOOSE_PYTHON_DIR
+elif [[ -f ${HIT_DIR}/hit.pyd ]]
+then
+    mv ${HIT_DIR}/hit.pyd $MOOSE_PYTHON_DIR
+    touch ${MOOSE_PYTHON_DIR}/hit.so
+fi
