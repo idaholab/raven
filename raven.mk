8b565cd58424fd53b7d7d3c846022fd6e961d70d
Derek Stucki <derek.stucki@inl.gov>
2018-02-27 11:24:08 -0700

    Added script to makefile.

--- raven.mk
+++ raven.mk
@@ -9,9 +9,12 @@ SHELL := /bin/bash
 include $(RAVEN_DIR)/amsc.mk
 ################################################################################
 
-framework_modules:: amsc python_crow_modules
+hit $(MOOSE_DIR)/python/hit.so:: $(FRAMEWORK_DIR)/contrib/hit/hit.cpp $(FRAMEWORK_DIR)/contrib/hit/lex.cc $(FRAMEWORK_DIR)/contrib/hit/parse.cc
+	bash -c 'cd scripts/TestHarness/hit-windows && ./build_hit.sh'
 
-all:: amsc python_crow_modules
+framework_modules:: amsc python_crow_modules hit
+
+all:: amsc python_crow_modules hit
 
 ####################################################################################
 #           find and remove all the *.pyc files (better safe then sorry)           #
@@ -32,8 +35,10 @@ clean::
           $(RAVEN_objects) \
           $(RAVEN_app_objects) \
           $(RAVEN_APP) \
-          $(RAVEN_plugins)
-	@rm -Rf $(RAVEN_DIR)/build
+          $(RAVEN_plugins) \
+	  $(MOOSE_DIR)/python/hit.so \
+	  $(MOOSE_DIR)/python/hit.pyd
+	@rm -Rf $(RAVEN_DIR)/build $(FRAMEWORK_DIR)/contrib/hit/build
 	@find $(RAVEN_DIR)/framework  -name '*.pyc' -exec rm '{}' \;
 
 cleanall::
