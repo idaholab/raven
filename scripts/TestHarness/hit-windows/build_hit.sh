ce047a5d2eefe79911e769d5794b9e1002f3a806
Derek Stucki <derek.stucki@inl.gov>
2018-02-28 16:29:46 -0700

    Exit code grabbing on hit build.

--- scripts/TestHarness/hit-windows/build_hit.sh
+++ scripts/TestHarness/hit-windows/build_hit.sh
@@ -7,12 +7,15 @@ then
     cp setup.py $HIT_DIR
     pushd $HIT_DIR
     python setup.py build_ext --inplace --compiler=msvc
+    EXIT_CODE=$?
     popd
     cp ${HIT_DIR}/hit.pyd $MOOSE_PYTHON_DIR
     touch ${MOOSE_PYTHON_DIR}/hit.so
 else
     make -C $HIT_DIR bindings
+    EXIT_CODE=$?
     cp ${HIT_DIR}/hit.so $MOOSE_PYTHON_DIR
 fi
 echo ls -l $MOOSE_PYTHON_DIR
 ls -l $MOOSE_PYTHON_DIR
+exit $EXIT_CODE
