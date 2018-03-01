460b32daad173340dcd3cc8abe54ad90643ddbdc
Derek Stucki <derek.stucki@inl.gov>
2018-03-01 09:22:31 -0700

    Added version check for xarray.

--- scripts/TestHarness/testers/RavenUtils.py
+++ scripts/TestHarness/testers/RavenUtils.py
@@ -32,11 +32,14 @@ def inPython3():
 #This list is made of (module, how to check the version, minimum version,
 # quality assurance module version, maximum version)
 # Deep learning requires Scikit-Learn version at least 0.18
-modules_to_try = [("numpy",'numpy.version.version',"1.8.0","1.11.0",None),
-                  ("h5py",'h5py.__version__','2.4.0','2.6.0',None),
-                  ("scipy",'scipy.__version__',"0.14.0","0.17.1",None),
-                  ("sklearn",'sklearn.__version__',"0.18","0.18.0",None),
-                  ("matplotlib",'matplotlib.__version__',"1.3.1","1.5.3",None)]
+modules_to_try = [
+  ("numpy",'numpy.version.version',"1.8.0","1.11.0",None),
+  ("h5py",'h5py.__version__','2.4.0','2.6.0',None),
+  ("scipy",'scipy.__version__',"0.14.0","0.17.1",None),
+  ("sklearn",'sklearn.__version__',"0.18","0.18.0",None),
+  ("matplotlib",'matplotlib.__version__',"1.3.1","1.5.3",None),
+  ("xarray",'xarray.__version__',"0.9.5",None),
+]
 
 def __lookUpPreferredVersion(name):
   """
