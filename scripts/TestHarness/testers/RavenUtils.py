93388a2a6c0e534abc01d8faa9bec9a10e9098ba
Andrea Alfonsi - INL <andrea.alfonsi@inl.gov>
2018-03-12 13:44:18 -0600

    Update RavenUtils.py
    removed xarray and netcdf dependency....this should go on dataobject-rework

--- scripts/TestHarness/testers/RavenUtils.py
+++ scripts/TestHarness/testers/RavenUtils.py
@@ -36,9 +36,8 @@ modules_to_try = [
   ("numpy",'numpy.version.version',"1.8.0","1.11.0",None),
   ("h5py",'h5py.__version__','2.4.0','2.6.0',None),
   ("scipy",'scipy.__version__',"0.14.0","0.17.1",None),
-  ("sklearn",'sklearn.__version__',"0.18","0.18.0",None),
-  ("matplotlib",'matplotlib.__version__',"1.3.1","1.5.3",None),
-  ("xarray",'xarray.__version__',"0.9.5","0.9.5",None),
+  ("sklearn",'sklearn.__version__',"0.16.1","0.17.1",None),
+  ("matplotlib",'matplotlib.__version__',"1.3.1","1.5.3",None)
 ]
 
 def __lookUpPreferredVersion(name):
@@ -57,8 +56,6 @@ __condaList = [("numpy",__lookUpPreferredVersion("numpy")),
                ("scipy",__lookUpPreferredVersion("scipy")),
                ("scikit-learn",__lookUpPreferredVersion("sklearn")),
                ("matplotlib",__lookUpPreferredVersion("matplotlib")),
-               ("xarray",__lookUpPreferredVersion("xarray")),
-               ("netcdf4",""),
                ("pyside",""),
                ("python","2.7"),
                ("hdf5",""),
