fa4145d70833540938d99a7028e614ab9fcdccf6
Paul W. Talbot <paul.talbot@inl.gov>
2018-03-14 11:19:20 -0600

    locking down xarray library versions

--- scripts/TestHarness/testers/RavenUtils.py
+++ scripts/TestHarness/testers/RavenUtils.py
@@ -36,6 +36,7 @@ modules_to_try = [("numpy",'numpy.version.version',"1.8.0","1.11.0",None),
                   ("h5py",'h5py.__version__','2.4.0','2.6.0',None),
                   ("scipy",'scipy.__version__',"0.14.0","0.17.1",None),
                   ("sklearn",'sklearn.__version__',"0.18","0.18.0",None),
+                  ("xarray",'xarray.__version__',"0.9.5","0.9.6","0.9.6"),
                   ("matplotlib",'matplotlib.__version__',"1.3.1","1.5.3",None)]
 
 def __lookUpPreferredVersion(name):
