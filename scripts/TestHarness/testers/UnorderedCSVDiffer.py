3f6a7e8a2d8e4d11e4af1d73fd6159dc0f28c9cd
Paul W. Talbot <paul.talbot@inl.gov>
2018-04-11 12:38:43 -0600

    fixed rel err in unordered csv differ

--- scripts/TestHarness/testers/UnorderedCSVDiffer.py
+++ scripts/TestHarness/testers/UnorderedCSVDiffer.py
@@ -65,9 +65,14 @@ class UnorderedCSVDiffer:
         val = -sys.maxint
       elif pd.isnull(val):
         val = sys.maxint
+      # relative error (scale by "val" unless it's 0)
+      if val == 0:
+        scale = 1.0
+      else:
+        scale = abs(val)
       try:
         # try float/int first
-        match = match[(abs(match[idx] - val) < self.__rel_err)]
+        match = match[(abs(match[idx] - val) < scale*self.__rel_err)]
       except TypeError:
         # otherwise, use exact matching
         match = match[match[idx] == val]
