ea0e94dcb2e5611814df5beb39f77aaab5f82acc
rouxpn <30636482+rouxpn@users.noreply.github.com>
2018-04-12 04:23:32 -0600

    Updated material parsers

--- framework/CodeInterfaces/PHISICS/MaterialParser.py
+++ framework/CodeInterfaces/PHISICS/MaterialParser.py
@@ -11,13 +11,38 @@ import xml.etree.ElementTree as ET
 
 class MaterialParser():
   """
-    Parses the PHISICS xml materil file and replaces the nominal values by the perturbed values.
+    Parses the PHISICS xml material file and replaces the nominal values by the perturbed values.
   """
+  def __init__(self,inputFiles,workingDir,**pertDict):
+    """
+      Constructor.
+      @ In, inputFiles, string, Qvalues library file
+      @ In, workingDir, string, path to working directory
+      @ In, pertDict, dictionary, dictionary of perturbed variables
+      @ Out, None
+    """
+    self.pertDict = self.scientificNotation(pertDict) # Perturbed variables
+    self.inputFiles = inputFiles
+    self.tree = ET.parse(self.inputFiles)             # xml tree  
+    self.root = self.tree.getroot()                   # xml root 
+    self.listedDict = self.fileReconstruction(self.pertDict)
+    self.printInput(workingDir)
+
+  def scientificNotation(self,pertDict):
+    """
+      Converts the numerical values into a scientific notation.
+      @ In, pertDict, dictionary, perturbed variables
+      @ Out, pertDict, dictionary, perturbed variables in scientific format
+    """
+    for key, value in pertDict.iteritems():
+      pertDict[key] = '%.3E' % Decimal(str(value)) 
+    return pertDict
+    
   def replaceValues(self,genericXMLdict):
     """
       Replaces the values from the pertured dict and puts them in the deconstructed original dictionary
-      @ In, genericXMLdict, dictionary, dictionary under the format  {X|Y|Z:nominalValue}
-      @ Out, genericXMLdict, dictionary, dictionary under the format  {X|Y|Z:perturbedValue}
+      @ In, genericXMLdict, dictionary, dictionary templated  {X|Y|Z:nominalValue}
+      @ Out, genericXMLdict, dictionary, dictionary templated  {X|Y|Z:perturbedValue}
     """
     setXML = set(genericXMLdict)
     setPertDict = set(self.pertDict)
@@ -55,10 +80,10 @@ class MaterialParser():
         isotopeList.append(isotopeXML.attrib.get('id'))
     matList = self.unifyElements(matList)
     isotopeList = self.unifyElements(isotopeList)
-    for i in xrange(0,len(matList)):
-      XMLdict['density'][matList[i]] = {}
-      for j in xrange(0,len(isotopeList)):
-        XMLdict['density'][matList[i]][isotopeList[j]] = {}
+    for mat in matList:
+      XMLdict['density'][mat] = {}
+      for isotope in isotopeList:
+        XMLdict['density'][mat][isotope] = {}
     for matXML in self.root.getiterator('mat'):
       for isotopeXML in matXML.findall('isotope'):
         XMLdict['density'][matXML.attrib.get('id')][isotopeXML.attrib.get('id')] = isotopeXML.attrib.get('density')
@@ -66,52 +91,34 @@ class MaterialParser():
   
   def unifyElements(self,listWithRepetitions):
     """
-      Takes a list as an entry and reduces the list into a list of unique elements.
-      @ In, listWithRepetitions, list
-      @ Out, listWithUniqueElements, list
+      removes any repetitions of elements in a list.
+      @ In, listWithRepetitions, list, list made of non-unique elements
+      @ Out, listWithUniqueElements, list, list made of unique elements
     """
     valueSeen = set()
     listWithUniqueElements = [x for x in listWithRepetitions if x not in valueSeen and not valueSeen.add(x)]
     return listWithUniqueElements
-    
-  def __init__(self,inputFiles,workingDir,**pertDict):
-    """
-      Constructor.
-      @ In, inputFiles, string, Qvalues library file
-      @ In, workingDir, string, path to working directory
-      @ In, pertDict, dictionary, dictionary of perturbed variables
-      @ Out, None
-    """
-    self.pertDict = pertDict
-    for key, value in self.pertDict.iteritems():
-      self.pertDict[key] = '%.3E' % Decimal(str(value)) #convert the values into scientific values
-    self.inputFiles = inputFiles
-    self.tree = ET.parse(self.inputFiles)
-    self.root = self.tree.getroot()
-    self.listedDict = self.fileReconstruction(self.pertDict)
-    self.printInput(workingDir)
 
   def fileReconstruction(self,deconstructedDict):
     """
       Converts the formatted dictionary -> {'DENSITY|FUEL1|U235':1.30, DENSITY|FUEL2|U238':4.69}.
       into a dictionary of dictionaries that has the format -> {'DENSITY':{'FUEL1':{'U235':1.30}, 'FUEL2':{'U238':4.69}}}
-      @ In, deconstructedDict, dictionary
-      @ Out, reconstructedDict, nested dictionary
+      @ In, deconstructedDict, dictionary, dictionary of perturbed variables
+      @ Out, reconstructedDict, dictionary, nested dictionary of perturbed variables
     """
     reconstructedDict = {}
     perturbedIsotopes = []
     perturbedMaterials = []
     perturbedPhysicalParameters = []
-    for i in deconstructedDict.iterkeys():
-      splittedKeywords = i.split('|')
-      perturbedIsotopes.append(splittedKeywords[2])
-      perturbedMaterials.append(splittedKeywords[1])
-      perturbedPhysicalParameters.append(splittedKeywords[0])
-    for i in xrange (0,len(perturbedPhysicalParameters)):
+    for key in deconstructedDict.iterkeys():
+      perturbedIsotopes.append(key.split('|')[2])
+      perturbedMaterials.append(key.split('|')[1])
+      perturbedPhysicalParameters.append(key.split('|')[0])
+    for i in range (len(perturbedPhysicalParameters)):
       reconstructedDict[perturbedPhysicalParameters[i]] = {}
-      for j in xrange (0,len(perturbedMaterials)):
+      for j in range (len(perturbedMaterials)):
         reconstructedDict[perturbedPhysicalParameters[i]][perturbedMaterials[j]] = {}
-        for k in xrange (0,len(perturbedIsotopes)):
+        for k in range (len(perturbedIsotopes)):
           reconstructedDict[perturbedPhysicalParameters[i]][perturbedMaterials[j]][perturbedIsotopes[k]] = {}
     for typeKey, value in deconstructedDict.iteritems():
       keyWords = typeKey.split('|')
@@ -120,10 +127,12 @@ class MaterialParser():
     
   def printInput(self,workingDir):
     """
-      Prints out the pertubed material number densities into a file.
-      @ In, workingDir, string, path to working directory 
+      Prints out the pertubed xml material file into a xml file. The workflow is: 
+      open a new file with a dummy name; parse the unperturbed library; print the line in the dummy and  
+      replace with perturbed variables if necessary, Change the name of the dummy file. 
+      @ In, workingDir, string, path to working directory
       @ Out, None
-    """   
+    """  
     modifiedFile = os.path.join(workingDir,'test.dat')
     open(modifiedFile, 'w')
     XMLdict = self.dictFormatingFromXmlToPerturbed()
