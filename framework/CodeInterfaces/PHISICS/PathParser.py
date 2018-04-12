229724dbdf0b7362cb076ceb88a9adc3d4e5e0a7
rouxpn <30636482+rouxpn@users.noreply.github.com>
2018-04-12 06:43:09 -0600

    path parser added

--- framework/CodeInterfaces/PHISICS/PathParser.py
+++ framework/CodeInterfaces/PHISICS/PathParser.py
@@ -11,32 +11,9 @@ from decimal import Decimal
 
 class PathParser():
   """
-    Parses the PHISICS xml decay library located in the path folder (betadecay, alphadecay etc.)
+    Parses the PHISICS decay Qvalue library located in the path folder (betadecay, alphadecay etc.)
     and replaces the nominal values by the perturbed values.
   """
-  def matrixPrinter(self,line,outfile):
-    """
-      Prints the perturbed decay matrix in the outfile.
-      @ In, line, string
-      @ In, outfile, string, output file name
-      @ Out, None
-    """
-    line = re.sub(r'(.*?)(\w+)(-)(\d+M?)',r'\1\2\4',line)
-    line = line.upper().split()
-    if line[0] in self.setOfPerturbedIsotopes:
-      try:
-        line[1] = str(self.listedQValuesDict.get(line[0]))
-      except:
-        raise Exception('Error. Check if the unperturbed library has defined values relative to the requested perturbed isotopes')
-    if len(line) > 1:
-      line[0] = "{0:<7s}".format(line[0])
-      line[1] = "{0:<11s}".format(line[1])
-      line = ''.join(line[0]+line[1]+"\n")
-      outfile.writelines(line)
-    if re.search(r'(.*?)END', line[0]):
-      self.stopFlag = self.stopFlag + 1
-      self.harcodingSection = 0
-          
   def __init__(self,inputFiles,workingDir,**pertDict):
     """  
       Constructor.
@@ -45,12 +22,46 @@ class PathParser():
       @ In, pertDict, dictionary, dictionary of perturbed variables
       @ Out, None
     """
+    self.endStringCounter = 0   # counts how many times 'END' occurs
+    self.harcodingSection = 0   # 0 if portions of files that will not be perturbed, one if variables can be perturbed
+    self.listedQValuesDict = {}
     self.inputFiles = inputFiles
-    self.pertQValuesDict = pertDict
-    for key, value in self.pertQValuesDict.iteritems():
-      self.pertQValuesDict[key] = '%.3E' % Decimal(str(value)) #convert the values into scientific values
-    self.fileReconstruction()
-    self.printInput(workingDir)
+    self.pertQValuesDict = self.scientificNotation(pertDict) # Perturbed variables
+    self.fileReconstruction()   # Puts the perturbed variables in a dictionary 
+    self.printInput(workingDir) # Replaces the the nominal values by the perturbed one and print in a file
+
+  def scientificNotation(self,pertDict):
+    """
+      Converts the numerical values into a scientific notation.
+      @ In, pertDict, dictionary, perturbed variables
+      @ Out, pertDict, dictionary, perturbed variables in scientific format
+    """
+    for key, value in pertDict.iteritems():
+      pertDict[key] = '%.3E' % Decimal(str(value)) 
+    return pertDict
+    
+  def matrixPrinter(self,line,outfile):
+    """
+      Prints the perturbed decay matrix in the outfile.
+      @ In, line, string, input file line
+      @ In, outfile, file object, input file in file object format
+      @ Out, None
+    """
+    line = re.sub(r'(.*?)(\w+)(-)(\d+M?)',r'\1\2\4',line)
+    line = line.upper().split()
+    if line[0] in self.setOfPerturbedIsotopes:
+      try:
+        line[1] = str(self.listedQValuesDict.get(line[0]))
+      except KeyError:
+        raise KeyError('Error. Check if the unperturbed library has defined values relative to the requested perturbed isotopes')
+    if len(line) > 1:
+      line[0] = "{0:<7s}".format(line[0])
+      line[1] = "{0:<11s}".format(line[1])
+      line = ''.join(line[0]+line[1]+"\n")
+      outfile.writelines(line)
+    if re.search(r'(.*?)END', line[0]):
+      self.endStringCounter = self.endStringCounter + 1
+      self.harcodingSection = 0
     
   def fileReconstruction(self):
     """
@@ -59,13 +70,12 @@ class PathParser():
       @ In, None
       @ Out, reconstructedDict, nested dictionary
     """
-    self.listedQValuesDict = {}
+    
     perturbedIsotopes = []
-    for i in self.pertQValuesDict.iterkeys():
-      splittedDecayKeywords = i.split('|')
-      perturbedIsotopes.append(splittedDecayKeywords[1])
-    for i in xrange (0,len(perturbedIsotopes)):
-      self.listedQValuesDict[perturbedIsotopes[i]] = {}   # declare all the dictionaries
+    for key in self.pertQValuesDict.iterkeys():
+      perturbedIsotopes.append(key.split('|')[1])
+    for perturbedIsotope in perturbedIsotopes:
+      self.listedQValuesDict[perturbedIsotope] = {}   # declare all the dictionaries
     for isotopeKeyName, QValue in self.pertQValuesDict.iteritems():
       isotopeName = isotopeKeyName.split('|')
       self.listedQValuesDict[isotopeName[1]] = QValue
@@ -73,13 +83,13 @@ class PathParser():
     
   def printInput(self,workingDir):
     """
-      Prints out the pertubed decay Qvalues library into a file.
-      @ In, workingDir, string, absolute path to working directory
+      Prints out the pertubed decay qvalue file into a .dat file. The workflow is: 
+      open a new file with a dummy name; parse the unperturbed library; print the line in the dummy and  
+      replace with perturbed variables if necessary, Change the name of the dummy file. 
+      @ In, workingDir, string, path to working directory
       @ Out, None
-    """
+    """  
     sectionCounter = 0
-    self.stopFlag = 0
-    self.harcodingSection = 0
     modifiedFile = os.path.join(workingDir,'test.dat')
     open(modifiedFile, 'w')
     with open(modifiedFile, 'a') as outfile:
@@ -89,12 +99,12 @@ class PathParser():
             sectionCounter = sectionCounter + 1
           if not line.split(): 
             continue  # if the line is blank, ignore it
-          if sectionCounter == 1 and self.stopFlag == 0: #actinide section
+          if sectionCounter == 1 and self.endStringCounter == 0: #actinide section
             self.harcodingSection = 1
-            self.matrixPrinter(line, outfile)
-          if sectionCounter == 2 and self.stopFlag == 1: #FP section
+            self.matrixPrinter(line,outfile)
+          if sectionCounter == 2 and self.endStringCounter == 1: #FP section
             self.harcodingSection = 2
-            self.matrixPrinter(line, outfile)
+            self.matrixPrinter(line,outfile)
           if self.harcodingSection != 1 and self.harcodingSection !=2:
             outfile.writelines(line)
     os.rename(modifiedFile,self.inputFiles)
