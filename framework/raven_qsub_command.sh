3009fc7eeb7290e4cec24e9c900cf970287662f0
Paul W. Talbot <paul.talbot@inl.gov>
2018-06-15 10:14:28 -0600

    attempting to make conda definitions work by environment var

--- framework/raven_qsub_command.sh
+++ framework/raven_qsub_command.sh
@@ -4,6 +4,9 @@ if test -n "$PBS_O_WORKDIR"; then
     cd $PBS_O_WORKDIR
 fi
 
+# set up conda definitions location (for the establish_conda_env.sh script)
+export CONDA_DEFS="/apps/local/miniconda2/4.5.4/etc/profile.d/conda.sh"
+
 which python
 which mpiexec
 echo $COMMAND
