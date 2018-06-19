bf0210240c828a22160026d4b558be54ce85e216
Paul W. Talbot <paul.talbot@inl.gov>
2018-06-19 14:51:56 -0600

    cleaned up raven_qsub_command

--- framework/raven_qsub_command.sh
+++ framework/raven_qsub_command.sh
@@ -4,11 +4,8 @@ if test -n "$PBS_O_WORKDIR"; then
     cd $PBS_O_WORKDIR
 fi
 
-# set up conda definitions location (for the establish_conda_env.sh script)
-export CONDA_DEFS="/apps/local/miniconda2/4.5.4/etc/profile.d/conda.sh"
-export CONDA_ENVS_PATH="~/.conda/falcon/envs"
-# note that $RAVEN_LIBS_NAME should be set here as well ...
-# TODO does this run qsubbed commands with the provided env name correctly?
+# conda definitions should be set up in raven/.ravenrc after installing with raven/scripts/establish_conda_env.sh
+##  also the name of the raven libraries conda environment
 
 which python
 which mpiexec
