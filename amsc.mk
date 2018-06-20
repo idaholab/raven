ffce40869c28ac29290d0e57f764402079535326
Paul W. Talbot <paul.talbot@inl.gov>
2018-06-20 13:58:24 -0600

    put back in macosx deployment target

--- amsc.mk
+++ amsc.mk
@@ -8,5 +8,4 @@ amsc :: $(AMSC_MODULE)
 $(AMSC_MODULE) : $(MY_DIR)/src/contrib/amsc.i $(AMSC_srcfiles)
 	@echo "Building "$@"..."
 	(cd $(MY_DIR) && unset CXX CC && . $(MY_DIR)/scripts/establish_conda_env.sh --load && python ./setup.py build_ext build install --install-platlib=./framework/contrib/AMSC)
-	# (cd $(MY_DIR) && unset CXX CC && if test `uname` = Darwin; then MACOSX_DEPLOYMENT_TARGET=10.9; export MACOSX_DEPLOYMENT_TARGET; fi && . $(MY_DIR)/scripts/establish_conda_env.sh --load && python ./setup.py build_ext build install --install-platlib=./framework/contrib/AMSC)
 	@echo "Done"
