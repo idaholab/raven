2d5b1c662474f3c0f2a2af404465f5e555e75e0b
Paul W. Talbot <paul.talbot@inl.gov>
2018-07-16 10:04:50 -0600

    working locally, attempting to fix

--- tests/cluster_tests/run_mpi_test.sh
+++ tests/cluster_tests/run_mpi_test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo 'DEBUGG chdir'
 cd $PBS_O_WORKDIR/
 
+echo 'DEBUGG module load 1'
 module load raven-devel
+echo 'DEBUGG module load 2'
 module load MVAPICH2/2.0.1-GCC-4.9.2
+echo 'DEBUGG library load'
 source activate raven_libraries
 
+echo 'DEBUGG run'
 python  ../../framework/Driver.py test_mpi.xml
+echo 'DEBUGG done'
