cd0f15bb7d78a4b66f9db12aefa6100b705e78fb
Paul W. Talbot <paul.talbot@inl.gov>
2018-07-16 10:46:36 -0600

    iterating

--- tests/cluster_tests/run_mpi_test.sh
+++ tests/cluster_tests/run_mpi_test.sh
@@ -8,7 +8,7 @@ module load raven-devel
 echo 'DEBUGG module load 2'
 module load MVAPICH2/2.0.1-GCC-4.9.2
 echo 'DEBUGG library load'
-source activate raven_libraries
+source ../scripts/establish_conda_env.sh --load
 
 echo 'DEBUGG run'
 python  ../../framework/Driver.py test_mpi.xml
