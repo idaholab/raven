d42b144cf5072bd2af7a4629ad858891de57f751
Congjian Wang <congjian.wang@inl.gov>
2018-09-06 13:55:24 -0600

    BasicStatistics performance improvements
    * update bstat pp
    
    * apply xr apply_ufunc
    
    * using dataset to compute scalar metrics
    
    * using xarray to compute the medians
    
    * using xarray to compute sen
    
    * using xarray to compute covariance
    
    * using xarray to compute pearson
    
    * using xarray to compute variance dependent sensitivity
    
    * fix typo
    
    * fix implementation bugs
    
    * fix more implementation bugs
    
    * add checks for sensitivity calculations, multi-colinearity should be resolved in the future
    
    * fix implementation bugs in time dependent covariance calculations
    
    * fix some bugs
    
    * use functions for metric calculations
    
    * use function to compute variance dependent sensitivity, and fix the percentile implementation bug
    
    * update docstring and cleanup
    
    * clean up and store the results into xarray.Dataset
    
    * start to implement load from xarray dataset functions in DataObjects
    
    * add functions to allow load dataset into dataobject
    
    * convert dataset to dict in basicStatistics
    
    * revert changes in tests
    
    * fix percentile calculations
    
    * add tests for basicStatisticsPP to dump the results to dataset
    
    * revert changes in DataObjects
    
    * allow the dictionary to pass to basic statisitics, this will allow other postprocessor to use the basic statistics pp without changes of the code
    
    * update manual for basicStats pp with DataSet outputs
    
    * resolve comments
    
    * clean up

--- framework/DataObjects/DataSet.py
+++ framework/DataObjects/DataSet.py
@@ -411,10 +411,10 @@ class DataSet(DataObject):
       self.raiseAnError(RuntimeError,'Unrecognized request type:',type(var))
     return res
 
-  def load(self,fileName,style='netCDF',**kwargs):
+  def load(self,dataIn,style='netCDF',**kwargs):
     """
       Reads this dataset from disk based on the format.
-      @ In, fileName, str, path and name of file to read
+      @ In, dataIn, str, path and name of file to read
       @ In, style, str, optional, options are enumerated below
       @ In, kwargs, dict, optional, additional arguments to pass to reading function
       @ Out, None
@@ -422,17 +422,19 @@ class DataSet(DataObject):
     style = style.lower()
     # if fileToLoad in kwargs, then filename is actualle fileName/fileToLoad
     if 'fileToLoad' in kwargs.keys():
-      fileName = kwargs['fileToLoad'].getAbsFile()
+      dataIn = kwargs['fileToLoad'].getAbsFile()
     # load based on style for loading
     if style == 'netcdf':
-      self._fromNetCDF(fileName,**kwargs)
+      self._fromNetCDF(dataIn,**kwargs)
     elif style == 'csv':
       # make sure we don't include the "csv"
-      if fileName.endswith('.csv'):
-        fileName = fileName[:-4]
-      self._fromCSV(fileName,**kwargs)
+      if dataIn.endswith('.csv'):
+        dataIn = dataIn[:-4]
+      self._fromCSV(dataIn,**kwargs)
     elif style == 'dict':
-      self._fromDict(fileName,**kwargs)
+      self._fromDict(dataIn,**kwargs)
+    elif style == 'dataset':
+      self._fromXarrayDataset(dataIn)
     # TODO dask
     else:
       self.raiseAnError(NotImplementedError,'Unrecognized read style: "{}"'.format(style))
@@ -631,14 +633,15 @@ class DataSet(DataObject):
     if style.lower() == 'netcdf':
       self._toNetCDF(fileName,**kwargs)
     elif style.lower() == 'csv':
-      if len(self._data[self.sampleTag])==0: #TODO what if it's just metadata?
+      if len(self.asDataset().variables)==0: #TODO what if it's just metadata?
         self.raiseAWarning('Nothing to write!')
         return
       #first write the CSV
       firstIndex = kwargs.get('firstIndex',0)
       self._toCSV(fileName,start=firstIndex,**kwargs)
       # then the metaxml
-      self._toCSVXML(fileName,**kwargs)
+      if 'DataSet' in self._meta.keys():
+        self._toCSVXML(fileName,**kwargs)
     # TODO dask?
     else:
       self.raiseAnError(NotImplementedError,'Unrecognized write style: "{}"'.format(style))
@@ -656,6 +659,15 @@ class DataSet(DataObject):
       @ Out, int, number of samples in this dataset
     """
     return self.size
+
+  @property
+  def isEmpty(self):
+    """
+      @ In, None
+      @ Out, boolean, True if the dataset is empty otherwise False
+    """
+    empty = True if self.size == 0 else False
+    return empty
 
   @property
   def vars(self):
@@ -1272,6 +1284,43 @@ class DataSet(DataObject):
     # convert metadata back to XML files
     for key,val in self._data.attrs.items():
       self._meta[key] = pk.loads(val.encode('utf-8'))
+
+  def _fromXarrayDataset(self,dataset):
+    """
+    """
+    if not self.isEmpty:
+      self.raiseAnError(IOError, 'DataObject', self.name.strip(),'is not empty!')
+    #select data from dataset
+    providedVars  = set(dataset.data_vars.keys())
+    requiredVars  = set(self.getVars())
+    ## figure out who's missing from the IO space
+    missing = requiredVars - providedVars
+    if len(missing) > 0:
+      self.raiseAnError(KeyError,'Variables are missing from "source" that are required for data object "',
+                                  self.name.strip(),'":',",".join(missing))
+    # remove self.sampleTag since it is an internal used dimension
+    providedDims = set(dataset.sizes.keys()) - set([self.sampleTag])
+    requiredDims = set(self.indexes)
+    missing = requiredDims - providedDims
+    if len(missing) > 0:
+      self.raiseAnError(KeyError,'Dimensions are missing from "source" that are required for data object "',
+                                  self.name.strip(),'":',",".join(missing))
+    # select the required data from given dataset
+    datasetSub = dataset[list(requiredVars)]
+    # check the dimensions
+    for var in self.vars:
+      requiredDims = set(self.getDimensions(var)[var])
+      # make sure "dims" isn't polluted
+      assert(self.sampleTag not in requiredDims)
+      providedDims = set(datasetSub[var].sizes.keys()) - set([self.sampleTag])
+      if requiredDims != providedDims:
+        self.raiseAnError(KeyError,'Dimensions of variable',var,'from "source"', ",".join(providedDims),
+                'is not consistent with the required dimensions for data object "',
+                self.name.strip(),'":',",".join(requiredDims))
+    self._orderedVars = self.vars + self.indexes
+    self._data = datasetSub
+    for key, val in self._data.attrs.items():
+      self._meta[key] = val
 
   def _getCompatibleType(self,val):
     """
