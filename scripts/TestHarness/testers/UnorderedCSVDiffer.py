5c3c1c5273fdddbaa62b0327b04a8214cc7babb0
Congjian Wang <congjian.wang@inl.gov>
2018-09-25 12:43:11 -0600

    debug

--- scripts/TestHarness/testers/UnorderedCSVDiffer.py
+++ scripts/TestHarness/testers/UnorderedCSVDiffer.py
@@ -222,7 +222,8 @@ class UnorderedCSVDiffer:
         if len(match) == 0:
           same = False
           msg.append('Could not find match for row "{}" in Gold:\n{}'.format(idx+1,find)) #+1 because of header row
-          print(testCSV)
+          msg.append('The Test output csv is:')
+          msg.append(str(testCSV))
           # stop looking once a mismatch is found
           break
       self.finalizeMessage(same,msg,testFilename)
