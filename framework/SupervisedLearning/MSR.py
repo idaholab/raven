37680a1dbe0d49f1ee60a225bf43d83cf1ed9a26
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2018-10-18 15:50:03 -0600

    Deunicode strings so in Python 2 amsc can use them.

--- framework/SupervisedLearning/MSR.py
+++ framework/SupervisedLearning/MSR.py
@@ -27,6 +27,7 @@ warnings.simplefilter('default',DeprecationWarning)
 #External Modules------------------------------------------------------------------------------------
 import numpy as np
 import math
+import sys
 #External Modules End--------------------------------------------------------------------------------
 
 #Internal Modules------------------------------------------------------------------------------------
@@ -34,6 +35,16 @@ from .NDinterpolatorRom import NDinterpolatorRom
 from .SupervisedLearning import supervisedLearning
 from sklearn import neighbors, svm
 #Internal Modules End--------------------------------------------------------------------------------
+
+def _toStr(s):
+  """
+    Removes unicode from strings in Python 2 so amsc can use it.
+    @ In, s, unicode or str, String to convert to plain str
+    @ Out, s, str, Converted str
+  """
+  if sys.version_info.major > 2:
+    return s
+  return s.encode('ascii')
 
 class MSR(NDinterpolatorRom):
   """
@@ -102,9 +113,9 @@ class MSR(NDinterpolatorRom):
     # value checking in place since the type cast can fail.
     for key,val in kwargs.items():
       if key.lower() == 'graph':
-        self.graph = val.strip().lower()
+        self.graph = _toStr(val.strip()).lower()
       elif key.lower() == "gradient":
-        self.gradient = val.strip().lower()
+        self.gradient = _toStr(val.strip()).lower()
       elif key.lower() == "beta":
         try:
           self.beta = float(val)
@@ -112,7 +123,7 @@ class MSR(NDinterpolatorRom):
           # If the user has specified a graph, use it, otherwise be sure to use
           #  the default when checking whether this is a warning or an error
           if 'graph' in kwargs:
-            graph = kwargs['graph'].strip().lower()
+            graph = _toStr(kwargs['graph'].strip()).lower()
           else:
             graph = self.graph
           if graph.endswith('beta skeleton'):
@@ -146,7 +157,7 @@ class MSR(NDinterpolatorRom):
             # If the user has specified a strategy, use it, otherwise be sure to
             #  use the default when checking whether this is a warning or an error
             if 'partitionPredictor' in kwargs:
-              partPredictor = kwargs['partitionPredictor'].strip().lower()
+              partPredictor = _toStr(kwargs['partitionPredictor'].strip()).lower()
             else:
               partPredictor = self.partitionPredictor
             if partPredictor == 'kde':
@@ -158,9 +169,9 @@ class MSR(NDinterpolatorRom):
                                  'using the', partPredictor, 'partition',
                                  'predictor')
       elif key.lower() == 'persistence':
-        self.persistence = val.strip().lower()
+        self.persistence = _toStr(val.strip()).lower()
       elif key.lower() == 'partitionpredictor':
-        self.partitionPredictor = val.strip().lower()
+        self.partitionPredictor = _toStr(val.strip()).lower()
       elif key.lower() == 'smooth':
         self.blending = True
       elif key.lower() == "kernel":
@@ -266,7 +277,7 @@ class MSR(NDinterpolatorRom):
     if self.knn <= 0:
       self.knn = self.X.shape[0]
 
-    names = self.features + self.target
+    names = [_toStr(name) for name in self.features + self.target]
     # Data is already normalized, so ignore this parameter
     ### Comment replicated from the post-processor version, not sure what it
     ### means (DM)
