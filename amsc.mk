e46bbe867df3dd1e334b790bfe11a0b61065ee85
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2019-01-11 16:32:12 -0700

    removed echos

--- amsc.mk
+++ amsc.mk
@@ -9,16 +9,12 @@ ifeq ($(CROW_USE_PYTHON3),TRUE)
 amsc :: $(AMSC_MODULE)
 $(AMSC_MODULE) : $(MY_DIR)/src/contrib/amsc.i $(AMSC_srcfiles)
 	@echo "Building "$@"..."
-	@echo $(NO_CONDA)
-	@echo "NO_CONDA"
 	(cd $(MY_DIR) && unset CXX CC && python3 ./setup3.py build_ext build install --install-platlib=./framework/contrib/AMSC)
 	@echo "Done"
 else
 amsc :: $(AMSC_MODULE)
 $(AMSC_MODULE) : $(MY_DIR)/src/contrib/amsc.i $(AMSC_srcfiles)
 	@echo "Building "$@"..."
-	@echo $(NO_CONDA)
-	@echo "NO_CONDA"
 	(cd $(MY_DIR) && unset CXX CC && python ./setup.py build_ext build install --install-platlib=./framework/contrib/AMSC)
 	@echo "Done"
 endif
@@ -27,16 +23,12 @@ ifeq ($(CROW_USE_PYTHON3),TRUE)
 amsc :: $(AMSC_MODULE)
 $(AMSC_MODULE) : $(MY_DIR)/src/contrib/amsc.i $(AMSC_srcfiles)
 	@echo "Building "$@"..."
-	@echo "$(NO_CONDA)
-	@echo "NO_CONDA"
 	(cd $(MY_DIR) && unset CXX CC && . $(MY_DIR)/scripts/establish_conda_env.sh --load && python3 ./setup3.py build_ext build install --install-platlib=./framework/contrib/AMSC)
 	@echo "Done"
 else
 amsc :: $(AMSC_MODULE)
 $(AMSC_MODULE) : $(MY_DIR)/src/contrib/amsc.i $(AMSC_srcfiles)
 	@echo "Building "$@"..."
-	@echo $(NO_CONDA)
-	@echo "NO_CONDA"
 	(cd $(MY_DIR) && unset CXX CC && . $(MY_DIR)/scripts/establish_conda_env.sh --load && python ./setup.py build_ext build install --install-platlib=./framework/contrib/AMSC)
 	@echo "Done"
 endif
