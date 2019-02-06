27e65d57cc623af28f17a3f8f705439afab422a4
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2019-02-06 16:57:21 -0700

    Add the reason to the failed list (such as Failed, Diff etc)

--- rook/main.py
+++ rook/main.py
@@ -189,7 +189,7 @@ def process_result(index, _input_data, output_data):
     okaycolor = skip_color
   else:
     results["fail"] += 1
-    failed_list.append(process_test_name)
+    failed_list.append(Tester.get_group_name(group)+" "+process_test_name)
     print(output_data.output)
     print(output_data.message)
     okaycolor = fail_color
