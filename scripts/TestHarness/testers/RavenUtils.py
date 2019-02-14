ca2902219e1ac979301fafd80d9d1a067fe77a0c
Congjian Wang <congjian.wang@inl.gov>
2019-02-14 12:14:45 -0700

    downgrade numpy to prevent print difs in tests

--- scripts/TestHarness/testers/RavenUtils.py
+++ scripts/TestHarness/testers/RavenUtils.py
@@ -65,7 +65,7 @@ modules_to_try_py3 = [("h5py", 'h5py.__version__', '2.4.0', '2.7.1', None), # 2.
                       ("xarray", 'xarray.__version__', "0.9.5", "0.10.3", None),
                       ("sklearn", 'sklearn.__version__', "0.19.1", "0.19.1", None),
                       ("scipy", 'scipy.__version__', "1.1.0", "1.1.0", None),
-                      ("numpy", 'numpy.__version__', "1.13.3", "1.14.0", None),
+                      ("numpy", 'numpy.__version__', "1.13.3", "1.13.3", None),
                       ("matplotlib", 'matplotlib.__version__', "1.3.1", "2.2.3", None),
                       ## NOTE there is a known bug in netCDF4 prior to 1.3.1 where
                       # having a path length of exactly 88 characters can create a
