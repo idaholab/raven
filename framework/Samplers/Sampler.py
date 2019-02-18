fc9dcd4ca15cdcf11e9526da2b2c3ba2a7a8202e
Congjian Wang <congjian.wang@inl.gov>
2019-02-18 13:06:01 -0700

    fix

--- framework/Samplers/Sampler.py
+++ framework/Samplers/Sampler.py
@@ -275,10 +275,10 @@ class Sampler(utils.metaclass_insert(abc.ABCMeta,BaseType),Assembler):
       @ Out, None
     """
     prefix = ""
-    for childChild in inp.subparts:
-      if childChild.getName() =='distribution':
+    for child in inp.subparts:
+      if child.getName() =='distribution':
         prefix = "<distribution>"
-        toBeSampled = childChild.value
+        toBeSampled = child.value
     self.toBeSampled[prefix+inp.parameterValues['name']] = toBeSampled
 
   def _readVariable(self, inp):
@@ -295,35 +295,35 @@ class Sampler(utils.metaclass_insert(abc.ABCMeta,BaseType),Assembler):
     if 'shape' in inp.parameterValues:
       self.variableShapes[varName] = inp.parameterValues['shape']
     # read subnodes
-    for childChild in inp.subparts:
-      if childChild.getName() =='distribution':
+    for child in inp.subparts:
+      if child.getName() =='distribution':
         # can only have a distribution if doesn't already have a distribution or function
         if not foundDistOrFunc:
           foundDistOrFunc = True
         else:
           self.raiseAnError(IOError,'A sampled variable cannot have both a distribution and a function, or more than one of either!')
         # name of the distribution to sample
-        toBeSampled = childChild.value
+        toBeSampled = child.value
         varData={}
-        varData['name']=childChild.value
+        varData['name']=child.value
         # variable dimensionality
-        if 'dim' not in childChild.parameterValues:
+        if 'dim' not in child.parameterValues:
           dim=1
         else:
-          dim=childChild.parameterValues['dim']
+          dim=child.parameterValues['dim']
         varData['dim']=dim
         # set up mapping for variable to distribution
         self.variables2distributionsMapping[varName] = varData
         # flag distribution as needing to be sampled
         self.toBeSampled[varName] = toBeSampled
-      elif childChild.getName() == 'function':
+      elif child.getName() == 'function':
         # can only have a function if doesn't already have a distribution or function
         if not foundDistOrFunc:
           foundDistOrFunc = True
         else:
           self.raiseAnError(IOError,'A sampled variable cannot have both a distribution and a function!')
         # function name
-        toBeSampled = childChild.value
+        toBeSampled = child.value
         # track variable as a functional sample
         self.dependentSample[varName] = toBeSampled
 
@@ -338,21 +338,21 @@ class Sampler(utils.metaclass_insert(abc.ABCMeta,BaseType),Assembler):
     """
     transformationDict = {}
     listIndex = None
-    for childChild in inp.subparts:
-      if childChild.getName() == "latentVariables":
-        transformationDict[childChild.getName()] = list(childChild.value)
-      elif childChild.getName() == "manifestVariables":
-        transformationDict[childChild.getName()] = list(childChild.value)
-      elif childChild.getName() == "manifestVariablesIndex":
+    for child in inp.subparts:
+      if child.getName() == "latentVariables":
+        transformationDict[child.getName()] = list(child.value)
+      elif child.getName() == "manifestVariables":
+        transformationDict[child.getName()] = list(child.value)
+      elif child.getName() == "manifestVariablesIndex":
         # the index provided by the input file starts from 1, but the index used by the code starts from 0.
-        listIndex = list(int(val) - 1  for val in childChild.value)
-      elif childChild.getName() == "method":
-        self.transformationMethod[child.parameterValues['distribution']] = childChild.value
+        listIndex = list(int(val) - 1  for val in child.value)
+      elif child.getName() == "method":
+        self.transformationMethod[inp.parameterValues['distribution']] = child.value
     if listIndex == None:
       self.raiseAWarning('Index is not provided for manifestVariables, default index will be used instead!')
       listIndex = range(len(transformationDict["manifestVariables"]))
     transformationDict["manifestVariablesIndex"] = listIndex
-    self.variablesTransformationDict[child.parameterValues['distribution']] = transformationDict
+    self.variablesTransformationDict[inp.parameterValues['distribution']] = transformationDict
 
   def _readInConstant(self,inp):
     """
