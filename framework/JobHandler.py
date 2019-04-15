17340ddc2c84ed72f5a163ff65ebcc1d2d7fe2f6
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2019-04-15 12:35:17 -0600

    Use sys.executable as python command for ppserver.

--- framework/JobHandler.py
+++ framework/JobHandler.py
@@ -275,7 +275,7 @@ class JobHandler(MessageHandler.MessageUser):
         #subprocess.Popen(['ssh', nodeId, "python2.7", ppserverScript,"-w",str(ntasks),"-i",remoteHostName,"-p",str(newPort),"-t","1000","-g",localenv["PYTHONPATH"],"-d"],shell=False,stdout=outFile,stderr=outFile,env=localenv)
 
         ## Instead, let's build the command and then call the os-agnostic version
-        pythonCommand = utils.getPythonCommand()
+        pythonCommand = sys.executable #utils.getPythonCommand()
 
         command=" ".join([pythonCommand,ppserverScript,"-w",str(ntasks),"-i",remoteHostName,"-p",str(newPort),"-t","50000","-g",localenv["PYTHONPATH"],"-d"])
         utils.pickleSafeSubprocessPopen(['ssh',nodeId,"COMMAND='"+command+"'",self.runInfoDict['RemoteRunCommand']],shell=False,stdout=outFile,stderr=outFile,env=localenv)
