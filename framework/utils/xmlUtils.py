dee6f0b34c3ac1bc0c2360f64850126f74450e94
Paul W. Talbot <paul.talbot@inl.gov>
2019-05-07 09:00:20 -0600

    testing hooks

--- framework/utils/xmlUtils.py
+++ framework/utils/xmlUtils.py
@@ -414,11 +414,6 @@ class CommentedTreeBuilder(ET.TreeBuilder):
     # self._parser.CommentHandler = self.comment
 
   def comment(self, data):
-    """
-      Typifies comments in the XML tree
-      @ In, data, instance, internal ElementTree data structure
-      @ Out, None
-    """
     self.start(ET.Comment, {})
     self.data(data)
     self.end(ET.Comment)
