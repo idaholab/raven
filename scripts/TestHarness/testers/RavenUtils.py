e3474fcac8a95d0e4959a5c4c90dea31bf678050
Andrea Alfonsi <andrea.alfonsi@inl.gov>
2019-07-18 14:38:38 -0600

    modified pip list

--- scripts/TestHarness/testers/RavenUtils.py
+++ scripts/TestHarness/testers/RavenUtils.py
@@ -105,7 +105,11 @@ __pipList = [("numpy", __lookup_preferred_version("numpy")),
              ("netCDF4", __lookup_preferred_version("netCDF4")),
              ("statsmodels", __lookup_preferred_version("statsmodels")),
              ("tensorflow", __lookup_preferred_version("tensorflow", optional=True)),
-             ("pandas", __lookup_preferred_version("pandas"))]
+             ("pandas", __lookup_preferred_version("pandas")),
+             ("pylint", __lookup_preferred_version("pylint")),
+             ("psutil", __lookup_preferred_version("psutil")),
+             ("coverage", __lookup_preferred_version("coverage")),
+             ("lxml", __lookup_preferred_version("lxml"))]
 
 def module_report(module, version=''):
   """
