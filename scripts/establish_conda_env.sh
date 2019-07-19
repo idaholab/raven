5070b62b96a954bf881f74f861cbff80cf0c84c8
Andrea Alfonsi <Andrea.Alfonsi@inl.gov>
2019-07-19 16:06:41 -0600

    build_raven can detect if conda or not conda

--- scripts/establish_conda_env.sh
+++ scripts/establish_conda_env.sh
@@ -12,7 +12,6 @@
 
 ECE_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RAVEN_UTILS=${ECE_SCRIPT_DIR}/TestHarness/testers/RavenUtils.py
-
 # fail if ANYTHING this script fails (mostly, there are exceptions)
 set -e
 
@@ -39,31 +38,6 @@ function establish_OS ()
 			OSOPTION=""
 			;;
 	esac
-}
-
-function read_ravenrc ()
-{
-  # $1 should be the keyword we're looking for
-  # returns keyword argument through echo
-  ## note that "| xargs" trims leading and trailing whitespace
-  local TARGET=`echo $1 | xargs`
-  # location of the RC file
-  local RCNAME="${ECE_SCRIPT_DIR}/../.ravenrc"
-  # if the RC file exists, loop through it and read keyword arguments split by "="
-  if [ -f "$RCNAME" ]; then
-    while IFS='=' read -r KEY ARG || [[ -n "$keyarg" ]]; do
-      # trim whitespace
-      KEY=`echo $KEY | xargs`
-      ARG=`echo $ARG | xargs`
-      # check for key match
-      if [ "$KEY" = "$TARGET" ]; then
-        echo "$ARG"
-        return 0
-      fi
-    done < ${RCNAME}
-  fi
-  # if not found, return empty
-  echo ''
 }
 
 function find_conda_defs ()
@@ -224,7 +198,10 @@ function set_install_settings()
 
 
 # main
-
+# source read ravenrc script
+RAVEN_RC_SCRIPT=$ECE_SCRIPT_DIR/read_ravenrc.sh
+RAVEN_RC_SCRIPT="${RAVEN_RC_SCRIPT//\\//}"
+source $RAVEN_RC_SCRIPT
 # set default operation
 ECE_MODE=1 # 1 for loading, 2 for install, 0 for help
 INSTALL_OPTIONAL="" # --optional if installing optional, otherwise blank
