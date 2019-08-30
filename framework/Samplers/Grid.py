549d26edca1d00fe11345a152bd1c22e5abba185
Andrea Alfonsi - INL <andrea.alfonsi@inl.gov>
2019-08-30 11:30:44 -0600

    Supporting PySide2 and addressing Grid defect (#1008)
    * new verbage
    
    * Closes #1007
    
    * andrea
    
    * Closes #1003. Closes #973
    
    * fixed ascii character problem
    
    * cleaned csv loader
    
    * skiped ui
    
    * modified plugin test
    
    * initial porting to PySide2
    
    * oka
    
    * removed folder accidentally added
    
    * Scene needs to have the path item added.
    
    * Switching to Qt5 because that is the default with Pyside2.
    
    * Fixing problems with UI and python 3 compatibility.
    
    * Qt5 does not have a rotate for simple text.
    
    * Updating for python 3 and qt5/Pyside2
    
    * Use Agg backend in windows, and nothing uses self.ddata so don't save
    
    Even the docstring said it wasn't returned.
    
    * addressed Paul comment
    
    * reverted input param for global grid

--- framework/Samplers/Grid.py
+++ framework/Samplers/Grid.py
@@ -100,6 +100,7 @@ class Grid(ForwardSampler):
     if 'limit' in paramInput.parameterValues:
       self.raiseAnError(IOError,'limit is not used in Grid sampler')
     self.limit = 1
+    ##FIXME: THIS READ MORE XML MUST BE CONVERTED IN THE INPUTPARAMETER COLLECTOR!!!!!!!
     self.gridEntity._readMoreXml(xmlNode,dimensionTags=["variable","Distribution"],messageHandler=self.messageHandler, dimTagsPrefix={"Distribution":"<distribution>"})
     grdInfo = self.gridEntity.returnParameter("gridInfo")
     for axis, value in grdInfo.items():
