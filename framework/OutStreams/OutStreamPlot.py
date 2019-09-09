9441e34f80fc43b9ea54e7b07ca66a18df4db997
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2019-09-09 09:59:55 -0600

    handle_close now needed on OSX as well, so removing check that disables it.

--- framework/OutStreams/OutStreamPlot.py
+++ framework/OutStreams/OutStreamPlot.py
@@ -1901,18 +1901,15 @@ class OutStreamPlot(OutStreamManager):
     # self.plt3D.draw(self.fig.canvas.renderer)
 
     if 'screen' in self.destinations and display:
-      if platform.system() in ['Linux','Windows']:
-        # XXX For some reason, this is required on Linux, but causes
-        # OSX to fail.  Which is correct for windows has not been determined.
-        def handle_close(event):
-          """
-            This method is aimed to handle the closing of figures (overall when in interactive mode)
-            @ In, event, instance, the event to close
-            @ Out, None
-          """
-          self.fig.canvas.stop_event_loop()
-          self.raiseAMessage('Closed Figure')
-        self.fig.canvas.mpl_connect('close_event', handle_close)
+      def handle_close(event):
+        """
+        This method is aimed to handle the closing of figures (overall when in interactive mode)
+        @ In, event, instance, the event to close
+        @ Out, None
+        """
+        self.fig.canvas.stop_event_loop()
+        self.raiseAMessage('Closed Figure')
+      self.fig.canvas.mpl_connect('close_event', handle_close)
       # self.plt.pause(1e-6)
       ## The following code is extracted from pyplot.pause without actually
       ## needing to force the code to sleep, according to MPL's documentation,
