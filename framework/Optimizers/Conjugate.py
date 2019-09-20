fix the FD Central gradient

framework/Optimizers/Conjugate.py
@@ -29,6 +29,8 @@ import os
 import copy
 import numpy as np
 from numpy import linalg as LA
+from numpy import (atleast_1d, eye, mgrid, argmin, zeros, shape, squeeze,
+                   asarray, sqrt, Inf, asfarray, isinf)
 import scipy
 from scipy.optimize import minpack2
 #External Modules End--------------------------------------------------------------------------------
@@ -72,58 +74,6 @@ class ConjugateGradient(SPSA):
   ###############
   # Run Methods #
   ###############
-
-  ###################
-  # Utility Methods #
-  ###################
-  def _getPerturbationDirection(self, perturbationIndex, step = None):
-    """
-      This method is aimed to get the perturbation direction (i.e. in this case the random perturbation versor)
-      @ In, perturbationIndex, int, the perturbation index (stored in self.perturbationIndices)
-      @ In, step, int, the step index, zero indexed
-      @ Out, direction, list, the versor for each optimization dimension
-    """
-    _, varId, denoId, cdId = self._identifierToLabel(perturbationIndex)
-    direction = np.zeros(len(self.getOptVars())).tolist()
-    if cdId == 0:
-      direction[varId] = 1.0
-    else:
-      direction[varId] = -1.0
-
-    self.currentDirection = direction
-    return direction
-
-  def localEvaluateGradient(self, traj):
-    """
-      Local method to evaluate gradient.
-      @ In, traj, int, the trajectory id
-      @ Out, gradient, dict, dictionary containing gradient estimation. gradient should have the form {varName: gradEstimation}
-    """
-    gradient = {}
-    inVars = self.getOptVars()
-    opt = self.realizations[traj]['denoised']['opt'][0]
-    allGrads = self.realizations[traj]['denoised']['grad']
-    for g,pert in enumerate(allGrads):
-      varId = g % len(inVars)
-      var = inVars[varId]
-      lossDiff = mathUtils.diffWithInfinites(pert[self.objVar],opt[self.objVar])
-      # unlike SPSA, keep the loss diff magnitude so we get the exact right direction
-      if self.optType == 'max':
-        lossDiff *= -1.0
-      dh = pert[var] - opt[var]
-      if abs(dh) < 1e-15:
-        self.raiseADebug('Checking Var:',var)
-        self.raiseADebug('Opt point   :',opt)
-        self.raiseADebug('Grad point  :',pert)
-        self.raiseAnError(RuntimeError,'While calculating the gradArray a "dh" very close to zero was found for var:',var)
-      if var in gradient:
-        gradient[var] += np.atleast_1d(lossDiff / dh)
-      else:
-        gradient[var] = np.atleast_1d(lossDiff / dh)
-    print('jz is inside local gradient',gradient)
-    return gradient
-
-
   def localFinalizeActualSampling(self,jobObject,model,myInput):
       """
         Overwrite only if you need something special at the end of each run....
@@ -133,13 +83,6 @@ class ConjugateGradient(SPSA):
         @ In, myInput, list, the generating input
         @ Out, None
       """
-      #print(self.counter)
-      # {'mdlEval': 3, 'varsUpdate': [1], 'recentOptHist': {0: [{'ans': 17.071067811865476, 'x': 0.75, 'y': 0.25}, {}]}, 'persistence': {0: 0}, 'perturbation': {0: 0}, 'gradientHistory': {0: [{'x': 0.9575438204512511, 'y': -0.28828775887231545}, {}]}, 'gradNormHistory': {0: [0.001414213562373151, 0.0]}, 'solutionUpdate': {0: 1}, 'lastStepSize': {0: 0.070710678118654766}}
-
-      #
-      # print(jobObject.getMetadata())
-      # {'SampledVars': {'x': 0.36458285425512926, 'y': -0.45922995415366263}, 'SampledVarsPb': {}, 'crowDist': {}, 'prefix': '0_1_0', 'trajID': 1, 'varsUpdate': 1}
-      print('samesamesame')
       # collect finished jobs
       prefix = jobObject.getMetadata()['prefix']
       traj, step, identifier = [int(x) for x in prefix.split('_')] # FIXME This isn't generic for any prefixing system
@@ -190,18 +133,105 @@ class ConjugateGradient(SPSA):
           gradDone = all( len(self.realizations[traj]['denoised']['grad'][i]) for i in range(self.paramDict['pertSingleGrad']))
 
 
+          if not converged and optDone and gradDone:
+            print(self.realizations[traj]['denoised'])
 
 
+            ###parameter
+            gtol=1e-08
+            norm= Inf
+            eps=1.4901161193847656e-08
+            disp=True
+            maxiter=2000
+            return_all=True
+            indexmap = self.getOptVars()
+            # change xk into array with index matching the index map
+            xk = dict((var,self.realizations[traj]['denoised']['opt'][0][var]) for var in indexmap)
+            xk = np.asarray(list(xk.values()))
+            gfk = dict((var,self.localEvaluateGradient(traj)[var][0]) for var in indexmap)
+            gfk = np.asarray(list(gfk.values()))
 
+            k =0
+            old_fval = self.realizations[traj]['denoised']['opt'][0][self.objVar]
+
+            old_old_fval = old_fval + np.linalg.norm(gfk) / 2
+            allvecs = [xk]
+            pk = -gfk
+            gnorm = np.amax(np.abs(gfk))
+            sigma_3 = 0.01
+
+
+            # print('this is a test jz')
+            # print('need f')
+            # print('need fprime')
+            print('xk',xk)
+            print('gfk',gfk)
+            print('k=',k)
+            print('old_fval =',old_fval)
+            print('old_old_fval =',old_old_fval)
+            # print([gfk])
+
+            # while (gnorm > gtol) and (k < maxiter):
+            #   print('inside while')
+            #   deltak = np.dot(gfk, gfk)
+            #   print(deltak)
+            # # try:
+            #   # alpha_k, fc, gc, old_fval, old_old_fval, gfkp1 = \
+            #   #         _line_search_wolfe12(f, myfprime, xk, pk, gfk, old_fval,
+            #   #                               old_old_fval, c2=0.4, amin=1e-100, amax=1e100,
+            #   #                               extra_condition=descent_condition)
+            #   # extra_condition = self.descentCondition(alpha, xkp1, fp1, gfkp1)
+            #   # alpha_k, fc, gc, old_fval, old_old_fval, gfkp1  = line_search_wolfe1(f, fprime, xk, pk, gfk,
+            #   #                                                   old_fval, old_old_fval,**kwargs)
+            #   # newargs = args
+            #   gradient = True
+            #   gval = [gfk]
+            #   derphi0 = np.dot(gfk, pk)
+            #   # def phi(s):
+            #   #   return f(xk + s*pk)
+            #   # def derphi(s):
+            #   #   gval[0] = fprime(xk + s*pk, *newargs)
+            #   #   return np.dot(gval[0], pk)
+
+            #   # stp, fval, old_fval = scalar_search_wolfe1(
+            #   #                     phi, derphi, old_fval, old_old_fval, derphi0,
+            #   #                     c1=c1, c2=c2, amax=amax, amin=amin, xtol=xtol)
+            #   alpha1 = min(1.0, 1.01*2*(old_fval - old_old_fval)/derphi0)
+            #   phi1 = old_fval
+            #   derphi1 = derphi0
+            #   isave = np.zeros((2,), np.intc)
+            #   dsave = np.zeros((13,), float)
+            #   task = b'START'
+            #   maxiter = 10
+            #   print('ok here')
+            #   for i in range(maxiter):
+            #     print(alpha1)
+            #     print(phi1)
+            #     print(derphi1)
+            #     stp, phi1, derphi1, task = minpack2.dcsrch(alpha1, phi1, derphi1,
+            #                                       ftol=1e-4, gtol=0.4, xtol=1e-14, task = task,
+            #                                       stpmin=1e-100, stpmax=1e100, isave = isave , dsave=dsave)
+            #     print('lulueluelueluleuleuleuleuleu',stp, phi1, derphi1, task[:2])
+            #     i+=1
+            #     # if task[:2] == b'FG':
+            #     #   alpha1 = stp
+            #     #   newx = xk + stp*pk
+            #     #   phi1 = f(newx)
+            #     #   derphi1 = np.dot(fprime(newx), pk)
+            #     # else:
+            #     #   break
+            #   k += 100
+            #   # except:
+            #   #   print("An exception occurred")
 
 
 
 
 
           if not converged and optDone and gradDone:
-
+            print('this is not a test jz')
             optCandidate = self.normalizeData(self.realizations[traj]['denoised']['opt'][0])
-            print('hehehehehehehehe',self.realizations[traj]['denoised']['opt'][0],optCandidate)
+            print('not converged and optDone and gradDone optdenoised and normaled',self.realizations[traj]['denoised']['opt'][0],optCandidate)
             # update solution export
             ## only write here if we want to write on EVERY optimizer iteration (each new optimal point)
             if self.writeSolnExportOn == 'every':
@@ -228,6 +258,58 @@ class ConjugateGradient(SPSA):
               self._createPerturbationPoints(traj, new)
             # reset storage
             self._setupNewStorage(traj)
+
+  ###################
+  # Utility Methods #
+  ###################
+  def _getPerturbationDirection(self, perturbationIndex, step = None):
+    """
+      This method is aimed to get the perturbation direction (i.e. in this case the random perturbation versor)
+      @ In, perturbationIndex, int, the perturbation index (stored in self.perturbationIndices)
+      @ In, step, int, the step index, zero indexed
+      @ Out, direction, list, the versor for each optimization dimension
+    """
+    _, varId, denoId, cdId = self._identifierToLabel(perturbationIndex)
+    direction = np.zeros(len(self.getOptVars())).tolist()
+    if cdId == 0:
+      direction[varId] = 1.0
+    else:
+      direction[varId] = -1.0
+
+    self.currentDirection = direction
+    return direction
+
+  def localEvaluateGradient(self, traj):
+    """
+      Local method to evaluate gradient.
+      @ In, traj, int, the trajectory id
+      @ Out, gradient, dict, dictionary containing gradient estimation. gradient should have the form {varName: gradEstimation}
+    """
+    gradient = {}
+    gi = {}
+    inVars = self.getOptVars()
+    opt = self.realizations[traj]['denoised']['opt'][0]
+    allGrads = self.realizations[traj]['denoised']['grad']
+    for g,pert in enumerate(allGrads):
+      varId = g % len(inVars)
+      var = inVars[varId]
+      lossDiff = mathUtils.diffWithInfinites(pert[self.objVar],opt[self.objVar])
+      # unlike SPSA, keep the loss diff magnitude so we get the exact right direction
+      if self.optType == 'max':
+        lossDiff *= -1.0
+      dh = pert[var] - opt[var]
+      if abs(dh) < 1e-15:
+        self.raiseADebug('Checking Var:',var)
+        self.raiseADebug('Opt point   :',opt)
+        self.raiseADebug('Grad point  :',pert)
+        self.raiseAnError(RuntimeError,'While calculating the gradArray a "dh" very close to zero was found for var:',var)
+      if gradient.get(var) == None:
+        gi[var] = 0
+        gradient[var] = np.atleast_1d(lossDiff / dh)
+      else:
+        gi[var] += 1
+        gradient[var] = (gradient[var] + np.atleast_1d(lossDiff / dh))/(gi[var]  + 1)
+    return gradient
 
   def _newOptPointAdd(self, gradient, traj):
     """
@@ -274,10 +356,129 @@ class ConjugateGradient(SPSA):
     # modify step size based on the history of the gradients used
     frac = self.fractionalStepChangeFromGradHistory(traj)
     new = size*frac
-
-
-
-
     self.raiseADebug('step gain size for traj "{}" iternum "{}": {:1.3e} (root {:1.2e} frac {:1.2e})'.format(traj,iterNum,new,size,frac))
     self.counter['lastStepSize'][traj] = new
     return new
+
+  def _createPerturbationPoints(self, traj, optPoint, submit=True):
+    """
+      Creates perturbation points based on a provided NORMALIZED data point
+      @ In, traj, int, integer label for current trajectory
+      @ In, optPoint, dict, current optimal point near which to calculate gradient
+      @ In, submit, bool, optional, if True then submit perturbation points to queue
+      @ Out, points, list(dict), perturbation points
+    """
+    print('trajtrajtrajtraj, optPoint, submit',traj, optPoint, submit)
+    points = []
+
+    distance = self.paramDict['pertDist'] * self.counter['lastStepSize'][traj]
+    # distance = self._computePerturbationDistance(traj,self.paramDict,self.counter['varsUpdate'][traj]+1)
+    # print(' distance self.perturbationIndices',distance,self.perturbationIndices distance 2...8)
+    for i in self.perturbationIndices:
+      direction = self._getPerturbationDirection(i, step = self.counter['varsUpdate'][traj])
+      point = {}
+      index = 0
+      for var in self.getOptVars():
+        size = np.prod(self.variableShapes[var])
+        if size > 1:
+          new = np.zeros(size)
+          for v, origVal in enumerate(optPoint[var]):
+            new[v] = origVal + distance*direction[index]
+            new[v] = self._checkBoundariesAndModify(1.0, 0.0, 1.0, new[v], 0.9999, 0.0001)
+            index += 1
+          point[var] = new
+        else:
+          val = optPoint[var] + distance*direction[index]
+          val = self._checkBoundariesAndModify(1.0, 0.0, 1.0, val, 0.9999, 0.0001)
+          index += 1
+          point[var] = val
+      points.append(point)
+
+      if submit:
+        prefix = self._createEvaluationIdentifier(traj,self.counter['varsUpdate'][traj],i)
+        self.submissionQueue[traj].append({'inputs':point, 'prefix':prefix})
+    print('points',points)
+    return points
+
+
+  def localGenerateInput(self,model,oldInput):
+    """
+      Method to generate input for model to run
+      @ In, model, model instance, it is the instance of a RAVEN model
+      @ In, oldInput, list, a list of the original needed inputs for the model (e.g. list of files, etc. etc)
+      @ Out, None
+    """
+    self.readyVarsUpdate = {traj:False for traj in self.optTrajLive}
+    # GradientBasedOptimizer.localGenerateInput(self,model,oldInput)
+    # print('model, self.optTraj, self.trajCycle',model,self.optTraj,self.trajCycle)
+    # find something to submit
+    for _ in self.optTraj:
+      # get next trajectory in line, which assures each gets fair treatment in submissions
+      traj = next(self.trajCycle)
+      # if this trajectory has a run to submit, populate the submission dictionaries
+      if len(self.submissionQueue[traj]):
+        prefix, point = self.getQueuedPoint(traj)
+        for var in self.getOptVars():
+          self.values[var] = point[var]
+        self.inputInfo['prefix'] = prefix
+        self.inputInfo['trajID'] = traj+1
+        self.inputInfo['varsUpdate'] = self.counter['varsUpdate'][traj]
+        print('jzjzjzlueluekue',self.inputInfo)
+        # if we found a submission, cease looking for submissions
+        return
+    # if no submissions were found, then we shouldn't have flagged ourselves as Ready or there's a bigger issue!
+    self.raiseAnError(RuntimeError,'Attempted to generate an input but there are none queued to provide!')
+
+  def getQueuedPoint(self,traj,denorm=True):
+    """
+      Pops the first point off the submission queue (or errors if empty).  By default denormalized the point before returning.
+      @ In, traj, int, the trajectory from whose queue we should obtain an entry
+      @ In, denorm, bool, optional, if True the input data will be denormalized before returning
+      @ Out, prefix, #_#_#
+      @ Out, point, dict, {var:val}
+    """
+    try:
+      entry = self.submissionQueue[traj].popleft()
+    except IndexError:
+      self.raiseAnError(RuntimeError,'Tried to get a point from submission queue of trajectory "{}" but it is empty!'.format(traj))
+    prefix = entry['prefix']
+    point = entry['inputs']
+    print('using this newnewnew')
+    if denorm:
+      point = self.denormalizeData(point)
+    return prefix,point
+
+
+
+
+  def polakRibierePowellStep(self, alpha, gfkp1=None):
+    print('polak_ribiere_powell_step')
+    xkp1 = xk + alpha * pk
+    if gfkp1 is None:
+      # gfkp1 = myfprime(xkp1)
+      gfkp1 = approx_fprime(xkp1, f, epsilon)
+    yk = gfkp1 - gfk
+    beta_k = max(0, np.dot(yk, gfkp1) / deltak)
+    pkp1 = -gfkp1 + beta_k * pk
+    gnorm = np.amax(np.abs(gfkp1))
+    return (alpha, xkp1, pkp1, gfkp1, gnorm)
+
+  def descentCondition(self, alpha, xkp1, fp1, gfkp1):
+    print('descent_condition')
+    print(alpha, xkp1, fp1, gfkp1)
+    # Polak-Ribiere+ needs an explicit check of a sufficient
+    # descent condition, which is not guaranteed by strong Wolfe.
+    #
+    # See Gilbert & Nocedal, "Global convergence properties of
+    # conjugate gradient methods for optimization",
+    # SIAM J. Optimization 2, 21 (1992).
+    cached_step[:] = self.polakRibierePowellStep(alpha, gfkp1)
+    alpha, xk, pk, gfk, gnorm = cached_step
+
+    # Accept step if it leads to convergence.
+    if gnorm <= gtol:
+      print('acetptingac')
+      return True
+
+    # Accept step if sufficient descent condition applies.
+    return np.dot(pk, gfk) <= -sigma_3 * np.dot(gfk, gfk)

framework/Optimizers/FiniteDifference.py
@@ -115,8 +115,6 @@ class FiniteDifference(SPSA):
     inVars = self.getOptVars()
     opt = self.realizations[traj]['denoised']['opt'][0]
     allGrads = self.realizations[traj]['denoised']['grad']
-    print('opt',opt)
-    print('allGrad',allGrads)
     for g,pert in enumerate(allGrads):
       varId = g % len(inVars)
       var = inVars[varId]
@@ -130,9 +128,10 @@ class FiniteDifference(SPSA):
         self.raiseADebug('Opt point   :',opt)
         self.raiseADebug('Grad point  :',pert)
         self.raiseAnError(RuntimeError,'While calculating the gradArray a "dh" very close to zero was found for var:',var)
-      if var in gradient:
-        gradient[var] += np.atleast_1d(lossDiff / dh)
-      else:
+      if gradient.get(var) == None:
+        gi[var] = 0
         gradient[var] = np.atleast_1d(lossDiff / dh)
-    print('gggggggggg',gradient)
+      else:
+        gi[var] += 1
+        gradient[var] = (gradient[var] + np.atleast_1d(lossDiff / dh))/(gi[var]  + 1)
     return gradient

framework/Optimizers/GradientBasedOptimizer.py
@@ -399,11 +399,6 @@ class GradientBasedOptimizer(Optimizer):
       @ Out, outputs, dict, dictionary of average values
     """
     # make a place to store distinct evaluation values
-    # print('inside collectionoptput')
-    # # print(collection)
-    # # print(self.gradDict)
-    # print(self.solutionExport.asDataset())
-    # # print(self.getOptVars()) x y
     outputs = dict((var,np.zeros(self.gradDict['numIterForAve'],dtype=object))
                       for var in self.solutionExport.getVars('output')
                       if var in self.mdlEvalHist.getVars('output'))
@@ -422,7 +417,7 @@ class GradientBasedOptimizer(Optimizer):
     for var,vals in outputs.items():
       outputs[var] = vals.mean()
     outputs.update(inputs)
-    print('average output',outputs)
+    # print('average output',outputs)
     return outputs
 
   def calculateMultivectorMagnitude(self,values):
@@ -655,6 +650,7 @@ class GradientBasedOptimizer(Optimizer):
       #entries into the queue are as {'inputs':{var:val}, 'prefix':runid} where runid is <traj>_<varUpdate>_<evalNumber> as 0_0_2
       nPoint = {'inputs':copy.deepcopy(point)} #deepcopy to prevent simultaneous alteration
       nPoint['prefix'] = self._createEvaluationIdentifier(traj,self.counter['varsUpdate'][traj],i) # from 0 to self.gradDict['numIterForAve'] are opt point evals
+      # this submission queue only have the denoise number of opt point
       self.submissionQueue[traj].append(nPoint)
 
   def _removeRedundantTraj(self, trajToRemove, currentInput):