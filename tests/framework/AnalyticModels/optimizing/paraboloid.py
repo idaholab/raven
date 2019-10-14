9a8ed7e239347e1f6f859912247939a057ba40a3
ZHOUJ2 <jiazhouinl@gmail.com>
2019-10-14 17:30:41 -0600

    requested changes

--- tests/framework/AnalyticModels/optimizing/paraboloid.py
+++ tests/framework/AnalyticModels/optimizing/paraboloid.py
@@ -11,9 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# takes input parameters x,y
+# returns value in "ans"
+# elliptic paraboloid function with a single global minimum
+# optimal minimum at f(-0.5, 0.5) = 0
+# parameter range is -2 <= x <= 2, -2 <= y <= 2
 import numpy as np
 
 def run(raven, Inputs):
+  """
+    External model to evaluates 10*(x+0.5)**2 + 10*(y-0.5)**2.
+    @ In, raven, object, container from RAVEN to store members on
+    @ In, Input, dict, dictionary containing inputs from RAVEN
+    @ Out, None
+  """
   x = raven.x
   y = raven.y
   raven.ans = main(x, y)
