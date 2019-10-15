3dad867ff4888626a17ed74010be7ce1d151f308
Paul W. Talbot <paul.talbot@inl.gov>
2019-10-15 14:49:15 -0600

    xpath unnecessary change reverted

--- framework/utils/xmlUtils.py
+++ framework/utils/xmlUtils.py
@@ -210,9 +210,7 @@ def findPath(root, path):
   """
   assert('|' not in path), 'Update XML search to use XPATH syntax!'
   # edit tags for allowable characters
-  # TODO this breaks for xpath searches using attributes!
-  if '[' not in path:
-    path = fixTagsInXpath(path)
+  path = fixTagsInXpath(path)
   found = root.findall(path)
   if len(found) < 1:
     return None
