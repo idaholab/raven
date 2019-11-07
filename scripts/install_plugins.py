e057cef73ff6cac1c9f64ccac6e2aea69cc15431
Paul W. Talbot <paul.talbot@inl.gov>
2019-11-07 14:54:12 -0700

    stash

--- scripts/install_plugins.py
+++ scripts/install_plugins.py
@@ -24,9 +24,7 @@ import os
 import sys
 import shutil
 import argparse
-frameworkDir = os.path.join(os.path.dirname(__file__), '..', 'framework')
-sys.path.append(frameworkDir)
-from utils import xmlUtils
+import plugin_handler as pluginHandler
 
 # set up command-line arguments
 parser = argparse.ArgumentParser(prog="RAVEN Plugin Installer",
@@ -41,89 +39,6 @@ parser.add_argument('-e', '--exclude', dest='exclude', action='append',
                     help='exclude designated folders from loading in RAVEN.')
 args = parser.parse_args()
 
-requiredDirs = ['src', 'doc', 'tests']
-
-def checkValidPlugin(rawLoc):
-  """
-    Checks that the plugin at "loc" is a valid one.
-    @ In, rawLoc, str, path to plugin (possibly relative)
-    @ Out, okay, bool, whether the plugin looks fine
-    @ Out, msgs, list(str), error messages collected during check.
-    @ Out, loc, str, absolute path to plugin
-  """
-  okay = True
-  msgs = []
-  loc = os.path.abspath(os.path.expanduser(rawLoc))
-  # check existence
-  if not os.path.isdir(loc):
-    okay = False
-    msgs.append('Not a valid directory: {}'.format(loc))
-  # check for source, doc, tests dirs
-  missing = []
-  for needDir in requiredDirs:
-    fullDir = os.path.join(loc, needDir)
-    if not os.path.isdir(fullDir):
-      missing.append(needDir)
-  if missing:
-    okay = False
-    for m in missing:
-      msgs.append('Required directory missing in {}: {}'.format(fullDir, m))
-
-  return okay, msgs, loc
-
-def _writeNewPluginXML(name, location):
-  """
-    Writes plugin information to XML
-    @ In, name, str, name of plugin
-    @ In, location, str, directory of plugin
-    @ Out, new, xml.etree.ElementTree.Element, new plugin information in xml
-  """
-  new = xmlUtils.newNode('plugin')
-  new.append(xmlUtils.newNode('name', text=name))
-  new.append(xmlUtils.newNode('location', text=location))
-  # TODO read a config file IN THE PLUGIN to determine what nodes it should include
-  ## for example, executable, default excluded dirs, etc
-  new.append(xmlUtils.newNode('exclude'))
-  return new
-
-def _updatePluginXML(root, name, location):
-  """
-    Update an existing plugin entry with new information
-    @ In, root, xml.etree.ElementTree.Element, root of plugin tree
-    @ In, name, str, name of plugin
-    @ In, location, str, location of plugin on disk
-    @ Out, match, xml.etree.ElementTree.Element, updated element
-  """
-  match = root.findall('./plugin/name[.=\'{}\']/..'.format(name))[0]
-  oldPath = match.find('location').text
-  # nothing to do if old path and new path are the same!
-  if oldPath != location:
-    # TODO overwrite or not as an option?
-    print('Updating existing location of plugin "{}" from "{}" to "{}"'.format(name, oldPath, location))
-    match.find('location').text = location
-  return match
-
-def _tellPluginAboutRaven(name, loc):
-  """
-    Informs plugin about raven framework location
-    @ In, name, str, name of plugin
-    @ In, loc, location of plugin
-    @ Out, None
-  """
-  # check for config file; load up a root element either way
-  configFile = os.path.join(loc, '.ravenconfig')
-  if os.path.isfile(configFile):
-    root, _ = xmlUtils.loadToTree(configFile)
-  else:
-    root = xmlUtils.newNode('RavenConfig')
-  # add raven information
-  ravenLoc = root.find('FrameworkLocation')
-  if ravenLoc is None:
-    ravenLoc = xmlUtils.newNode('FrameworkLocation')
-    root.append(ravenLoc)
-  ravenLoc.text = os.path.abspath(os.path.expanduser(frameworkDir))
-  xmlUtils.toFile(configFile, root)
-
 if __name__ == '__main__':
   ### Design notes
   # "Installing" is actually just the process of registering the location of the plugin
@@ -137,8 +52,9 @@ if __name__ == '__main__':
   # check legitimacy of plugin directories
   for s, sourceDir in enumerate(args.source_dir):
     loc = os.path.abspath(sourceDir)
-    okay, msgs, newLoc = checkValidPlugin(loc)
+    okay, msgs, newLoc = pluginHandler.checkValidPlugin(loc)
     if okay:
+      print(' ... plugin located at "{}" ...'.format(newLoc))
       sources.append(newLoc)
     else:
       print('ERROR: Plugin at "{}" had the following error(s):')
@@ -152,30 +68,32 @@ if __name__ == '__main__':
     #       register the plugin in the plugin_directory as being in the plugins folder
     raise NotImplementedError
 
-  # load sources
-  pluginTreeFile = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'plugins', 'plugin_directory.xml'))
-  if os.path.isfile(pluginTreeFile):
-    root, _ = xmlUtils.loadToTree(pluginTreeFile)
-  else:
-    root = xmlUtils.newNode('plugins')
+  infoFile, root = pluginHandler.loadPluginTree()
 
   # add or update plugins from sources
-  existing = [x.text.strip() for x in root.findall('./plugin/name')]
+  existing, _ = zip(*pluginHandler.getInstalledPlugins())
   for plugDir in sources:
     name = os.path.basename(plugDir)
     if name in existing:
-      match = _updatePluginXML(root, name, plugDir)
+      match = pluginHandler.updatePluginXML(root, name, plugDir)
+      print(' ... plugin "{}" path updated to "{}" ...'.format(name, plugDir))
     else:
       # create a new entry
-      new = _writeNewPluginXML(name, plugDir)
+      new = pluginHandler.writeNewPluginXML(name, plugDir)
+      print(' ... plugin "{}" path created as "{}" ...'.format(name, plugDir))
       root.append(new)
       match = new
     if args.exclude:
       match.find('exclude').text = ','.join(x for x in args.exclude)
     # tell plugin about RAVEN
-    _tellPluginAboutRaven(name, plugDir)
-  xmlUtils.toFile(pluginTreeFile, root, pretty=True)
+    rLoc = pluginHandler.tellPluginAboutRaven(plugDir)
+    print(' ... plugin "{}" informed of RAVEN at "{}"...'.format(name, rLoc))
+    ## TODO testing?
+    print(' ... plugin "{}" succesfully installed!'.format(name))
+  pluginHandler.writePluginTree(infoFile, root)
 
+  # XXX DEBUGG testing
+  print(pluginHandler.getPluginLocation('CashFlow'))
 
 
 
