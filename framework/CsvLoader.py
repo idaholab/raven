9e086ab460251ff99085ecb55e37712a7aaadeaf
Paul W. Talbot <paul.talbot@inl.gov>
2020-07-07 16:05:34 -0600

    two options for loading now, pandas by default and numpy if needed

--- framework/CsvLoader.py
+++ framework/CsvLoader.py
@@ -23,6 +23,7 @@ from __future__ import division, print_function, unicode_literals, absolute_impo
 
 #External
 #Modules------------------------------------------------------------------------------------
+import numpy as np
 import pandas as pd
 #External Modules End--------------------------------------------------------------------------------
 
@@ -35,6 +36,8 @@ class CsvLoader(MessageHandler.MessageUser):
   """
     Class aimed to load the CSV files
   """
+  acceptableUtils = ['pandas', 'numpy']
+
   def __init__(self, messageHandler):
     """
       Constructor
@@ -47,7 +50,24 @@ class CsvLoader(MessageHandler.MessageUser):
     self.allFieldNames = []               # "header" of the CSV file
     self.messageHandler = messageHandler  # message handling utility
 
-  def loadCsvFile(self, myFile, nullOK=None):
+  def loadCsvFile(self, myFile, nullOK=None, utility='pandas'):
+    """
+      Function to load a csv file into realization format
+      It also retrieves the headers
+      The format of the csv must be comma-separated (pandas readable)
+      @ In, myFile, string, Input file name (absolute path)
+      @ In, nullOK, bool, indicates if null values are acceptable
+      @ In, utility, str, indicates which utility should be used to load the csv
+      @ Out, loadCsvFile, pandas.DataFrame or numpy.ndarray, the loaded data
+    """
+    if utility == 'pandas':
+      return self._loadCsvPandas(myFile, nullOK=nullOK)
+    elif utility == 'numpy':
+      return self._loadCsvNumpy(myFile, nullOK=nullOK)
+    else:
+      self.raiseAnError(RuntimeError, f'Unrecognized CSV loading utility: "{utility}"')
+
+  def _loadCsvPandas(self, myFile, nullOK=None):
     """
       Function to load a csv file into realization format
       It also retrieves the headers
@@ -71,6 +91,35 @@ class CsvLoader(MessageHandler.MessageUser):
       self.raiseAnError(IOError, f'Invalid data in input file: row "{bad+1}" in "{myFile}"')
     self.allFieldNames = list(df.columns)
     return df
+
+  def _loadCsvNumpy(self, myFile, nullOK=None):
+    """
+      Function to load a csv file into realization format
+      It also retrieves the headers
+      The format of the csv must be comma-separated with all floats after header row
+      @ In, myFile, string, Input file name (absolute path)
+      @ In, nullOK, bool, indicates if null values are acceptable
+      @ Out, data, np.ndarray, the loaded data
+    """
+    with open(myFile, 'rb') as f:
+      head = f.readline().decode()
+    self.allFieldNames = list(x.strip() for x in head.split(','))
+    data = np.loadtxt(myFile, dtype=float, delimiter=',', ndmin=2, skiprows=1)
+    return data
+
+  def toRealization(self, data):
+    """
+      Converts data from the "loadCsvFile" format to a realization-style format (dictionary
+      currently)
+      @ In, data, pandas.DataFrame or np.ndarray, result of loadCsvFile
+      @ Out, rlz, dict, realization
+    """
+    rlz = {}
+    if isinstance(data, pd.DataFrame):
+      rlz = dict((header, np.array(data[header])) for header in self.allFieldNames)
+    elif isinstance(data, np.ndarray):
+      rlz = dict((header, entry) for header, entry in zip(self.allFieldNames, data.T))
+    return rlz
 
   def getAllFieldNames(self):
     """
