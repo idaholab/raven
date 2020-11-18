c4a31d0f627f35a9238a0b6dd6c8710456fc37d9
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2020-11-18 11:50:55 -0700

    stripping out python libraries.
    If they match the current python, they will be added back in.
    If they do not match, they will cause all kinds of trouble.

--- framework/RemoteNodeScripts/server_start.py
+++ framework/RemoteNodeScripts/server_start.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys, os, subprocess
+import sys, os, subprocess, re
 # get working dir
 workingDir=sys.argv[1]
 # change working dir
@@ -15,6 +15,18 @@ out = open(output, "a")
 print("working dir:", workingDir, file=out)
 print("command    :", command, file=out)
 print("pythonpath :", pythonpath, file=out)
+if re.search("lib.python", pythonpath):
+  #strip out python libraries from path
+  #XXX ideally, this would have a way to tell if
+  # the paths we are stripping are the real builtin python paths
+  # instead of just using a regular expression
+  splitted=pythonpath.split(os.pathsep)
+  newpath = []
+  for part in splitted:
+    if not re.search("lib.python", part):
+      newpath.append(part)
+  pythonpath = os.pathsep.join(newpath)
+  print("adjusted pythonpath:",pythonpath)
 os.environ["PYTHONPATH"] = pythonpath
 
 #Close standard in, out, and error
@@ -22,4 +34,5 @@ os.environ["PYTHONPATH"] = pythonpath
 for f in [0,1,2]:
   os.close(f)
 
+out.close()
 subprocess.Popen(command)
