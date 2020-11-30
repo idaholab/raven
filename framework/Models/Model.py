62242dbce73e160f7957e58c20a1110b7f186416
Dylan McDowell <dylanjm@users.noreply.github.com>
2020-11-30 11:22:42 -0700

    Try Except Block on Model.py found var

--- framework/Models/Model.py
+++ framework/Models/Model.py
@@ -271,7 +271,10 @@ class Model(utils.metaclass_insert(abc.ABCMeta,BaseType),Assembler):
       for varFramework,varModel in self.alias[aliasTyp].items():
         whichVar =  varModel if fromModelToFramework else varFramework
         notFound = 2**62
-        found = sampledVars.pop(whichVar,[notFound])
+        try:
+            found = sampledVars.pop(whichVar,[notFound])
+        except AttributeError:
+            found = [notFound]
         if not np.array_equal(np.asarray(found), [notFound]):
           if fromModelToFramework:
             sampledVars[varFramework] = originalVariables[varModel]
