19e989b959d63bddf6c93efca528aab0cc8fc3f8
alfoa <andrea.alfonsi@inl.gov>
2020-12-02 15:00:12 -0700

    added test for RELAP5 dynamic event tree

--- tests/framework/CodeInterfaceTests/RELAP5/RELAP5interfaceDET/LPI_B_time.py
+++
@@ -1,7 +0,0 @@
-def evaluate(self):
-    if self.N_LPI==0:
-        return 86400.0
-    elif self.N_LPI==1:
-        return 86400.0
-    elif self.N_LPI==2:
-        return self.LPI_B_time
--- tests/framework/CodeInterfaceTests/RELAP5/RELAP5interfaceDET/LPR_A_time.py
+++
@@ -1,8 +0,0 @@
-def evaluate(self):
-    self.N_LP = min(self.N_LPR,self.N_LPI)
-    if self.N_LP==0:
-        return 86400.0
-    elif self.N_LP==1:
-        return self.LPR_A_time
-    elif self.N_LP==2:
-        return self.LPR_A_time
