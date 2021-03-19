d6cb8151d00d9caeb7af88ef17ecc62b9cbb9021
yoshiurr-INL <78757932+yoshiurr-INL@users.noreply.github.com>
2021-03-19 11:54:42 -0700

    Update __init__.py
    Added PPDSSv2

--- framework/PostProcessors/__init__.py
+++ framework/PostProcessors/__init__.py
@@ -51,6 +51,7 @@ from .ComparisonStatisticsModule import ComparisonStatistics
 from .RealizationAverager import RealizationAverager
 from .ParetoFrontierPostProcessor import ParetoFrontier
 from .MCSimporter import MCSImporter
+from .PPDSSv2 import PPDSSv2
 # from .RavenOutput import RavenOutput # deprecated for now
 
 additionalModules = []
@@ -90,5 +91,6 @@ __all__ = ['PostProcessor',
            'SampleSelector',
            'ETImporter',
            'RealizationAverager',
+           'PPDSSv2',
            'ParetoFrontier'] + additionalModules
 #           'RavenOutput', # deprecated for now
