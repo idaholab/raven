dd437f28992dc7b6584e9c2af5add6460f8b6d38
Wang, Congjian <congjian.wang@inl.gov>
2021-04-13 13:26:31 -0600

    postprocessor plugin API

--- framework/Models/PostProcessors/ETImporter.py
+++ framework/Models/PostProcessors/ETImporter.py
@@ -16,24 +16,13 @@ Created on Nov 1, 2017
 
 @author: dan maljovec, mandd
 """
-
-#External Modules---------------------------------------------------------------
-import numpy as np
-import xml.etree.ElementTree as ET
-import copy
-#External Modules End-----------------------------------------------------------
-
 #Internal Modules---------------------------------------------------------------
-from .PostProcessorInterface import PostProcessorInterface
 from utils import InputData, InputTypes
-from utils import xmlUtils as xmlU
-from utils import utils
-import Files
+from PluginsBaseClasses.PostProcessorPluginBase import PostProcessorPluginBase
 from .ETStructure import ETStructure
 #Internal Modules End-----------------------------------------------------------
 
-
-class ETImporter(PostProcessorInterface):
+class ETImporter(PostProcessorPluginBase):
   """
     This is the base class of the PostProcessor that imports Event-Trees (ETs) into RAVEN as a PointSet
   """
@@ -71,16 +60,6 @@ class ETImporter(PostProcessorInterface):
     inputSpecification.addSub(InputData.parameterInputFactory("expand"    , contentType=InputTypes.BoolType))
     return inputSpecification
 
-  def initialize(self, runInfo, inputs, initDict) :
-    """
-      Method to initialize the pp.
-      @ In, runInfo, dict, dictionary of run info (e.g. working dir, etc)
-      @ In, inputs, list, list of inputs
-      @ In, initDict, dict, dictionary with initialization options
-      @ Out, None
-    """
-    super().initialize(runInfo, inputs, initDict)
-
   def _handleInput(self, paramInput):
     """
       Method that handles PostProcessor parameter input block.
@@ -91,8 +70,7 @@ class ETImporter(PostProcessorInterface):
     fileFormat = paramInput.findFirst('fileFormat')
     self.fileFormat = fileFormat.value
     if self.fileFormat not in self.allowedFormats:
-      self.raiseAnError(IOError, 'ETImporterPostProcessor Post-Processor ' + self.name + ', format ' + str(self.fileFormat) + ' : is not supported')
-
+      self.raiseAnError(IOError, 'ETImporter Post-Processor ' + self.name + ', format ' + str(self.fileFormat) + ' : is not supported')
     expand = paramInput.findFirst('expand')
     self.expand = expand.value
 
@@ -106,14 +84,3 @@ class ETImporter(PostProcessorInterface):
     outputDict, variables = eventTreeModel.returnDict()
     outputDict = {'data': outputDict, 'dims':{}}
     return outputDict
-
-  def collectOutput(self, finishedJob, output, options=None):
-    """
-      Function to place all of the computed data into the output object, (DataObjects)
-      @ In, finishedJob, object, JobHandler object that is in charge of running this PostProcessor
-      @ In, output, object, the object where we want to place our computed results
-      @ In, options, dict, optional, not used in PostProcessor.
-        dictionary of options that can be passed in when the collect of the output is performed by another model (e.g. EnsembleModel)
-      @ Out, None
-    """
-    super().collectOutput(finishedJob, output, options=options)
---
+++ framework/PluginsBaseClasses/PostProcessorPluginBase.py
@@ -0,0 +1,127 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Created on March 10, 2021
+
+@author: wangc
+"""
+
+#External Modules---------------------------------------------------------------
+import abc
+#External Modules End-----------------------------------------------------------
+
+#Internal Modules---------------------------------------------------------------
+import Files
+from utils import InputData, InputTypes
+from DataObjects import DataObject
+from Databases import Database
+from .PluginBase import PluginBase
+from Models.PostProcessors.PostProcessorInterface import PostProcessorInterface
+#Internal Modules End-----------------------------------------------------------
+
+class PostProcessorPluginBase(PostProcessorInterface, PluginBase):
+  """
+    This class represents a specialized class from which each PostProcessor plugins must inherit from
+  """
+  # List containing the methods that need to be checked in order to assess the
+  # validity of a certain plugin. This list needs to be populated by the derived class
+  _methodsToCheck = ['getInputSpecification', '_handleInput', 'run']
+  entityType = 'PostProcessor'
+
+  ##################################################
+  # Methods for Internal Use
+  ##################################################
+  def createNewInput(self,inputObjs,samplerType,**kwargs):
+    """
+      This function is used to convert internal DataObjects to user-friendly format of data.
+      The output from this function will be directly passed to the "run" method.
+      @ In, inputObjs, list, list of DataObjects
+      @ In, samplerType, string, is the type of sampler that is calling to generate a new input.
+          Not used for PostProcessor, and "None" is used during "Step" "PostProcess" handling
+      @ In, **kwargs, dict, is a dictionary that contains the information coming from the sampler,
+          a mandatory key is the sampledVars'that contains a dictionary {'name variable':value}.
+          Not used for PostProcessor, and {'SampledVars':{'prefix':'None'}, 'additionalEdits':{}}
+          is used during "Step" "PostProcess" handling
+      @ Out, inputDs, list, list of data set that will be directly used by the "PostProcessor.run" method.
+    """
+    #### TODO: This method probably need to move to PostProcessor Base Class when we have converted
+    #### all internal PostProcessors to use Dataset
+
+    ## Type 1: DataObjects => Dataset
+    ## Type 2: File => File
+    ## Type 3: HDF5 => ?
+    assert type(inputObjs) == list
+    inputDs = []
+    for inp in inputObjs:
+      if isinstance(inp, Files.File):
+        inputDs.append(inp)
+      elif isinstance(inp, DataObject.DataObject):
+        # convert to xarray.Dataset
+        inputDs.append(inp.asDataset())
+      elif isinstance(inp, Database):
+        self.raiseAnError(IOError, "Database", inp.name, "can not be handled directly by this Post Processor")
+      else:
+        self.raiseAnError(IOError, "Unknown input is found", str(inp))
+    return inputDs
+
+
+  ##################################################
+  # Plugin APIs
+  ##################################################
+  @classmethod
+  def getInputSpecification(cls):
+    """
+      Method to get a reference to a class that specifies the input data for
+      class cls.
+      @ In, cls, the class for which we are retrieving the specification
+      @ Out, inputSpecification, InputData.ParameterInput, class to use for
+        specifying input of cls.
+    """
+    inputSpecification = super().getInputSpecification()
+    return inputSpecification
+
+  def __init__(self):
+    """
+      Constructor
+      @ In, None
+      @ Out, None
+    """
+    super().__init__()
+
+  def initialize(self, runInfo, inputs, initDict=None):
+    """
+      This function is used to initialize the plugin, i.e. set up working dir,
+      call the initializePlugin method from the plugin
+      @ In, runInfo, dict, it is the run info from the jobHandler
+      @ In, inputs, list, it is a list containing whatever is passed with an input role in the step
+      @ In, initDict, dict, optional, dictionary of all objects available in the step is using this model
+    """
+    super().initialize(runInfo, inputs, initDict)
+
+  def _handleInput(self, paramInput):
+    """
+      Function to handle the common parts of the model parameter input.
+      @ In, paramInput, InputData.ParameterInput, the already parsed input.
+      @ Out, None
+    """
+    super()._handleInput(paramInput)
+
+  ### "run" is required for each specific PostProcessor, it is an abstractmethod in
+  ### PostProcessorInterface base class.
+  # def run(self, inputDs):
+  #   """
+  #     This method executes the postprocessor action.
+  #     @ In,  inputDs, list, list of Datasets
+  #     @ Out, outputDs, dict, xarray.Dataset, pd.DataFrame
+  #   """
