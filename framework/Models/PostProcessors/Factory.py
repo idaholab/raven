cfb57b0122c34ed3d565cb261865ffac62267364
Joshua J. Cogliati <joshua-cogliati-inl@users.noreply.github.com>
2021-04-28 12:39:30 -0600

    Add Lagged Variable PostProcessor (#1523)
    * Addition of the Lagged PostProcessor capability for artificially add "memory" in datasets.
    
    * Adding start of test input.
    
    * Adding skeleton HistorySetDelay class.
    
    * Implementing delayed parameters.
    
    * Adding HistorySetDelay test.
    
    * Adding documentation of HistorySetDelay
    
    * Failed attempt to convert to a post processor.
    
    * fixed PP for Josh
    
    * Changing to only set default on un-overwritten points.
    
    * Moving out of interfaced tests since it is no longer one.
    
    * Updating documentation with more detail.
    
    * Improving documentation.
    
    * Fixing review comments.
    
    Co-authored-by: alfoa <andrea.alfonsi@inl.gov>

--- framework/Models/PostProcessors/Factory.py
+++ framework/Models/PostProcessors/Factory.py
@@ -40,6 +40,7 @@ from .ParetoFrontierPostProcessor import ParetoFrontier
 from .MCSimporter import MCSImporter
 from .EconomicRatio import EconomicRatio
 from .Validation import Validation
+from .HistorySetDelay import HistorySetDelay
 ## These utilize the optional prequisite library PySide, so don't error if they
 ## do not import appropriately.
 try:
