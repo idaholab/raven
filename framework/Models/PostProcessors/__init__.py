b34ac218754182a6b355b35068c9b68570495fa2
Paul W. Talbot <paul.talbot@inl.gov>
2021-05-03 08:52:30 -0600

    docstring for pp module

--- framework/Models/PostProcessors/__init__.py
+++ framework/Models/PostProcessors/__init__.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+  Module definition for postprocessor interfaces.
+"""
+
 from .Factory import factory
