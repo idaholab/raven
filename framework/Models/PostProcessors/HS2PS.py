f9566e9dd6c9e689bd758ca8cabc5235f732820d
Wang, Congjian <congjian.wang@inl.gov>
2021-05-17 21:41:31 -0600

    add date to HS2PS

--- framework/Models/PostProcessors/HS2PS.py
+++ framework/Models/PostProcessors/HS2PS.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
+Created on Feb 04, 2016
 @author: mandd, wangc
 """
 #External Modules------------------------------------------------------------------------------------
