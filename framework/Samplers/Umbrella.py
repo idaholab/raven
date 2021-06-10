864a183fa6af70868950cc811e4d34282946522c
Tann10 <kavathekar.tanaya@gmail.com>
2021-06-09 19:42:01 -0700

    variable names refactored and tested code for n dimensions

--- framework/Samplers/Umbrella.py
+++ framework/Samplers/Umbrella.py
@@ -52,7 +52,6 @@ class Umbrella(ForwardSampler):
       @ Out, inputSpecification, InputData.ParameterInput, class to use for
         specifying input of cls.
     """
-    print("In this class!!!!!")
     inputSpecification = super(Umbrella, cls).getInputSpecification()
     umbrellaInput = InputData.parameterInputFactory("umbrella", contentType=InputTypes.StringType)
     samplerInitInput = InputData.parameterInputFactory("samplerInit")
@@ -105,194 +104,180 @@ class Umbrella(ForwardSampler):
     self.dimension = paramInput.findFirst('samplerInit').findFirst('dimensions').value
     self.weightTarget = paramInput.findFirst('samplerInit').findFirst('weightTarget').value
 
-    # if paramInput.findFirst('samplerInit') != None:
-    #   if self.limit is None:
-    #     self.raiseAnError(IOError,self,'Umbrella sampler '+self.name+' needs the limit block (number of samples) in the samplerInit block')
-    #   if paramInput.findFirst('samplerInit').findFirst('samplingType')!= None:
-    #     self.samplingType = paramInput.findFirst('samplerInit').findFirst('samplingType').value
-    #     if self.samplingType not in ['uniform']:
-    #       self.raiseAnError(IOError,self,'Umbrella sampler '+self.name+': specified type of samplingType is not recognized. Allowed type is: uniform')
-    #   else:
-    #     self.samplingType = None
-    # else:
-    #   self.raiseAnError(IOError,self,'Umbrella sampler '+self.name+' needs the samplerInit block')
+    if paramInput.findFirst('samplerInit') != None:
+      if self.limit is None:
+        self.raiseAnError(IOError,self,'Umbrella sampler '+self.name+' needs the limit block (number of samples) in the samplerInit block')
+      if paramInput.findFirst('samplerInit').findFirst('samplingType')!= None:
+        self.samplingType = paramInput.findFirst('samplerInit').findFirst('samplingType').value
+        if self.samplingType not in ['uniform']:
+          self.raiseAnError(IOError,self,'Umbrella sampler '+self.name+': specified type of samplingType is not recognized. Allowed type is: uniform')
+      else:
+        self.samplingType = None
+    else:
+      self.raiseAnError(IOError,self,'Umbrella sampler '+self.name+' needs the samplerInit block')
 
-
-  def get_std(self, k, sigma):
-    ## TODO parameterized tail probability
+  def getStd(self, k, sigma):
+    """
+         Method to get alpha and beta values of gamma distribution based on mode location, standard deviation and tail probability
+         @ In, k, mode location of the gamma distribution specified by the user
+         @ In, tail, tail probability of the distribution
+         @ Out, sigma, standard distribution of the gamma distribution
+    """
     beta = (k + np.sqrt(k ** 2 + 4 * sigma ** 2)) / (2 * sigma ** 2)
     alpha = beta * k + 1
     return (scipy.stats.gamma.cdf(k,
                                   a=alpha,
-                                  scale=1 / beta) - 0.4)
+                                  scale=1 / beta) - (1- self.tailProb))
 
-  def tail_prob_gamma(self, k, sigma):
+  def tailProbabilityGamma(self, k, sigma):
+    """
+         Method to collect parameters of gamma distribution in a dictionary
+         @ In, k, mode location of the gamma distribution specified by the user
+         @ In, sigma, standard distribution of the gamma distribution
+         @ Out, dictionary of gamma parameters
+    """
     D = k ** 2 + 4 * sigma ** 2
     beta = (k + D ** 0.5) / (2 * sigma ** 2)
     alpha = beta * k + 1
     value = 1 - gamma.cdf(k, scale=1 / beta, a=alpha)
     return {'alpha': alpha, 'beta': beta, 'tailprob': value,
             'k': k, 'sigma': sigma}
-  # Function for Non stratified importance sampling
-  def importance_sample(self, importance_samples):
+
+  def getDiscreteSamples(self, n, the_w):
     """
-
-    :param importance_samples:
-    :return:
+         Method to sample from a discrete distribution
+         @ In, n, samples size
+         @ In, the_w, weights of the mixture distribution
+         @ Out, dataframe, samples from a discrete distribution
     """
-    # importance_dist = norm(appro_mu, appro_sigma)
-    # target_dist = norm(target_mu, target_sigma)
-
-    importance_weights = self.distDict['target'].pdf(
-      importance_samples) / self.distDict['importance'].pdf(importance_samples)
-
-    sample_df = pd.DataFrame({'sample': importance_samples,
-                              'weights': importance_weights})
-    return sample_df
-
-  # Function to evaluate a multivariate gamma tail pdf in all hyper-quadrants with
-  # each tail being given a weight w. These weights need to sum to one.
-  def mv_gamma_tail_pdf(self, x, vertWeights):
-
-    n_vertices = len(vertWeights['vert'])
-    n_row = len(x)
-    pdf_values = np.zeros(n_row)
-
-    for i in range(n_row):
-      the_x = x[i, :]
-      total = 0
-
-      for k in range(n_vertices):
-        vertex = vertWeights['vert'][k]
-        x_vertex = the_x - vertex
-        x_vertex = x_vertex * vertex
-        marg_values = scipy.stats.gamma.pdf(x_vertex, scale=1 / self.tail_prob ['beta'], a=self.tail_prob ['alpha'])
-        prod_values = reduce(operator.mul, marg_values, 1)
-        total = total + vertWeights['w'][k] * prod_values
-      pdf_values[i] = total
-    return pd.DataFrame([x[:, 0], x[:, 1], pdf_values]).T
-
-  # Function to sample from a discrete distribution
-  def rdiscrete(self, n, the_w):
     P = np.cumsum(the_w)
     m = len(the_w)
     x = np.arange(1, m + 1)
-    unif_sample = np.random.uniform(size=n)
-    discrete_sample = np.zeros(n)
+    uniformSample = np.random.uniform(size=n)
+    discreteSample = np.zeros(n)
     for i in range(m - 1, -1, -1):
-      discrete_sample[unif_sample < P[i]] = x[i]
-    return discrete_sample
+      discreteSample[uniformSample < P[i]] = x[i]
+    return discreteSample
 
-  # Function to create the vertices of a K dimensional unit square
-  def Construct_Corner_Points(self, dimensions):
-    n_rows = 2 ** dimensions
-    vertices = np.zeros(shape=(n_rows, dimensions))
-    for i in range(1, n_rows + 1):
-      for j in range(1, dimensions + 1):
+  def constructCornerPoints(self):
+    """
+         Method to create the vertices of a K dimensional unit square
+         @ In, dimensions, dimensions specified by the user
+         @ Out, dataframe, vertices in K dimensional unit square
+    """
+    nrows = 2 ** self.dimension
+    vertices = np.zeros(shape=(nrows, self.dimension))
+    for i in range(1, nrows + 1):
+      for j in range(1, self.dimension + 1):
         power = math.floor((i - 1) / 2 ** (j - 1))
         vertices[i - 1, j - 1] = (-1) ** (power + 2)
     return vertices
 
-  def multivariate_umbrella_sample(self, n, w, p, k, mean, Pearsons_Rho_Matrix, vertWeights):
+  def gammaTailPDF(self, x, vertWeights):
+    """
+         Method to evaluate a multivariate gamma tail pdf in all hyper-quadrants with
+         each tail being given a weight w. These weights need to sum to one.
+         @ In, x, samples
+         @ In, vertWeights, weights of the mixture distribution
+         @ Out, dataframe, dataframe of tail samples and associated probability values
+    """
+    nvertices = len(vertWeights['vert'])
+    nrow = len(x)
+    pdfValues = np.zeros(nrow)
 
-    dimension = 2
-    # n_tails = 2 ** dimension
-    # tail_wts = (1 - w) / n_tails
-    # wts_list = [tail_wts for i in range(0, n_tails)]
-    # wts_list.insert(0, w)
-    wts_list = vertWeights['w']
-    wts_list = np.insert(wts_list, 0, w)
-    vertices = vertWeights['vert']
-    memb_sample = self.rdiscrete(n, wts_list)
-    mixt_sample = np.zeros((n, 2))
+    for i in range(nrow):
+      the_x = x[i, :]
+      total = 0
 
+      for k in range(nvertices):
+        vertex = vertWeights['vert'][k]
+        x_vertex = the_x - vertex
+        x_vertex = x_vertex * vertex
+        marginalValues = scipy.stats.gamma.pdf(x_vertex, scale=1 / self.tailProb['beta'], a=self.tailProb['alpha'])
+        prodValues = reduce(operator.mul, marginalValues, 1)
+        total = total + vertWeights['w'][k] * prodValues
+      pdfValues[i] = total
+    samples = pd.DataFrame(x)
+    samples['pdfValues'] = pdfValues
+    return samples
 
-    # vertices = Construct_Corner_Points(dimension)
-    for i in range(0, len(wts_list)):
-      n_sub = len(memb_sample[memb_sample == i])
-      if n_sub != 0:
-        if i == 0:
-
-          mixt_sample[memb_sample == i] = np.random.multivariate_normal(mean, Pearsons_Rho_Matrix, size=n_sub)
-        else:
-          for col in range(dimension):
-            mixt_sample[memb_sample == i, col] = vertices[i - 1, col] * gamma.rvs(scale=1 / self.tail_prob ['beta'],
-                                                                                  a=self.tail_prob ['alpha'],
-                                                                                  size=n_sub)
-    return mixt_sample
-
-  # Function to evaluate the multivariate umbrella pdf with gamma tails. The gamma tails
-  # have their seperate weighing scheme.
-  def multivariate_umbrella_pdf(self, x, vertWeights):
+  def umbrellaPDF(self, x, vertWeights):
+    """
+         Function to evaluate the multivariate umbrella pdf with gamma tails. The gamma tails have their seperate weighing scheme.
+         @ In, x, samples
+         @ In, vertWeights, weights of the mixture distribution
+         @ Out, dataframe, dataframe of samples and associated probability values
+    """
     # w is the weight of the target density (1-w) is distributed over the tails
-    mv_norm = multivariate_normal([0,0], [[1, 0.5],[0.5, 1]])
-    target_pdf = mv_norm.pdf(x)
+    mvnorm = multivariate_normal(self.mu, self.covariance)
+    targetPDF = mvnorm.pdf(x)
     # target_pdf = self.distDict['target'].pdf(x)
-    gamma_tail_pdf = self.mv_gamma_tail_pdf(x, vertWeights)
-    new_pdf_values = self.weightTarget * target_pdf + (1 - self.weightTarget) * gamma_tail_pdf[2]
-    gamma_tail_pdf[2] = new_pdf_values
-    return gamma_tail_pdf
+    gammaTailPDFValues = self.gammaTailPDF(x, vertWeights)
+    umbrellaPDFValues = self.weightTarget * targetPDF + (1 - self.weightTarget) * gammaTailPDFValues['pdfValues']
+    gammaTailPDFValues['pdfValues'] = umbrellaPDFValues
+    return gammaTailPDFValues
 
-  def multi_umbrella_with_weights(self,s, w, p, k, mu_orig, Pearsons_Rho_Matrix, vertWeights):
-    x_sample = self.multivariate_umbrella_sample(s, w, p, k, mu_orig, Pearsons_Rho_Matrix, vertWeights)
-    mv_norm = multivariate_normal(mu_orig, Pearsons_Rho_Matrix)
-    target_pdf = mv_norm.pdf(x_sample)
-    # Evaluating the importance weights
-    imp_weights = target_pdf / self.multivariate_umbrella_pdf(x_sample, w, p, 0, k, vertWeights, mu_orig, Pearsons_Rho_Matrix)[2]
-
-    results = pd.DataFrame(x_sample)
-    results['weights'] = imp_weights
-    return results
-
-  # Function to evaluate a univariate variate gamma tail sample in positive hyper-quadrant
-  def univ_gamma_tail_sample(self, sample_size):
-
-    sample = gamma.rvs(scale=1 / self.tail_prob ['beta'],
-                       a=self.tail_prob ['alpha'],
+  def univGammaTailSample(self, sample_size):
+    """
+         Method to evaluate a univariate variate gamma tail sample in positive hyper-quadrant
+         @ In, sample_size
+         @ Out, sample, univariate gamma samples
+    """
+    sample = gamma.rvs(scale=1 / self.tailProb ['beta'],
+                       a=self.tailProb ['alpha'],
                        size=sample_size)
     return sample
 
-  def multi_gamma_tail_sample(self, sample_size, vertWeights):
-    multi_sample = np.empty(shape=(sample_size, self.dimension))
+  def gammaTailSample(self, sampleSize, vertWeights):
+    """
+         Method to generate multivariate gamma tail samples
+         @ In, sample_size
+         @ In, vertWeights, weights of the mixture distribution
+         @ Out, dataframe, dataframe of samples
+    """
+    multiSamples = np.empty(shape=(sampleSize, self.dimension))
 
     for i in range(0, self.dimension):
-      sample = self.univ_gamma_tail_sample(sample_size)
-      multi_sample[:, i] = sample
-    vertex_sample = self.rdiscrete(sample_size, vertWeights['w'])
-    new_sample = multi_sample
+      samples = self.univGammaTailSample(sampleSize)
+      multiSamples[:, i] = samples
+    vertexSample = self.getDiscreteSamples(sampleSize, vertWeights['w'])
+    newSamples = multiSamples
 
-    for i in range(0, sample_size):
-      new_sample[i,] = multi_sample[i,] * vertWeights['vert'][int(vertex_sample[i]) - 1,]
+    for i in range(0, sampleSize):
+      newSamples[i,] = multiSamples[i,] * vertWeights['vert'][int(vertexSample[i]) - 1,]
+    newSamplesDF = pd.DataFrame(newSamples)
+    newSamplesDF['vert'] = vertexSample
+    newSamplesDF['tail_flag'] = 1
 
-    new_sample = pd.DataFrame(
-      {'vert': vertex_sample, 'new_sample_x1': new_sample[:, 0], 'new_sample_x2': new_sample[:, 1], 'tail_flag': 1})
-    return new_sample
+    return newSamplesDF
 
-  def multi_umbrella_sample(self, sample_size, vertWeights):
+  def umbrellaSample(self, sampleSize, vertWeights):
+    """
+         Method to generate samples from multivariate umbrella distribution
+         @ In, sample_size
+         @ In, vertWeights, weights of the mixture distribution
+         @ Out, dataframe of umbrella samples
+    """
     pmf = [self.weightTarget, 1 - self.weightTarget]
-    disc_sample = self.rdiscrete(sample_size, pmf)
-    target_sample_size = len(disc_sample[disc_sample == 1])
-    tail_sample_size = sample_size - target_sample_size
-    tail_sample = self.multi_gamma_tail_sample(tail_sample_size, vertWeights)
-    target_sample = np.random.multivariate_normal([0,0], [[1, 0.5],[0.5, 1]], size=target_sample_size)
-    vert_target = np.zeros(shape=target_sample_size)
-
-    target_sample = pd.DataFrame(
-      {'vert': vert_target, 'new_sample_x1': target_sample[:, 0], 'new_sample_x2': target_sample[:, 1], 'tail_flag': 0})
-    umbrella_sample = target_sample.append(tail_sample)
-    samples = umbrella_sample[['new_sample_x1', 'new_sample_x2']]
-    x_points = samples.to_numpy()
-    umb_pdf_values = self.multivariate_umbrella_pdf(x_points, vertWeights)
-    mv_norm = multivariate_normal([0,0], [[1, 0.5],[0.5, 1]])
-    target_pdf = mv_norm.pdf(x_points)
+    discreteSamples = self.getDiscreteSamples(sampleSize, pmf)
+    targetSampleSize = len(discreteSamples[discreteSamples == 1])
+    tailSampleSize = sampleSize - targetSampleSize
+    tailSamples = self.gammaTailSample(tailSampleSize, vertWeights)
+    targetSamples = np.random.multivariate_normal(self.mu, self.covariance, size=targetSampleSize)
+    vertTarget = np.zeros(shape=targetSampleSize)
+    targetSamplesDF = pd.DataFrame(targetSamples)
+    targetSamplesDF['vert'] = vertTarget
+    targetSamplesDF['tail_flag'] = 0
+    umbrellaSamples = targetSamplesDF.append(tailSamples, ignore_index=True)
+    samples = umbrellaSamples.loc[:, ~umbrellaSamples.columns.isin(['vert', 'tail_flag'])]
+    xpoints = samples.to_numpy()
+    umbrellaPDFValues = self.umbrellaPDF(xpoints, vertWeights)
+    mvnorm = multivariate_normal(self.mu, self.covariance)
+    targetPDF = mvnorm.pdf(xpoints)
     # Evaluating the importance weights
-    imp_weights = target_pdf / umb_pdf_values[2]
-
-    umbrella_samples = pd.DataFrame(
-      {'vert': umbrella_sample['vert'].to_numpy(), 'weights': imp_weights.to_numpy(), 'x1': x_points[:, 0],
-       'x2': x_points[:, 1], 'tail_flag': umbrella_sample['tail_flag'].to_numpy()})
-
-    return umbrella_samples
+    importanceWeights = targetPDF / umbrellaPDFValues['pdfValues']
+    umbrellaSamples['weights'] = importanceWeights
+    return umbrellaSamples
 
   def localGenerateInput(self, model, myInput):
     """
@@ -304,30 +289,28 @@ class Umbrella(ForwardSampler):
       @ Out, None
     """
 
-    vertices = self.Construct_Corner_Points(self.dimension)
-    n_vertices = len(vertices)
-    verticesWeights = np.ones(n_vertices) / n_vertices
+    vertices = self.constructCornerPoints()
+    nvertices = len(vertices)
+    verticesWeights = np.ones(nvertices) / nvertices
     verticesWeights = dict({"vert": vertices, "w": verticesWeights})
-    sigma_k = scipy.optimize.root(partial(self.get_std, self.modeLocation), 1, tol=0.0000001)['x'][0]
-    self.tail_prob = self.tail_prob_gamma(self.modeLocation, sigma_k)
-    sample_size = 30
-    umbrellaSample = self.multi_umbrella_sample(sample_size,
-                                             verticesWeights)
+    self.mu = [0] * self.dimension
+    dim = 2
+    rho = 0.5
+    self.covariance = np.ones(shape=(self.dimension, self.dimension)) * rho
+    for i in range(self.dimension):
+      self.covariance[i, i] = 1
+
+    sigma = scipy.optimize.root(partial(self.getStd, self.modeLocation), 1, tol=0.0000001)['x'][0]
+    self.tailProb = self.tailProbabilityGamma(self.modeLocation, sigma)
+    sampleSize = 30
+    umbrellaSample = self.umbrellaSample(sampleSize,
+                                         verticesWeights)
 
     print('umbrellaSample')
     print(umbrellaSample)
-
-    #
-    # self.values['x1'] = np.atleast_2d(umbrellaSample[['x1', 'x2']])
-    # self.values['x2'] = np.atleast_2d(umbrellaSample[['x1', 'x2']])
-    #
-    #
-    self.inputInfo['SampledVars']['x1'] = umbrellaSample['x1'].to_numpy()
-    # # self.inputInfo['SampledVars']['x2'] = umbrellaSample['x2']
-    # #
+    # self.inputInfo['SampledVars']['x1'] = umbrellaSample['0'].to_numpy()
     self.inputInfo['ProbabilityWeight'] = 1.0
-    self.inputInfo['ProbabilityWeight-x1'] =  umbrellaSample['weights'].to_numpy()
-    # self.inputInfo['ProbabilityWeight-x2'] = umbrellaSample['weights'].to_numpy()
+    self.inputInfo['ProbabilityWeight-x1'] = umbrellaSample['weights'].to_numpy()
     self.inputInfo['PointProbability'] =1.0
     self.inputInfo['SamplerType'] = 'Umbrella'
-    print(self.inputInfo)
+    # print(self.inputInfo)
