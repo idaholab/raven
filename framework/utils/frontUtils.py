eea5238fa4cf752cbfda3cb16e0cd77bb77fd1bf
Jimmy-INL <52417034+Jimmy-INL@users.noreply.github.com>
2021-06-27 22:08:57 -0600

    Adding crowdingDistance to frontUtils

--- framework/utils/frontUtils.py
+++ framework/utils/frontUtils.py
@@ -95,3 +95,30 @@ def rankNonDominatedFrontiers(data):
     nonDominatedRank[indicesNonDominated] = rank
   nonDominatedRank = list(nonDominatedRank)
   return nonDominatedRank
+
+def crowdingDistance(rank, popSize, objectives):
+    '''
+    Calculate the crowding distance for each front
+    @ In, rank: (Nx1) numpy array where front_i[i] is the front number for pop[i]
+    @ Out, crowd_dis: (Nx1) numpy array of crowding distances
+    '''
+    crowd_dis = np.zeros(popSize)
+    dist={}
+    fronts = np.unique(rank)
+    fronts = fronts[fronts!=np.inf]
+    for f in range(len(fronts)):
+        front = np.where(np.asarray(rank)==f+1)[0]
+        fmax = np.max(objectives[front, :], axis=0)
+        fmin = np.min(objectives[front, :], axis=0)
+        for prev, individual, next_ in batches(objectives):
+          dist[individual] += objectives[next_] - objectives[prev]
+        for i in range(np.shape(objectives)[1]):
+            sortedRank = np.argsort(objectives[front, i])
+            crowd_dis[front[sortedRank[0]]] = crowd_dis[front[sortedRank[-1]]] = np.inf
+            for j in range(1, len(front)-1):
+                crowd_dis[front[sortedRank[j]]] = crowd_dis[front[sortedRank[j]]] +(objectives[front[sortedRank[j+1]], i] - objectives[front[sortedRank[j-1]], i]) / (fmax[i]-fmin[i])
+    return crowd_dis
+
+def batches(iterable, n=3):
+        for i in range(len(iterable) - (n-1)):
+            yield iterable[i:i+n]
