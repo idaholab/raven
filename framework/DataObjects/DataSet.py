75f1f4cc20b5bf06282d946baeb581c26fca39c1
Jimmy-INL <52417034+Jimmy-INL@users.noreply.github.com>
2021-08-11 14:50:31 -0600

    fixing dataset issue

--- framework/DataObjects/DataSet.py
+++ framework/DataObjects/DataSet.py
@@ -305,8 +305,10 @@ class DataSet(DataObject):
     if outType == 'xrDataset':
       # return reference to the xArray
       data = self._convertToXrDataset()
-      if 'name' not in data.attrs:
-        data.attrs['name'] = self.name
+      if data != None:
+        if 'name' not in data.attrs:
+          data.attrs['name'] = self.name
+          
     elif outType=='dict':
       # return a dict (copy of data, no link to original)
       data = self._convertToDict()
@@ -2084,6 +2086,7 @@ class DataSet(DataObject):
     with open(fileName+'.xml','w') as ofile:
       #header
       ofile.writelines('<DataObjectMetadata name="{}">\n'.format(self.name))
+      meta.pop('name', None)
       for name in sorted(list(meta.keys())):
         target = meta[name]
         xml = xmlUtils.prettify(target.getRoot(),startingTabs=1,addRavenNewlines=False)
