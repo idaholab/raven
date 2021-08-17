9d96b4d076e6178c727af484d67fd274a9e0d018
Wang, Congjian <congjian.wang@inl.gov>
2021-08-17 08:44:51 -0600

    clean up DataSet

--- framework/DataObjects/DataSet.py
+++ framework/DataObjects/DataSet.py
@@ -305,11 +305,6 @@ class DataSet(DataObject):
     if outType == 'xrDataset':
       # return reference to the xArray
       data = self._convertToXrDataset()
-      # if data != None:
-      #   if 'attributes' not in data.attrs:
-      #     data.attrs['attributes'] = xmlUtils.StaticXmlElement('attributes')
-      #     data.attrs['attributes'].addScalar('general', 'datasetName', self.name)
-
     elif outType=='dict':
       # return a dict (copy of data, no link to original)
       data = self._convertToDict()
@@ -2089,7 +2084,6 @@ class DataSet(DataObject):
     with open(fileName+'.xml','w') as ofile:
       #header
       ofile.writelines('<DataObjectMetadata name="{}">\n'.format(self.name))
-      #meta.pop('name', None)
       for name in sorted(list(meta.keys())):
         target = meta[name]
         xml = xmlUtils.prettify(target.getRoot(),startingTabs=1,addRavenNewlines=False)
