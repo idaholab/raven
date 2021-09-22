84c065dc3d1643f357ef5f9dc4b1cda21768a30b
Paul W. Talbot <paul.talbot@inl.gov>
2021-09-22 12:17:29 -0600

    generalized TSAUser, got TSA PP and SynthHist working

--- framework/SupervisedLearning/SyntheticHistory.py
+++ framework/SupervisedLearning/SyntheticHistory.py
@@ -61,6 +61,7 @@ class SyntheticHistory(SupervisedLearning, TSAUser):
       A constructor that will appropriately intialize a supervised learning object
                            and printing messages
       @ In, kwargs: an arbitrary dictionary of keywords and values
+      @ Out, None
     """
     # general infrastructure
     SupervisedLearning.__init__(self)
@@ -85,7 +86,7 @@ class SyntheticHistory(SupervisedLearning, TSAUser):
       @ Out, None
     """
     self.raiseADebug('Training...')
-    self.trainTSASequential(targetVals)
+    self.trainTSASequential(targetVals, self.target)
 
   def __evaluateLocal__(self, featureVals):
     """
