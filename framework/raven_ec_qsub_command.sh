a6b69a7b745f3ffd168c6041023071dc879a65d4
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2021-10-21 11:53:16 -0600

    Fix to work with sawtooth.

---
+++ framework/raven_ec_qsub_command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if test -n "$PBS_O_WORKDIR"; then
+    echo Moving to working dir: ${PBS_O_WORKDIR}
+    cd $PBS_O_WORKDIR
+fi
+
+#echo RAVEN_FRAMEWORK_DIR $RAVEN_FRAMEWORK_DIR
+#source $RAVEN_FRAMEWORK_DIR/../scripts/establish_conda_env.sh --load
+module load pbs openmpi
+
+which python
+which mpiexec
+echo ''
+echo $COMMAND
+$COMMAND
