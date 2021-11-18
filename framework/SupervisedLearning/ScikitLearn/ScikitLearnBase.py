1ecd7f52ac7944adfe7a00e66c38a615f1b45cd4
Congjian Wang <congjian.wang@inl.gov>
2021-11-17 23:02:29 -0700

    update ScikitLearnBase the way to initialize the model

--- framework/SupervisedLearning/ScikitLearn/ScikitLearnBase.py
+++ framework/SupervisedLearning/ScikitLearn/ScikitLearnBase.py
@@ -45,10 +45,11 @@ class ScikitLearnBase(SupervisedLearning):
       @ Out, None
     """
     super().__init__()
-    self.uniqueVals = None
-    self.settings = None
-    self.model = None
-    self.multioutputWrapper = True
+    self.uniqueVals = None # flag to indicate targets only have a single unique value
+    self.settings = None # initial settings for the ROM
+    self.model = None # Scikitlearn estimator/model
+    self.multioutputWrapper = True # If True, use MultiOutputRegressor or MultiOutputClassifier to wrap self.model else
+                                   # the self.model can handle multioutput/multi-targets prediction
 
   def updateSettings(self, settings):
     """
@@ -74,9 +75,25 @@ class ScikitLearnBase(SupervisedLearning):
       @ In, settings, dict, the dictionary containin the parameters/settings to instanciate the model
       @ Out, None
     """
-    settings = self.updateSettings(settings)
     self.settings = settings
-    self.model.set_params(**settings)
+    self.model = self.model(**settings)
+    if self.multioutputWrapper:
+      self.multioutput(self.info['problemtype'])
+
+  def multioutput(self, type='regression'):
+    """
+      Method to extend ScikitLearn ROM that do not natively support multi-target regression/classification
+      @ In, type, str, either regression or classification
+      @ Out, None
+    """
+    import sklearn.multioutput
+    if type == 'regression':
+      self.model = sklearn.multioutput.MultiOutputRegressor(self.model)
+    elif type == 'classification':
+      self.model = sklearn.multioutput.MultiOutputClassifier(self.model)
+    else:
+      self.raiseAnError(IOError, 'The "type" param for function "multioutput" should be either "regression" or "classification"! but got',
+                        type)
 
   def setEstimator(self, estimatorList):
     """
@@ -96,7 +113,6 @@ class ScikitLearnBase(SupervisedLearning):
         associated with the corresponding points in featureVals
     """
     # check if all targets only have a single unique value, just store that value, no need to fit/train
-    self.uniqueVals = None
     if all([len(np.unique(targetVals[:,index])) == 1 for index in range(targetVals.shape[1])]):
       self.uniqueVals = [np.unique(targetVals[:,index])[0] for index in range(targetVals.shape[1]) ]
     else:
@@ -140,7 +156,8 @@ class ScikitLearnBase(SupervisedLearning):
       @ In, None
       @ Out, None
     """
-    self.model.set_params(**self.settings)
+    settings = self.updateSettings(self.settings)
+    self.model.set_params(**settings)
 
   def __returnInitialParametersLocal__(self):
     """
@@ -148,7 +165,7 @@ class ScikitLearnBase(SupervisedLearning):
       @ In, None
       @ Out, params, dict,  dictionary of parameter names and initial values
     """
-    params = self.model.get_params()
+    params = self.settings
     return params
 
   def __returnCurrentSettingLocal__(self):
