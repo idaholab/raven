cd4b542531da70d2fad58fb2ca5b87f07546a440
mandd <diego.mandelli@inl.gov>
2021-11-30 09:25:05 -0700

    Revert "added check"
    This reverts commit 56efd26f7a64cb926019e3356b2b95c1353da229.

--- framework/Optimizers/parentSelectors/parentSelectors.py
+++ framework/Optimizers/parentSelectors/parentSelectors.py
@@ -110,11 +110,8 @@ def tournamentSelection(population,**kwargs):
     matrixOperation = np.zeros((popSize,2))
 
   indexes = list(np.arange(popSize))
-  indexesShuffled = randomUtils.randomChoice(indexes, size=popSize, replace=False, engine=None)
-  
-  if popSize<2*nParents:
-    raise ValueError('In tournamentSelection the number of parents cannot be larger than half of the population size.')
-    
+  indexesShuffled = randomUtils.randomChoice(indexes, size = popSize, replace = False, engine = None)
+
   for idx, val in enumerate(indexesShuffled):
     matrixOperation[idx,:] = matrixOperationRaw[val,:]
 
