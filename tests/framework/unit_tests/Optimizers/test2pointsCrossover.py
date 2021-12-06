44c04df1a2d61916627c9856cec5682ed14f33eb
mandd <diego.mandelli@inl.gov>
2021-12-06 16:12:00 -0700

    added 2pointXover file

---
+++ tests/framework/unit_tests/Optimizers/test2pointsCrossover.py
@@ -0,0 +1,96 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+  Testing for the twoPointsCrossover method
+  @authors: Mohammad Abdo, Diego Mandelli, Andrea Alfonsi
+"""
+import os
+import sys
+import xarray as xr
+import numpy as np
+
+ravenPath = os.path.abspath(os.path.join(__file__, *['..'] * 5, 'framework'))
+print('... located RAVEN at:', ravenPath)
+sys.path.append(ravenPath)
+import Driver
+from Optimizers.crossOverOperators.crossovers import returnInstance
+
+twoPointsCrossover = returnInstance('tester', 'twoPointsCrossover')
+
+#
+#
+# checkers
+#
+def checkSameDataArrays(comment, resultedDA, expectedDA, update=True):
+  """
+    This method compares two identical things
+    @ In, comment, string, a comment printed out if it fails
+    @ In, resultedDA, xr.DataArray, the resulted DataArray to be tested
+    @ In, expectedDA, xr.DataArray, the expected DataArray
+    @ In, update, bool, optional, if False then don't update results counter
+    @ Out, res, bool, True if same
+  """
+  res = resultedDA.identical(expectedDA)
+  if update:
+    if res:
+      results["pass"] += 1
+    else:
+      print("checking string", comment, '|', value, "!=", expected)
+      results["fail"] += 1
+  return res
+
+results = {'pass': 0, 'fail': 0}
+#
+#
+# initialization
+#
+optVars = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8']
+population = [[11,12,13,14,15,16,17,18],
+              [21,22,23,24,25,26,27,28],
+              [31,32,33,34,35,36,37,38],
+              [41,42,43,44,45,46,47,48]]
+population = xr.DataArray(population,
+                          dims   = ['chromosome','Gene'],
+                          coords = {'chromosome': np.arange(np.shape(population)[0]),
+                                    'Gene':optVars})
+nParents = 2
+children = twoPointsCrossover(population)
+
+print('twoPointsCrossover')
+print('*'*19)
+print('generated children are: {}'.format(children))
+expectedChildren = xr.DataArray([[ 11.,  22.,  23.,  24.,  25.,  26.,  17.,  18.],
+                                 [ 21.,  12.,  13.,  14.,  15.,  16.,  27.,  28.],
+                                 [ 11.,  12.,  13.,  14.,  15.,  36.,  37.,  18.],
+                                 [ 31.,  32.,  33.,  34.,  35.,  16.,  17.,  38.],
+                                 [ 11.,  42.,  43.,  44.,  45.,  46.,  47.,  18.],
+                                 [ 41.,  12.,  13.,  14.,  15.,  16.,  17.,  48.],
+                                 [ 21.,  22.,  33.,  34.,  35.,  36.,  37.,  28.],
+                                 [ 31.,  32.,  23.,  24.,  25.,  26.,  27.,  38.],
+                                 [ 21.,  22.,  43.,  44.,  45.,  26.,  27.,  28.],
+                                 [ 41.,  42.,  23.,  24.,  25.,  46.,  47.,  48.],
+                                 [ 31.,  42.,  43.,  44.,  45.,  36.,  37.,  38.],
+                                 [ 41.,  32.,  33.,  34.,  35.,  46.,  47.,  48.]],
+                                 dims   = ['chromosome','Gene'],
+                                 coords = {'chromosome': np.arange(12),
+                                           'Gene'      : optVars})
+
+## TESTING
+# Test survivor population
+checkSameDataArrays('Check survived population data array',children,expectedChildren)
+#
+# end
+#
+print('Results:', results)
+sys.exit(results['fail'])
