82191e156dcbc3ecdcb13022ba7792d69bc38a67
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2022-07-07 13:47:12 -0600

    Adding a comment describing __func

--- ravenframework/Runners/DistributedMemoryRunner.py
+++ ravenframework/Runners/DistributedMemoryRunner.py
@@ -55,6 +55,7 @@ class DistributedMemoryRunner(InternalRunner):
     if not im.isLibAvail("ray"):
       self.__ppserver, args = args[0], args[1:]
     super().__init__(args, functionToRun, **kwargs)
+    # __func (when using ray) is the object ref
     self.__func = None
 
   def isDone(self):
