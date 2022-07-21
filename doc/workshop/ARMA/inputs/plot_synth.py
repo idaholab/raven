023efca52d1bf4d6e500900a8f5db17408f76849
Dylan McDowell <dylanjm@users.noreply.github.com>
2022-07-21 15:22:21 -0600

    Address comments

--- doc/workshop/ARMA/inputs/plot_synth.py
+++ doc/workshop/ARMA/inputs/plot_synth.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
-
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Simple plotting script to create visualizations of ARMA workshop material
+"""
 import sys
 import os
 import matplotlib.pyplot as plt
@@ -7,25 +22,24 @@ import pandas as pd
 
 def main():
     try:
-        synth_fp = os.path.abspath(sys.argv[1])
-        real_fp = os.path.abspath(os.path.join(os.path.dirname(synth_fp), "dataSet_0.csv"))
+        synthFilepath = os.path.abspath(sys.argv[1])
+        realFilepath = os.path.abspath(os.path.join(os.path.dirname(synthFilepath), "dataSet_0.csv"))
     except IndexError:
         print("ERROR: Filepath to synthetic data not provided")
 
 
-    synth = pd.read_csv(synth_fp)
-    real = pd.read_csv(real_fp)
+    synth = pd.read_csv(synthFilepath)
+    real = pd.read_csv(realFilepath)
 
     fig, ax = plt.subplots()
-
     ax.plot(synth.iloc[:, 0], synth.iloc[:, 1], color="goldenrod", label='Synthetic Signal')
     ax.plot(real.iloc[:, 0], real.iloc[:, 1], label='Original Signal')
     ax.set_xlabel('Time')
     ax.set_ylabel('Value')
     ax.legend()
 
-    output_fp = os.path.join(os.path.dirname(real_fp), "results.png")
-    plt.savefig(output_fp)
+    outputFilepath = os.path.join(os.path.dirname(real_fp), "results.png")
+    plt.savefig(outputFilepath)
 
 
 if __name__ == '__main__':
