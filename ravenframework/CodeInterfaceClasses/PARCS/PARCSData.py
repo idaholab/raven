a4f55d915c98c6104be41fe821fb84bd0f4d60f2
nhnkkhang <nhnknhatkhang@unist.ac.kr>
2022-11-12 11:37:14 -0500

    Adding interface and test xml files

---
+++ ravenframework/CodeInterfaceClasses/PARCS/PARCSData.py
@@ -0,0 +1,205 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Created on Oct 25, 2022
+Last modified on Oct 26, 2022
+@author: khnguy22 NCSU
+
+comments: Interface for PARCS loading pattern optimzation
+          Originally, this was based on SIMULATE3 output structure
+"""
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import os, gc, sys, copy, h5py, math
+import time
+import numpy 
+import pickle
+import random
+import os
+import copy
+import shutil
+#from ravenframework.utils import utils
+
+class PARCSData:
+  """
+  Class that parses output of PARCS for a multiple run
+  Partially copied from SIMULATE3 interface
+  """
+  def __init__(self,depletionfile,pinpowerfile):
+    """
+    Constructor
+    @ In, depletionfile, string or dict, depletion file name to be parsed, read one file at a time
+    @ In, pinpowerfile, string or dict, pinpower file name to be parsed, read one file at a time
+    @ Out, None
+    """
+    self.data = {}
+    self.lines = open(os.path.abspath(os.path.expanduser(depletionfile)),"r").readlines()
+    # retrieve data, only needed data for optimization problem
+    extractedData = self.getParam()
+    self.data['keff'] = {'info_ids':extractedData['info_ids'][0], 'values':extractedData['values'][0]}
+    self.data['FDeltaH'] = {'info_ids':extractedData['info_ids'][3], 'values':extractedData['values'][3]}
+    self.data["boron"] = {'info_ids':extractedData['info_ids'][4], 'values':extractedData['values'][4]}
+    self.data["cycle_length"] = {'info_ids':extractedData['info_ids'][2], 'values':extractedData['values'][2]}
+    self.data["PinPowerPeaking"] = {'info_ids':extractedData['info_ids'][1], 'values':extractedData['values'][1]}
+    self.data["exposure"] = {'info_ids':extractedData['info_ids'][5], 'values':extractedData['values'][5]}
+    # this is a dummy variable for demonstration
+    # Multi-objective --> single objective
+    self.data["target"] = self.getTarget()
+    # check if something has been found
+    if all(v is None for v in self.data.values()):
+      raise IOError("No readable outputs have been found!")
+    ## for pin power
+    self.pinPower = self.getPinpower(pinpowerfile)
+#------------------------------------------------------------------------------------------
+  #function to retrivedata
+  def getSummary(self):
+    """
+    Get the starting line and endding line of the summary in output file
+    @ In, None
+    # Out, lineBeg, lineEnd
+    """
+    lineBeg = 0 
+    lineEnd = 0 
+    nlines =  len(self.lines)
+    for i in range (nlines):
+      if self.lines[i].find('summary:')>=0:
+        lineBeg = i+4
+        for j in range (lineBeg, nlines):
+          if self.lines[j].find('========================')>=0:
+            lineEnd = j 
+            break
+        break
+
+    return lineBeg, lineEnd
+
+  def getParam(self):
+    """
+    Extract all the parameters value from output file lines
+    @ In, None
+    @ Out, outputDict, dict, the dictionary containing the read data (None if none found)
+                           {'info_ids':list(of ids of data),
+                            'values': list}
+    """
+    lineBeg, lineEnd = self.getSummary()
+    cycLength = []
+    k_eff = []
+    FQ = []
+    FdelH = []
+    BU = []
+    BoronCon = []
+    Tfuel = []
+    Tmod = []
+    for i in range (lineBeg, lineEnd):
+      elem=self.lines[i].split()
+      cycLength.append(float(elem[2]))
+      k_eff.append(float(elem[3]))
+      fq = float(elem[4].split('(')[0])
+      if elem[4].find(')')>0:
+        indx=4
+      else:
+        indx=5
+      FQ.append(fq)
+      if elem[indx+1].find(')')>0:
+        indx =indx+1
+      FdelH.append(float(elem[indx+1]))
+      BU.append(float(elem[indx+3]))
+      BoronCon.append(float(elem[indx+6]))
+      Tmod.append(float(elem[indx+8]))
+      Tfuel.append(float(elem[indx+7]))
+    ## create a check point
+    check = [cycLength, k_eff, FQ, FdelH, BU, BoronCon, Tfuel, Tmod]
+    c = [ii for ii in check if not ii]
+    if c:
+      return ValueError("No values returned. Check output File executed correctly")
+    ### get cycle length at 10ppm interpolated
+    for i in range (len(BoronCon)):
+      if (BoronCon[i] - 0.1)<1e-3:
+        idx_ = i
+        break
+    EOCboron = 10
+    cycLengthEOC = cycLength[idx_-1] +(EOCboron-BoronCon[idx_-1])*(cycLength[idx_]-cycLength[idx_-1])\
+                  /(BoronCon[idx_]-BoronCon[idx_-1]) 
+    outDict = {'info_ids':[['eoc_keff'], ['PinPowerPeaking'], ['MaxEFPD'],['MaxFDH'],
+                              ['max_boron'], ['exposure']], 
+               'values': [[k_eff[-1]], [max(FQ)], [cycLengthEOC], [max(FdelH)],
+                              [max(BoronCon)], [BU[-1]] ]}
+    return outDict
+
+  def getTarget(self):
+    """
+    This is a function to convert the fitness function to be output variable and make the
+    problem to be single-objective rather than multi-objective optimzation
+    @ In, None
+    @ Out, outputDict, dict, the dictionary containing the read data (None if none found)
+                       {'info_ids':list(of ids of data),
+                        'values': list}
+    """
+    tmp = -1.0*max(0,self.data["boron"]['values'][0] - 1300)\
+          -400*max(0,self.data["PinPowerPeaking"]["values"][0]-2.1) \
+          -400*max(0,self.data['FDeltaH']["values"][0]-1.48)\
+          +self.data["cycle_length"]["values"][0]
+    outputDict = {'info_ids':['target'], 'values': [tmp]}
+
+    return outputDict
+
+  def writeCSV(self, fileout):
+    """
+      Print Data into CSV format
+      @ In, fileout, str, the output file name
+      @ Out, None
+    """
+    fileObject = open(fileout.strip()+".csv", mode='wb+') if not fileout.endswith('csv') else open(fileout.strip(), mode='wb+')
+    headers=[]
+    timeGrid = None
+    nParams = numpy.sum([len(data['info_ids']) for data in self.data.values() if data is not None and type(data) is dict])
+    ndata = 1
+    outputMatrix = numpy.zeros((nParams,1))
+    tmp = [data['info_ids'] for data in self.data.values() if data is not None and type(data) is dict]
+    index=0
+    for data in self.data.values():
+      if data is not None and type(data) is dict:
+        headers.extend(data['info_ids'])
+        for i in range(len(data['info_ids'])):
+          outputMatrix[index]= data['values'][i]
+          index=index+1
+    numpy.savetxt(fileObject, outputMatrix.T, delimiter=',', header=','.join(headers), comments='')
+    fileObject.close()
+
+  def getPinpower(self, depletionfile):
+    lines = open(os.path.abspath(os.path.expanduser(depletionfile)),"r").readlines()
+    n_line = len(lines)
+    bu_step = []
+    FAinfo = []
+    Nodeinfo = []
+    Pinpower = []
+    step = 0
+    bu_step.append(0)
+    for i in range(n_line):
+      if lines[i].find("At Time:") >=0:
+        step = step+1 
+        bu_step.append(step)
+        if lines[i].find("Assembly Coordinate (i,j):")>=0:
+          temp = lines[i].split()
+          Nodeinfo.append([temp[10],temp[11],temp[15]])
+          N = lines[i+1].split()
+          N = int(N[-1])
+          pinarray = []
+          for jj in range (i+2, N+3):
+            pinarray.append([float(val) for val in lines[jj].split()[1:]])
+          Pinpower.append(pinarray)
+          FAinfo.append([temp[3],temp[4], float(lines[N+3].split()[-1])])
+    
+    outputDict = {'info_ids':[['FAInfor'], ['nodeInfor'], ['pinPowerMap']], 
+                  'values': [[FAinfo], [Nodeinfo], [Pinpower]]}
+    return outputDict
---
+++ ravenframework/CodeInterfaceClasses/PARCS/SpecificParser.py
@@ -0,0 +1,300 @@
+"""
+Created on Aug. 10th, 2022
+@author: khnguy22
+comment: Specific parser for PRACS interface
+"""
+
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import os
+from xml.etree import ElementTree as ET
+
+class DataParser():
+  """
+    Parse the data in RAVEN input in to PARCS input
+    and be saved for the perturbed input generator in Perturbed parser
+    Note: this data parser is only called once
+  """
+
+  def __init__(self, inputFile):
+    """
+      Constructor.
+      @ In, inputFiles, string, xml PARCS parameters file
+      @ In, workingDir, string, absolute path to the working directory
+      @ Out, None
+    """
+    self.inputFile = inputFile
+    self.getParameters()
+
+  def getParameters(self):
+    """
+      Get required parameters from xml file for generating
+      Constructor.
+      @ In, None
+      @ Out, None
+    """
+
+    fullFile = os.path.join(self.inputFile)
+    dorm = ET.parse(fullFile)
+    root = dorm.getroot()
+    self.THFlag = root.find('THFlag').text.strip()
+    self.power = root.find('power').text.strip()
+    self.initialBoron = root.find('initialBoron').text.strip()
+    self.coretype = root.find('coretype').text.strip()
+    self.XSdir = root.find('XSdir').text.strip()
+    self.Depdir = root.find('Depdir').text.strip()
+    self.DepHistory = root.find('DepHistory').text.strip()
+    self.NFA = root.find('NFA').text.strip()
+    self.NAxial = root.find('NAxial').text.strip()
+    self.geometry = root.find('Geometry').text.strip()
+    self.FA_Pitch = root.find('FA_Pitch').text.strip()
+    self.FA_Power = root.find('FA_Power').text.strip()
+    self.grid_x = root.find('grid_x').text.strip()
+    self.grid_y = root.find('grid_y').text.strip()
+    self.grid_z = root.find('grid_z').text.strip()
+    self.neutmesh_x = root.find('neutmesh_x').text.strip()
+    self.neutmesh_y = root.find('neutmesh_y').text.strip()
+    self.BC = root.find('BC').text.strip()
+    self.FAdict = []
+    for FA in root.iter('FA'):
+      self.FAdict.append(FA.attrib)
+    self.XSdict =[]
+    for xs in root.iter('XS'):
+      self.XSdict.append(xs.attrib)
+    
+
+class PerturbedPaser():
+  """
+  Parse value in the perturbed xml file replaces the nominal values by the perturbed values.
+  """
+
+  def __init__(self, inputFile, workingDir, inputName, perturbDict):
+    """
+    Constructor.
+      @ In, inputFiles, string, xml PARCS varibles that will be perturbed file
+      @ In, workingDir, string, absolute path to the working directory
+      @ In, inputName, string, inputname for PARCS input file
+      @ In, perturbDict, dictionary, dictionary of perturbed variables
+      @ Out, None
+    """
+
+    self.inputFile = inputFile #original perturb input file
+    self.perturbDict = perturbDict #get from RAVEN
+    self.workingDir = workingDir
+    self.inputName = inputName
+
+    # get perturbed value and create new xml file
+    self.replaceOldFile()
+
+  def replaceOldFile(self):
+    """
+    Replace orignal xml file with perturbed variables
+    @ In, perturbed variables dictionary
+    @ workingDir, absolute path to the working directory
+    @ Out, None
+    """
+    perturbedID = []
+    perturbedVal = []
+    
+    for key, value in self.perturbDict.items():
+      id_ = ''.join([n for n in key if n.isdigit()])
+      try:
+        id_ = int(id_)
+      except:
+        ValueError('There is no id indication in variable from RAVEN, please check!')
+      
+      perturbedID.append(int(id_))
+      perturbedVal.append(int(value))
+    #sorting 
+    perturbedVal_sorted =  [val for _,val in sorted(zip(perturbedID,perturbedVal))]
+    perturbedVal =  perturbedVal_sorted
+    perturbedID = sorted(perturbedID)
+    fullFile = os.path.join(self.inputFile)
+    dorm = ET.parse(fullFile)
+    root = dorm.getroot()
+    self.data = root
+    for child in self.data:
+      idx = perturbedID.index(int(child.attrib['id']))
+      child.attrib['FAid'] = str(perturbedVal[idx])
+    writedata = ET.tostring(root,encoding='utf8', method='xml')
+    newfile = open(self.inputFile, "w")
+    newfile.write(writedata.decode())
+    newfile.close()
+
+  def generatePARCSInput(self, parameter):
+    """
+    Generate new input for PARCS to run
+    @ In, parameter data in DataParser class
+    @ Out, None
+    """
+    file_ = open(f"{self.workingDir}/{self.inputName}",'w')
+    ## write initial lines 
+    file_.write(f"!****************************************************************************** \n")
+    file_.write(f"CASEID {self.inputName}              OECD NEA MSLB  \n")
+    file_.write(f"!****************************************************************************** \n")
+    file_.write(f"CNTL \n")
+    file_.write(f"      TH_FDBK   {parameter.THFlag}  \n")
+    file_.write(f"      CORE_POWER {parameter.power}  \n")
+    file_.write(f"      CORE_TYPE  {parameter.coretype}  \n")
+    file_.write(f"      PPM        {parameter.initialBoron}  \n")
+    file_.write(f"      DEPLETION  T  1.0E-3 T \n")
+    file_.write(f"      TREE_XS    T  16 T  T  F  F  T  F  T  F  T  F  F  T  T  F  \n")
+    file_.write(f"      BANK_POS   100 100 100 100 100 100  0 46.2 0 \n")
+    file_.write(f"      XE_SM      1 1 \n")
+    file_.write(f"      SEARCH     ppm \n")
+    file_.write(f"      XS_EXTRAP  1.0 0.3 0.8 0.2 \n")
+    file_.write(f"      PIN_POWER  T   \n")
+    file_.write(f"      PRINT_OPT T F T T F T F F F T  F  F  F  F  F   \n")
+    file_.write(f"PARAM \n")
+    file_.write(f"      LSOLVER  1 1 20 \n")
+    file_.write(f"      NODAL_KERN     NEMMG    ! FMFD    ! FDM \n")
+    file_.write(f"      CMFD     2 \n")
+    file_.write(f"      DECUSP   2 \n")
+    file_.write(f"      INIT_GUESS 0 \n")
+    file_.write(f"      conv_ss   1.e-6 5.e-5 1.e-3 0.001 !epseig,epsl2,epslinf,epstf \n")
+    file_.write(f"      eps_erf   0.010 \n")
+    file_.write(f"      eps_anm   0.000001 \n")
+    file_.write(f"      nlupd_ss  5 5 1 \n")
+    file_.write(f"GEOM \n")
+    file_.write(f"      geo_dim {parameter.NFA} {parameter.NFA} {parameter.NAxial} 1 1 \n") # full core geomerty
+    file_.write(f"      Rad_Conf                        !! \n")
+    loadingPattern = getcoremap(parameter,[int(child.attrib['FAid']) for child in self.data], parameter.geometry) 
+    file_.write(loadingPattern) 
+    file_.write("\n") 
+    file_.write(f"      grid_x      {parameter.grid_x} \n")
+    file_.write(f"      neutmesh_x  {parameter.neutmesh_x} \n")
+    file_.write(f"      grid_y      {parameter.grid_y} \n")
+    file_.write(f"      neutmesh_y  {parameter.neutmesh_y} \n")
+    file_.write(f"      grid_z      {parameter.grid_z} \n")
+    file_.write(f"      Boun_cond   {parameter.BC} \n")
+    tmp= []
+    for fa in parameter.FAdict:
+      if fa['name'].lower() != 'none' or float(fa['FAid'])>=0:
+        file_.write(f"      assy_type   {fa['type']}   {fa['structure']} \n")
+    file_.write("\n")
+    ##create pin calculation map 
+    pinmap = loadingPattern
+    for fa in parameter.FAdict:
+      if fa['name'].lower() == 'none' :
+        pinmap = pinmap.replace(fa['type'],"  ")
+      elif fa['name'].lower() == 'ref' :
+        pinmap = pinmap.replace(fa['type']," 0")
+    file_.write("\n")
+    file_.write(f"     pincal_loc \n")
+    file_.write(f"                        \n")
+    file_.write(pinmap)
+    file_.write("\n")
+    file_.write("\n")
+    file_.write(f"TH \n")
+    file_.write(f"      unif_th         0.7  600.0  300.0 \n")
+    file_.write(f"FDBK \n")
+    file_.write(f"      fa_powpit       {parameter.FA_Power}   {parameter.FA_Pitch} \n")
+    file_.write("\n")
+
+    file_.write(f"DEPL \n")
+    file_.write(f"      TIME_STP  {parameter.DepHistory}  \n")
+    file_.write(f"      INP_HST   '../../{parameter.Depdir}/boc_exp_fc.dep' -2 1 \n")
+    for xs in parameter.XSdict:
+      file_.write(f"      PMAXS_F   {xs['id']} '../../{parameter.XSdir}/{xs['name']}'                 {xs['id']}   \n")
+    file_.write(f".  \n")
+    file_.close()
+
+# Outside functions
+def findType(FAid,FAdict):
+  """
+  Get type of FA ID
+  """
+  FAtype = [id['type'] for id in FAdict if id['FAid']==str(FAid)][0]
+  return FAtype
+
+def getcoremap(parameter, FAID, geometrykey):
+  """
+  Genrate Loading Pattern
+  @IN: DataParser class
+  @IN: FAID sorted list, geometry key for full or quater core 
+  @OUT: Loading Pattern
+  """
+  FAdict = parameter.FAdict
+  maxType = max([id['type'] for id in FAdict])
+  numberSpaces = len(str(maxType)) + 2
+  emptyMap=[]
+  if geometrykey.lower()=='full':
+    rows, cols=17,17
+    x_start, y_start = 9, 9
+  elif geometrykey.lower() =='quater':
+    rows, cols=9,9
+    x_start, y_start = 1, 1
+  else:
+    raise ValueError("No available geometry key. Only full or quater core is supported")
+  for i in range(rows):
+      col = []
+      for j in range(cols):
+          col.append(0)
+      emptyMap.append(col)
+  idx_ = 0
+  val = FAID[idx_]
+  val = findType(val,FAdict)
+  for x in range(x_start,x_start+9): # 17x17 core
+    for y in range (y_start,x+1): 
+      val = FAID[idx_]
+      val = findType(val,FAdict)
+      if geometrykey.lower()=='full':
+        outCoordianate = get_index_full(x,y,x_start,y_start)
+      else:
+        outCoordianate = get_index_quater(x,y)
+      for i in outCoordianate:
+        emptyMap[i[0]-1][i[1]-1]=val
+      idx_ = idx_+1
+  loadingPattern = "      "
+  for i in emptyMap:
+      for j in i:
+          value=j
+          str_ = f"{value}"
+          loadingPattern += f"{str_.rjust(numberSpaces)}"
+      loadingPattern += "\n"
+      loadingPattern += "      "
+  return loadingPattern
+
+def get_index_full(x,y, x0, y0):
+  """
+  Get the index of symetric element in a 1/8 th symmetric core map
+  to a full core map
+  Input: x,y coordinate of the element
+        x0,y0 coordinate of the center element 
+  Output: list of indices [(x,y)]
+  """
+  deltaX = x-x0
+  deltaY = y-y0
+  temparray = [] 
+  temparray.append([x0+deltaX,y0+deltaY])
+  temparray.append([x0+deltaX,y0-deltaY])
+  temparray.append([x0-deltaX,y0+deltaY])
+  temparray.append([x0-deltaX,y0-deltaY])
+  temparray.append([x0+deltaY,y0+deltaX])
+  temparray.append([x0+deltaY,y0-deltaX])
+  temparray.append([x0-deltaY,y0+deltaX])
+  temparray.append([x0-deltaY,y0-deltaX])
+  
+  #remove duplicate
+  outarray = []
+  [outarray.append(i) for i in temparray if i not in outarray]
+  return outarray
+
+def get_index_quater(x,y):
+    """
+    Get the index of symetric element in a 1/8 th symmetric core map
+    to quater core 
+    Input: x,y coordinate of the element
+    Output: list of indices [(x,y)]
+    """
+    temparray = [] 
+    temparray.append([x,y])
+    temparray.append([y,x])
+
+    
+    #remove duplicate
+    outarray = []
+    
+    [outarray.append(i) for i in temparray if i not in outarray]
+
+    return outarray
