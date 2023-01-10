ab84b92525308a148ec03ed39021653e865b9868
Shiming <yin239@purdue.edu>
2023-01-09 22:15:51 -0500

    Delete the class of PCM_snapshot and merge it into previous PCM class. SETH example's RAVEN input file has been modified accordingly, and the timestep included for testing is reduced.

--- ravenframework/Models/PostProcessors/Factory.py
+++ ravenframework/Models/PostProcessors/Factory.py
@@ -39,7 +39,6 @@ from .ValidationBase import ValidationBase
 from .Validations import Probabilistic
 from .Validations import PPDSS
 from .Validations import PhysicsGuidedCoverageMapping
-from .Validations import PhysicsGuidedCoverageMapping_snapshot
 from .TSACharacterizer import TSACharacterizer
 
 ### PostProcessorFunctions (orig: InterfacedPostProcessor)
