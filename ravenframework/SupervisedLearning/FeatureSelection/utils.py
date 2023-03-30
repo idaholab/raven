f7d3c96c753e35f5955e3ca0d497d328fdabac65
Dylan McDowell <dylanjm@users.noreply.github.com>
2023-03-30 13:17:06 -0600

    Remove negation of condtional

--- ravenframework/SupervisedLearning/FeatureSelection/utils.py
+++ ravenframework/SupervisedLearning/FeatureSelection/utils.py
@@ -64,7 +64,7 @@ def screenAndTrainEstimator(Xreduced, yreduced, estimator, support, params, incl
     @ In, addOnKeys, list, optional, list of additional keys to remove
     @ Out, None
   """
-  assert not issubclass(estimator.__class__, SupervisedLearning.SupervisedLearning), \
+  assert issubclass(estimator.__class__, SupervisedLearning.SupervisedLearning), \
     f"estimator class str(estimator.__class__) is not a SupervisedLearning derived class"
   vals = screenInputParams(support, params, includedParams, addOnKeys = addOnKeys)
   if vals:
