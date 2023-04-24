90c5c2959f8ae83ecc96870e129977a2b7a926d8
Shiming <yin239@purdue.edu>
2023-04-24 02:40:47 -0400

    Update PCM by adding the Time dependent PCM (T-Dep PCM) into PhysicsGuidedCoverageMapping.py

--- ravenframework/Models/PostProcessors/Validations/PhysicsGuidedCoverageMapping.py
+++ ravenframework/Models/PostProcessors/Validations/PhysicsGuidedCoverageMapping.py
@@ -27,6 +27,8 @@ import numpy as np
 import xarray as xr
 from scipy import stats
 from sklearn.linear_model import OrthogonalMatchingPursuit
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import StandardScaler
 #External Modules End--------------------------------------------------------------------------------
 
 #Internal Modules------------------------------------------------------------------------------------
@@ -93,7 +95,10 @@ class PhysicsGuidedCoverageMapping(ValidationBase):
     evaluation ={k: np.atleast_1d(val) for k, val in self._evaluate(inputIn['Data'][0][-1].coords.keys(), dataDict, **{'dataobjectNames': names}).items()}
     if 'snapshot_pri_post_stdReduct' in evaluation.keys():
       pivotParameter = self.pivotParameter
-      evaluation[pivotParameter] = inputIn['Data'][0][-1]['time']
+      evaluation[pivotParameter] = inputIn['Data'][0][-1]['timeSnapshot']
+    if 'Tdep_pri_post_stdReduct' in evaluation.keys():
+      pivotParameter = self.pivotParameter
+      evaluation[pivotParameter] = inputIn['Data'][0][-1]['timeTdep']
 
     return evaluation
 
@@ -107,6 +112,188 @@ class PhysicsGuidedCoverageMapping(ValidationBase):
       @ In, kwargs, dict, keyword arguments
       @ Out, outputDict, dict, dictionary containing the results {"pri_post_stdReduct_<targName>":value}
     """
+    """
+      Functions to be applied
+    """
+    #PCM function for snapshot PCM and Static PCM
+    def PCM(featData, msrData, targData):
+      outputArray = []
+      for t in range(targData.shape[1]):
+        # Probability Weights to be used in the future
+        yExp = np.array(featData)
+        yMsr = np.array(msrData)
+        # Reference values of Experiments, yExpRef in M
+        # Sample mean as reference value for simplicity
+        # Can be user-defined in the future
+        yExpRef = np.mean(yExp, axis=0)
+        # Usually the reference value is given,
+        # and will not be zero, e.g. reference fuel temperature.
+        # Standardization
+        yExpStd = (yExp-yExpRef)/yExpRef
+        yMsrStd = (yMsr-yExpRef)/yExpRef
+
+        # For each Target/Application model/response, calculate an uncertainty reduction fraction
+        # using all available Features/Experiments
+
+        # Application responses yApp in Nx1
+        yApp = np.array(targData[:,t])
+        # Reference values of Application, yAppRef is a scalar
+        yAppRef = np.mean(yApp)
+        # Standardization
+        yAppStd = (yApp-yAppRef)/yAppRef
+
+        # Single Experiment response
+        if yExpStd.shape[1]==1:
+          yExpReg = yExpStd.flatten()
+          yMsrReg = yMsrStd.flatten()
+        # Pseudo response of multiple Experiment responses
+        # OrthogonalMatchingPursuit from sklearn used here
+        # Possibly change to other regressors
+        elif yExpStd.shape[1]>1:
+          regrExp = OrthogonalMatchingPursuit(fit_intercept=False).fit(yExpStd, yAppStd)
+          yExpReg = regrExp.predict(yExpStd)
+          # Combine measurements by multiple Experiment regression
+          yMsrReg = regrExp.predict(yMsrStd)
+
+        # Measurement PDF with KDE
+        knlMsr = stats.gaussian_kde(yMsrReg)
+
+        # KDE for joint PDF between Exp and App
+        m1 = yExpReg[:]
+        m2 = yAppStd.flatten()
+        xmin = m1.min()
+        xmax = m1.max()
+        ymin = m2.min()
+        ymax = m2.max()
+        # Grid of Experiment (X), grid of Application (Y)
+        X, Y = np.mgrid[xmin:xmax:self.binKDE, ymin:ymax:self.binKDE]
+        psts = np.vstack([X.ravel(), Y.ravel()])
+        vals = np.vstack([m1, m2])
+        # Measurement PDF over Exp range
+        pdfMsr = knlMsr(X[:, 0])
+
+        # Condition number of matrix of feature and target
+        condNum = np.linalg.cond(vals)
+        # If condition number is greater than 100
+        invErr = 100
+        # Check whether the covavariance matrix is positive definite
+        if condNum>=invErr:
+            # If singular matrix, measurement of Experiment is directly transfered
+            # as predicted Application
+            pdfAppPred = knlMsr(Y[0, :])
+        else:
+            # If not, KDE of Experiment and Application
+            knl = stats.gaussian_kde(vals)
+            # Joint PDF of Experiment and Application
+            Z = np.reshape(knl(psts).T, X.shape)
+            # yAppPred by integrating p(yexp, yapp)p(ymsr) over [yexp.min(), yexp.max()]
+            pdfAppPred = np.dot(Z, pdfMsr.reshape(pdfMsr.shape[0], 1))
+
+        # Normalized PDF of predicted application
+        pdfAppPredNorm = pdfAppPred.flatten()/pdfAppPred.sum()/np.diff(Y[0, :])[0]
+
+        # Calculate Expectation (average value) of predicted application
+        # by integrating xf(x), where f(x) is PDF of x
+        predMean = 0.0
+        for i in range(len(Y[0, :])):
+            predMean += Y[0, i]*pdfAppPredNorm[i]*(Y[0, 1]-Y[0, 0])
+
+        # Calculate Variance of predicted application
+        # by integrating (x-mu_x)^2f(x), where f(x) is PDF of x
+        predVar = 0.0
+        for i in range(len(Y[0, :])):
+            predVar += (Y[0, i]-predMean)**2.0 * pdfAppPredNorm[i]*(Y[0, 1]-Y[0, 0])
+
+        # Predicted standard deviation is square root of variance
+        predStd = np.sqrt(predVar)
+        # Prior standard deviation is the sample standard deviation
+        # Consider probability weights in the future
+        priStd = np.std(yAppStd)
+        # Uncertainty reduction fraction is 1.0-sigma_pred/sigma_pri
+        outputArray.append(1.0-predStd/priStd)
+      return outputArray
+
+    # functions for T-Dep PCM
+    def KNN(exp, app, expM, k=5, Metric='minkowski'):
+      # check the dimension of exp matrix, \
+      # if it only has one feature, enasure to make it as a 2D matrix instead of a 1D vector
+      if expM.ndim==1:
+          expM = expM.reshape(1, expM.shape[0])
+      # Scale exp data with its mean (mu) and standard deviation (std).  [expS = (exp - mu)/std]
+      # Scale expM data with by same mu and std. [expMS = (expM - mu)/std]
+      scaler = StandardScaler()
+      expS = scaler.fit_transform(exp)
+      expMS = scaler.transform(expM)
+      # Fit KNN regression model
+      model = KNeighborsRegressor(n_neighbors=k, metric=Metric)
+      model.fit(expS, app)
+      # Predict on measurement set
+      appPred = model.predict(expMS)
+
+      return appPred
+
+    def FindRank(ref, x, xM):
+      x = (x.T/ref).T
+      error = (np.std(xM, axis=0)/ np.mean(xM, axis=0))[0] #msr error
+      #scaling by reference
+      u, s, v_T = np.linalg.svd(x)
+      # Find the maximum of the residual under different rank
+      maxError = np.zeros(np.mean(xM, axis=0).shape[0])
+      for k in range(1,maxError.shape[0]+1):
+        res = x - u[:,:k]@np.diag(s)[:k,:k]@v_T[:k,:]#residual under different rank
+        maxError[k-1] = np.max(abs(res)) # maximum of the residual
+      xMAX=min(np.argwhere(maxError<error/6)) #rank corresponding to msr error
+
+      return xMAX[0]
+
+    def pcmTdep(featData, msrData, targData):
+      yExp = np.array(featData)
+      yApp = np.array(targData)
+      yExpMsr = np.array(msrData)
+      # Find a reference sample which is the samples' average for scaling later
+      yExpMean = np.mean(yExp, axis=0)
+      yAppMean = np.mean(yApp, axis=0)
+
+      # calculate alphaApp as linear combination of alphaExp (with RankApp, RankExp)
+      yAppRef = yAppMean
+      yAppScaled = (yApp[:,:] - yAppRef).T #scaled application prior data
+      uApp, sApp, vApp_T = np.linalg.svd(yAppScaled, full_matrices=False)
+      alphaApp = (uApp.T@yAppScaled).T # coefficients of application prior data under UApp subspace
+
+
+      yExpRef = yExpMean
+      yExpScaled = (yExp[:,:] - yExpRef).T #scaled experiment prior data
+      uExp, sExp, vExp_T = np.linalg.svd(yExpScaled, full_matrices=False)
+      alphaExp = (uExp.T@yExpScaled).T # coefficients of experiment prior data under UExp subspace
+
+      yExpMsrScaled = ((yExpMsr - yExpRef)).T # scaled experiments' Measurement data
+      alphaExpMsr = (uExp.T@yExpMsrScaled).T #  coefficients of experiments' Measurement data under UExp subspace
+
+
+      #KNN regression
+      rkApp = FindRank(yAppRef, yAppScaled, yExpMsr) #rank of application
+      rkExp = FindRank(yExpRef, yExpScaled, yExpMsr)  # rank of experiments
+      alphaAppHat=np.zeros((yExpMsr.shape[0],rkApp))
+      for i in range(rkApp):
+        alphaAppHat[:,i] = KNN(alphaExp[:,:rkExp], alphaApp[:,i], alphaExpMsr[:,:rkExp],k=5)
+
+      # reconstruct posterior appliaction data by the perdicted application coefficients alphaAppm_hat
+      yAppPredScaled = (uApp[:,:rkApp] @ alphaAppHat.T).T
+      yAppPred = yAppPredScaled + yAppRef
+      predMean = np.mean(yAppPred, axis=0)
+      predStd = np.std(yAppPred, axis=0)/predMean
+      priStd = np.std(yApp, axis=0)/np.mean(yApp, axis=0)
+
+      outputArray=[]
+      for t in range(yApp.shape[1]):
+        outputArray.append(1.0-predStd[t]/priStd[t])
+
+      return outputArray
+
+
+    """
+      Data reading and processing with different version of PCM
+    """
     names = kwargs.get('dataobjectNames')
     outputDict = {}
     # Create empty list for multiple Exp responses
@@ -115,7 +302,6 @@ class PhysicsGuidedCoverageMapping(ValidationBase):
     targData =[]
     featPW = []
     msrPW = []
-    outputArray = []
 
     for feat, msr, targ in zip(self.features, self.measurements, self.targets):
       featDataProb = self._getDataFromDataDict(datasets, feat, names)
@@ -127,132 +313,59 @@ class PhysicsGuidedCoverageMapping(ValidationBase):
       # Data values in <x>Data, <x>=targ, feat, msr
       targData.append(targDataProb[0].flatten())
 
+
     # Probability Weights for future use
     featPW.append(featDataProb[1])
     msrPW.append(msrDataProb[1])
     # Probability Weights values in <x>PW, , <x>=targ, feat, msr
     targPW = targDataProb[1]
 
-    if 'time' in keys:
+    featPW = np.array(featPW).T
+    msrPW = np.array(msrPW).T
+    targPW = np.array(targPW).T
+
+    if 'timeTdep' in keys:
+      pcmVersion = 'Tdep'
+      self.raiseAMessage('***    Running Tdep-PCM       ***')
+      # Data of size (num_of_samples, num_of_features)
+      num_of_featuresExp = np.asarray(datasets['exp'].get('timeTdep')).shape[0]
+      num_of_featuresApp = np.asarray(datasets['app'].get('timeTdep')).shape[0]
+      num_of_samples = np.asarray(datasets['exp'].get('RAVEN_sample_ID')).shape[0]
+      featData = np.array(featData).reshape(num_of_samples, num_of_featuresExp)
+      msrData = np.array(msrData).reshape(num_of_samples, num_of_featuresExp)
+      targData = np.array(targData).reshape(num_of_samples, num_of_featuresApp)
+
+      outputArray = pcmTdep(featData, msrData, targData)
+
+    elif 'timeSnapshot' in keys:
       pcmVersion = 'snapshot'
       self.raiseAMessage('***    Running Snapshot-PCM   ***')
       # Data of size (num_of_samples, num_of_features)
-      num_of_features = np.asarray(datasets['exp'].get('time')).shape[0]
+      num_of_features = np.asarray(datasets['exp'].get('timeSnapshot')).shape[0]
       num_of_samples = np.asarray(datasets['exp'].get('RAVEN_sample_ID')).shape[0]
       featData = np.array(featData).reshape(num_of_samples, num_of_features)
       msrData = np.array(msrData).reshape(num_of_samples, num_of_features)
       targData = np.array(targData).reshape(num_of_samples, num_of_features)
+      outputArray = PCM(featData, msrData, targData)
+
     else:
       pcmVersion = 'static'
       self.raiseAMessage('***     Running Static-PCM    ***')
       featData = np.array(featData).T
       msrData = np.array(msrData).T
       targData = np.array(targData).T
-
-    featPW = np.array(featPW).T
-    msrPW = np.array(msrPW).T
-    targPW = np.array(targPW).T
-
-    for t in range(targData.shape[1]):
-      # Probability Weights to be used in the future
-      yExp = np.array(featData)
-      yMsr = np.array(msrData)
-      # Reference values of Experiments, yExpRef in M
-      # Sample mean as reference value for simplicity
-      # Can be user-defined in the future
-      yExpRef = np.mean(yExp, axis=0)
-      # Usually the reference value is given,
-      # and will not be zero, e.g. reference fuel temperature.
-      # Standardization
-      yExpStd = (yExp-yExpRef)/yExpRef
-      yMsrStd = (yMsr-yExpRef)/yExpRef
-
-      # For each Target/Application model/response, calculate an uncertainty reduction fraction
-      # using all available Features/Experiments
-
-      # Application responses yApp in Nx1
-      yApp = np.array(targData[:,t])
-      # Reference values of Application, yAppRef is a scalar
-      yAppRef = np.mean(yApp)
-      # Standardization
-      yAppStd = (yApp-yAppRef)/yAppRef
-
-      # Single Experiment response
-      if yExpStd.shape[1]==1:
-        yExpReg = yExpStd.flatten()
-        yMsrReg = yMsrStd.flatten()
-      # Pseudo response of multiple Experiment responses
-      # OrthogonalMatchingPursuit from sklearn used here
-      # Possibly change to other regressors
-      elif yExpStd.shape[1]>1:
-        regrExp = OrthogonalMatchingPursuit(fit_intercept=False).fit(yExpStd, yAppStd)
-        yExpReg = regrExp.predict(yExpStd)
-        # Combine measurements by multiple Experiment regression
-        yMsrReg = regrExp.predict(yMsrStd)
-
-      # Measurement PDF with KDE
-      knlMsr = stats.gaussian_kde(yMsrReg)
-
-      # KDE for joint PDF between Exp and App
-      m1 = yExpReg[:]
-      m2 = yAppStd.flatten()
-      xmin = m1.min()
-      xmax = m1.max()
-      ymin = m2.min()
-      ymax = m2.max()
-      # Grid of Experiment (X), grid of Application (Y)
-      X, Y = np.mgrid[xmin:xmax:self.binKDE, ymin:ymax:self.binKDE]
-      psts = np.vstack([X.ravel(), Y.ravel()])
-      vals = np.vstack([m1, m2])
-      # Measurement PDF over Exp range
-      pdfMsr = knlMsr(X[:, 0])
-
-      # Condition number of matrix of feature and target
-      condNum = np.linalg.cond(vals)
-      # If condition number is greater than 100
-      invErr = 100
-      # Check whether the covavariance matrix is positive definite
-      if condNum>=invErr:
-          # If singular matrix, measurement of Experiment is directly transfered
-          # as predicted Application
-          pdfAppPred = knlMsr(Y[0, :])
-      else:
-          # If not, KDE of Experiment and Application
-          knl = stats.gaussian_kde(vals)
-          # Joint PDF of Experiment and Application
-          Z = np.reshape(knl(psts).T, X.shape)
-          # yAppPred by integrating p(yexp, yapp)p(ymsr) over [yexp.min(), yexp.max()]
-          pdfAppPred = np.dot(Z, pdfMsr.reshape(pdfMsr.shape[0], 1))
-
-      # Normalized PDF of predicted application
-      pdfAppPredNorm = pdfAppPred.flatten()/pdfAppPred.sum()/np.diff(Y[0, :])[0]
-
-      # Calculate Expectation (average value) of predicted application
-      # by integrating xf(x), where f(x) is PDF of x
-      predMean = 0.0
-      for i in range(len(Y[0, :])):
-          predMean += Y[0, i]*pdfAppPredNorm[i]*(Y[0, 1]-Y[0, 0])
-
-      # Calculate Variance of predicted application
-      # by integrating (x-mu_x)^2f(x), where f(x) is PDF of x
-      predVar = 0.0
-      for i in range(len(Y[0, :])):
-          predVar += (Y[0, i]-predMean)**2.0 * pdfAppPredNorm[i]*(Y[0, 1]-Y[0, 0])
-
-      # Predicted standard deviation is square root of variance
-      predStd = np.sqrt(predVar)
-      # Prior standard deviation is the sample standard deviation
-      # Consider probability weights in the future
-      priStd = np.std(yAppStd)
-      # Uncertainty reduction fraction is 1.0-sigma_pred/sigma_pri
-      outputArray.append(1.0-predStd/priStd)
+      outputArray = PCM(featData, msrData, targData)
 
     for targ in self.targets:
       if pcmVersion=='snapshot':
         name = "snapshot_pri_post_stdReduct"
       if pcmVersion=='static':
         name = "static_pri_post_stdReduct_" + targ.split('|')[-1]
+      if pcmVersion=='Tdep':
+        name = "Tdep_pri_post_stdReduct"
 
       outputDict[name] = np.asarray(outputArray)
 
     return outputDict
+
+
