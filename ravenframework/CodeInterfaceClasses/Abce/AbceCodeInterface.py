7da2750c7d0564b362485d0babf718274767269c
jia.zhou <48254033+JiaZhou-PU@users.noreply.github.com>
2023-07-11 15:36:05 -0500

    first commit

---
+++ ravenframework/CodeInterfaceClasses/Abce/AbceCodeInterface.py
@@ -0,0 +1,186 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Created on 2020-Sept-2
+
+This is a CodeInterface for the Presient code.
+
+"""
+
+import os
+import re
+import warnings
+
+from ravenframework.CodeInterfaceBaseClass import CodeInterfaceBase
+from ..Generic.GenericCodeInterface import GenericParser
+
+class Abce(CodeInterfaceBase):
+  """
+    This class is used to run the Abce code.
+  """
+
+  def generateCommand(self,inputFiles,executable,clargs=None, fargs=None, preExec=None):    
+    """
+      See base class.  Collects all the clargs and the executable to produce the command-line call.
+      Returns tuple of commands and base file name for run.
+      Commands are a list of tuples, indicating parallel/serial and the execution command to use.
+      @ In, inputFiles, list, List of input files (length of the list depends on the number of inputs have been added in the Step is running this code)
+      @ In, executable, string, executable name with absolute path (e.g. /home/path_to_executable/code.exe)
+      @ In, clargs, dict, optional, dictionary containing the command-line flags the user can specify in the input (e.g. under the node < Code >< clargstype =0 input0arg =0 i0extension =0 .inp0/ >< /Code >)
+      @ In, fargs, dict, optional, a dictionary containing the auxiliary input file variables the user can specify in the input (e.g. under the node < Code >< fileargstype =0 input0arg =0 aux0extension =0 .aux0/ >< /Code >)
+      @ In, preExec, string, optional, a string the command that needs to be pre-executed before the actual command here defined
+      @ Out, returnCommand, tuple, tuple containing the generated command. returnCommand[0] is the command to run the code (string), returnCommand[1] is the name of the output root
+    """
+    if clargs==None:
+      raise IOError('No input file was specified in clargs!')
+    #check for duplicate extension use
+    usedExts = list(ext[0][0] for ext in clargs['input'].values() if len(ext) != 0)
+    if len(usedExts) != len(set(usedExts)):
+      raise IOError('GenericCodeInterface cannot handle multiple input files with the same extension.  You may need to write your own interface.')
+    for inf in inputFiles:
+      ext = '.' + inf.getExt() if inf.getExt() is not None else ''
+      try:
+        usedExts.remove(ext)
+      except ValueError:
+        pass
+    if len(usedExts) != 0:
+      raise IOError('Input extension',','.join(usedExts),'listed in XML node Code, but not found in the list of Input of <Files>')
+    
+    def findSettingIndex(inputFiles,ext):
+      """
+
+      Find the settings file and return its index in the inputFiles list.
+      @ In, inputFiles, list of InputFile objects
+      @ In, ext, string, extension of the settings file
+      """
+      for index,inputFile in enumerate(inputFiles):
+        if inputFile.getBase() == 'settings' and inputFile.getExt() == ext:
+          return index,inputFile
+      raise IOError('No settings file with extension '+ext+' found!')
+    #prepend
+    todo = ''
+    todo += clargs['pre']+' '
+    todo += executable
+    index=None
+    #inputs
+    for flag,elems in clargs['input'].items():
+      if flag == 'noarg':
+        continue
+      todo += ' '+flag
+      for elem in elems:
+        ext, delimiter = elem[0], elem[1]
+        idx,fname = findSettingIndex(inputFiles,ext.strip('.'))
+        todo += delimiter + fname.getFilename()
+        if index == None:
+          index = idx
+    self.caseName = inputFiles[index].getBase()
+    outFile = 'out~'+self.caseName
+    if 'output' in clargs:
+      todo+=' '+clargs['output']+' '+outFile
+    todo+=' '+clargs['text']
+    todo+=' '+clargs['post']
+    returnCommand = [('parallel',todo)],outFile
+    print('Execution Command: '+str(returnCommand[0]))
+    return returnCommand
+    
+  def createNewInput(self,currentInputFiles,origInputFiles,samplerType,**Kwargs):    
+    """
+      This method is used to generate an input based on the information passed in.
+      @ In, currentInputFiles, list,  list of current input files (input files from last this method call)
+      @ In, oriInputFiles, list, list of the original input files
+      @ In, samplerType, string, Sampler type (e.g. MonteCarlo, Adaptive, etc. see manual Samplers section)
+      @ In, Kwargs, dictionary, kwarded dictionary of parameters. In this dictionary there is another dictionary called "SampledVars"
+            where RAVEN stores the variables that got sampled (e.g. Kwargs['SampledVars'] => {'var1':10,'var2':40})
+      @ Out, newInputFiles, list, list of newer input files, list of the new input files (modified and not)
+    """
+    infiles=[]
+    origfiles=[]
+    #FIXME possible danger here from reading binary files
+    for inputFile in currentInputFiles:
+      if inputFile.getExt() in self.getInputExtension():
+        infiles.append(inputFile)
+    for inputFile in origInputFiles:
+      if inputFile.getExt() in self.getInputExtension():
+        origfiles.append(inputFile)
+    parser = GenericParser.GenericParser(infiles)
+    parser.modifyInternalDictionary(**Kwargs)
+    parser.writeNewInput(infiles,origfiles)
+    return currentInputFiles
+
+  def finalizeCodeOutput(self, command, codeLogFile, subDirectory):
+    """
+      Convert csv information to RAVEN's prefered formats
+      Joins together two different csv files and also reorders it a bit.
+      @ In, command, ignored
+      @ In, codeLogFile, ignored
+      @ In, subDirectory, string, the subdirectory where the information is.
+      @ Out, directory, string, the base name of the csv file
+    """
+    # print('command',command)
+    # print('output',output)
+    # print('workingDir',workingDir)
+    # command python  /Users/l00317783/Library/CloudStorage/Box-Box/abce/run.py --settings_file=settings.yml --inputs_path=inputs --verbosity=3
+    # output out~settings
+    # workingDir /Users/l00317783/Library/CloudStorage/Box-Box/abceheron/others/rra/rra/abcesetting/sweep/2
+    #print("finalizeCodeOutput", command, codeLogFile, subDirectory)
+    outDict = {}
+    # toRead = "hourly_summary" #"Daily_summary"
+    # if self._outputDirectory is not None:
+    #   directory = os.path.join(subDirectory, self._outputDirectory)
+    # else:
+    #   directory = subDirectory
+    # readFile = os.path.join(directory, toRead)
+    # if toRead.lower().startswith("hourly"):
+    #   busData, busList, busDataList, hasMinute = self._readBusData(os.path.join(directory, "bus_detail.csv"))
+    #   outDict = {}
+    #   inFile = open(readFile+".csv","r")
+    #   hasNetDemand = False
+    #   firstLine = inFile.readline()
+    #   date, hour, rest = firstLine.split(",", maxsplit=2)
+    #   restSplit = rest.rstrip().split(",")
+    #   hourKey = hour.strip()
+    #   timeKey = date.rstrip()+"_"+hourKey
+    #   outDict[timeKey] = []
+    #   outDict[hour] = []
+    #   otherKeys = restSplit
+    #   for key in otherKeys:
+    #     outDict[key] = []
+    #   if "RenewablesUsed" in outDict and "Demand" in outDict:
+    #     hasNetDemand = True
+    #     outDict["NetDemand"] = []
+    #   first = False
+    #   for bus in busList:
+    #     for dataName in busDataList:
+    #       outDict[bus+"_"+dataName] = []
+
+    #   for line in inFile.readlines():
+    #     date, hour, rest = line.split(",", maxsplit=2)
+    #     restSplit = rest.rstrip().split(",")
+    #     timeValue = date.rstrip()+"_"+hour.lstrip()
+    #     outDict[timeKey].append(timeValue)
+    #     outDict[hourKey].append(float(hour))
+    #     for key,value in zip(otherKeys,restSplit):
+    #       outDict[key].append(float(value))
+    #     if hasNetDemand:
+    #       #Calculate the demand - renewables used to get net demand
+    #       netDemand = outDict["Demand"][-1]  - outDict["RenewablesUsed"][-1]
+    #       outDict["NetDemand"].append(netDemand)
+    #     for bus in busList:
+    #       if hasMinute:
+    #         busTimeKey = (date, hour, '0')
+    #       else:
+    #         busTimeKey = (date, hour)
+    #       for dataName, data in zip(busDataList,busData[busTimeKey][bus]):
+    #         outDict[bus+"_"+dataName].append(float(data) if len(data) > 0 else float("NaN"))
+    return outDict
