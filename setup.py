177725318819492917e2d8e1904f750cc7f2846b
Joshua J. Cogliati <joshua-cogliati-inl@users.noreply.github.com>
2023-11-06 14:44:13 -0700

    Remove using distutils (#2202)
    * Removing use of distutils.
    
    * Simplifying CustomBuild a bit.

--- setup.py
+++ setup.py
@@ -11,11 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from distutils.core import setup, Extension
-from distutils.command.build import build
+import setuptools
+from setuptools import setup, Extension
+from setuptools.command.build_py import build_py
 import os
 import sys
-import setuptools
+
 
 # Replicating the methods used in the RAVEN Makefile to find CROW_DIR,
 # If the Makefile changes to be more robust, so should this
@@ -31,11 +32,10 @@ UTIL_INCLUDE_DIR = os.path.join(CROW_DIR,'include', 'utilities')
 
 # We need a custom build order in order to ensure that amsc.py is available
 # before we try to copy it to the target location
-class CustomBuild(build):
-    sub_commands = [('build_ext', build.has_ext_modules),
-                    ('build_py', build.has_pure_modules),
-                    ('build_clib', build.has_c_libraries),
-                    ('build_scripts', build.has_scripts)]
+class CustomBuild(build_py):
+    def run(self):
+        self.run_command('build_ext')
+        super().run()
 
 
 include_dirs=[RAVEN_INCLUDE_DIR,BOOST_INCLUDE_DIR, DIST_INCLUDE_DIR, UTIL_INCLUDE_DIR]
@@ -87,4 +87,4 @@ setup(name='raven_framework',
                     include_dirs=include_dirs, swig_opts=swig_opts,extra_compile_args=extra_compile_args)],
       py_modules=['AMSC.amsc','crow_modules.distribution1D','crow_modules.randomENG','crow_modules.interpolationND', 'AMSC.AMSC_Object'],
       packages=['ravenframework.'+x for x in setuptools.find_packages('ravenframework')]+['ravenframework'],
-      cmdclass={'build': CustomBuild})
+      cmdclass={'build_py': CustomBuild})
