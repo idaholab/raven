5b5aa328d723e694848ad7278401d00d5565636f
mandd <diego.mandelli@inl.gov>
2023-12-05 11:05:51 -0700

    regold some tests and add more

--- ravenframework/Distributions.py
+++ ravenframework/Distributions.py
@@ -1925,10 +1925,6 @@ class UniformDiscrete(Distribution):
     self.upperBound = inputDict['upperBound']
     self.nPoints    = inputDict['nPoints']
     self.initializeDistribution()
-    '''self.categoricalDist = Categorical()
-    self.categoricalDist.initializeFromDict(inputDict)
-    initialPerm = randomUtils.randomPermutation(inputDict['outcome'].tolist(),self)
-    self.pot = np.asarray(initialPerm)'''
 
   def pdf(self,x):
     """
@@ -1977,24 +1973,16 @@ class UniformDiscrete(Distribution):
       @ In, discardedElems, np array, list of discarded elements
       @ Out, rvsValue, float, the random state
     """
-    if self.nPoints is None:
-      self.xArray   = np.arange(self.lowerBound,self.upperBound+1)
-    else:
-      self.xArray   = np.linspace(self.lowerBound,self.upperBound,self.nPoints)
+    xArray = np.setdiff1d(self.xArray,discardedElems)
+    pdfArray = 1/xArray.size * np.ones(xArray.size)
 
-    self.xArray = np.setdiff1d(self.xArray,discardedElems)
-
-    self.pdfArray = 1/self.xArray.size * np.ones(self.xArray.size)
     paramsDict={}
-    paramsDict['strategy'] = self.strategy
-    paramsDict['lowerBound'] = self.lowerBound
-    paramsDict['upperBound'] = self.upperBound
-    paramsDict['nPoints'] = self.nPoints
-
-    self.tempUniformDiscrete = UniformDiscrete()
-    self.tempUniformDiscrete.initializeFromDict(paramsDict)
-
-    rvsValue = self.tempUniformDiscrete.rvs()
+    paramsDict['outcome'] = xArray
+    paramsDict['state'] = pdfArray
+    self.categoricalDist = Categorical()
+    self.categoricalDist.initializeFromDict(paramsDict)
+    rvsValue = self.categoricalDist.rvs()
+    
     return rvsValue
 
   def reset(self):
