0c0973de126e2c7ce12f639466fecb2533de800f
Gabriel J. Soto <Gabriel.SotoGonzalez@inl.gov>
2024-07-29 10:00:31 -0600

    multiresolution tests

---
+++ tests/framework/ROM/TimeSeries/MultiResolutionTSA/TrainingData/generateSimple.py
@@ -0,0 +1,66 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+  Generates training data in the form of Fourier signals.
+"""
+import numpy as np
+import pandas as pd
+from generators import fourier, arma, toFile
+
+plot = True
+
+pivot = np.arange(100)
+
+amps = [8, 10, 12]
+periods = [12.5, 25, 50]
+phases = [0, np.pi/4, np.pi]
+intercept = 42
+f = fourier(amps, periods, phases, pivot, mean=intercept)
+
+slags = [0.4, 0.2]
+nlags = [0.3, 0.2, 0.1]
+a0, _ = arma(slags, nlags, pivot, plot=False)
+slags = [0.5, 0.3]
+nlags = [0.1, 0.05, 0.01]
+a1, _ = arma(slags, nlags, pivot, plot=False)
+
+s0 = f + a0
+s1 = f + a1
+
+if plot:
+  import matplotlib.pyplot as plt
+  fig, ax = plt.subplots()
+  ax.plot(pivot, s0, '.-', label='0')
+  ax.plot(pivot, s1, '.-', label='1')
+  ax.legend()
+  plt.show()
+
+
+out = np.zeros((len(pivot), 3))
+out[:, 0] = pivot
+out[:, 1] = s0
+out[:, 2] = s1
+# toFile(out, 'simpleMR')
+
+# Write signals to file using pandas DataFrames
+df = pd.DataFrame({'pivot': out[:, 0], 'signal0': out[:, 1], 'signal1': out[:, 2]})
+df.to_csv('simpleMR_A.csv', index=False)
+
+# Create pointer CSV file
+pointer = pd.DataFrame({
+    'scaling': [1],
+    'macro': [1],
+    'filename': ['simpleMR_A.csv']
+})
+pointer.to_csv('simpleMR.csv', index=False)
