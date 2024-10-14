e7ec6b90a70b0bcbb344558e16244c1f8bedf0d1
Jimmy-INL <mohammad.abdo@inl.gov>
2024-10-14 14:11:10 -0600

    addressing comments related to fitness.py

--- ravenframework/Optimizers/fitness/fitness.py
+++ ravenframework/Optimizers/fitness/fitness.py
@@ -33,164 +33,143 @@ import sys
 #               We should make this method flexible to accept different set of params
 
 # @profile
-def invLinear(rlz,**kwargs):
+def invLinear(rlz, **kwargs):
   r"""
-    Inverse linear fitness method requires that the fitness value is inversely proportional to the objective function
-    This method is designed such that:
-    For minimization Problems:
-    1.  As the objective function decreases (comes closer to the min value), the fitness value increases
-    2.  As the objective function increases (away from the min value), the fitness value decreases
-    3.  As the solution violates the constraints the fitness should decrease and hence the solution is less favored by the algorithm.
-
-    For maximization problems the objective value is multiplied by -1 and hence the previous trends are inverted.
-    A great quality of this fitness is that if the objective value is equal for multiple solutions it selects the furthest from constraint violation.
-
-    .. math::
-
-    fitness = -a * obj - b * \Sum_{j=1}^{nConstraint} max(0,-penalty_j)
-    @ In, rlz, xr.Dataset, containing the evaluation of a certain
-              set of individuals (can be the initial population for the very first iteration,
-              or a population of offsprings)
-    @ In, kwargs, dict, dictionary of parameters for this fitness method:
-          objVar, string, the name of the objective variable
-          a, float, linear coefficient for the objective function (default = 1.0)
-          b, float, linear coefficient for the penalty measure. (default = 10.0)
-          ConstraintFunction, xr.DataArray, measuring the severity of the constraint violation. The higher positive value the individual  has,
-          the farthest from violating the constraint it is, The highest negative value it have the largest the violation is.
-    @ Out, fitness, xr.DataArray, the fitness function of the given objective corresponding to a specific chromosome.
+  Inverse linear fitness method where the fitness value is inversely proportional to the objective function.
+  For minimization problems:
+  1. As the objective decreases (closer to min), the fitness value increases.
+  2. As the objective increases (away from the min), the fitness value decreases.
+  3. If the solution violates the constraints, the fitness decreases, making it less favorable.
+  For maximization problems, the objective value is negated, inverting the trends.
+  Formula:
+  fitness = -a * obj - b * \Sum_{j=1}^{nConstraint} max(0, -penalty_j)
+  @ In, rlz, xr.Dataset, containing the evaluation of a set of individuals
+  @ In, kwargs, dict, dictionary of parameters:
+        objVar, list of strings or single string, name(s) of the objective variable(s)
+        a, list of floats, linear coefficient(s) for the objective function (default = 1.0 for each objective)
+        b, list of floats, linear coefficient(s) for the penalty measure (default = 10.0 for each objective)
+        constraintFunction, xr.DataArray, measuring the severity of the constraint violation.
+        type, list of strings, indicating 'min' or 'max' for each objective
+  @ Out, fitnessSet, xr.Dataset, the fitness function for the given population.
   """
-  #NOTE invLinear is not yet support Multi-objective optimization problem solving. Further literature reivew applying invLinear method to multi-objective optimization
-  #     needs to be involved. Potentially, applying obj_Worst in fitness function (i.e., -a[j] * (rlz[objVar][objVar[j]].data).reshape(-1,1) - b[j] * np.sum(np.maximum(0,-penalty),axis=-1).reshape(-1,1))
-  #     should be considerd .
-  a = [1.0] if kwargs['a'] == None else kwargs['a']
-  b = [10.0] if kwargs['b'] == None else kwargs['b']
-  penalty = 0.0 if kwargs['constraintFunction'].all() == None  else kwargs['constraintFunction'].data
-  objVar = [kwargs['objVar']] if isinstance(kwargs['objVar'], str) == True else kwargs['objVar']
-  for j in range(len(objVar)):
-    data = np.atleast_1d(rlz[objVar][objVar[j]].data)
-    fitness = -a[j] * (rlz[objVar][objVar[j]].data).reshape(-1,1) - b[j] * np.sum(np.maximum(0,-penalty),axis=-1).reshape(-1,1)
-    fitness = xr.DataArray(np.squeeze(fitness),
-                           dims=['chromosome'],
-                           coords={'chromosome': np.arange(len(data))})
-    if j == 0:
-        fitnessSet = fitness.to_dataset(name = objVar[j])
-    else:
-        fitnessSet[objVar[j]] = fitness
+  objVar = kwargs['objVar']
+  a = [1.0] * len(objVar) if kwargs.get('a') is None else kwargs['a']  # Scaling factors for objectives
+  b = [10.0] * len(objVar) if kwargs.get('b') is None else kwargs['b']  # Penalty scaling factors
+  g = kwargs['constraintFunction'] if 'constraintFunction' in kwargs else None  # Constraint evaluations
+  fitnessSet = xr.Dataset()
+  for i, obj in enumerate(objVar):
+      data = np.atleast_1d(rlz[obj].data)  # Objective values
+      fitness = np.zeros(data.shape)
+      for ind in range(data.size):
+          # Calculate base fitness: Inversely proportional to the objective value
+          fit = -a[i] * data[ind]
+          # Apply penalties for constraint violations, if any
+          if g is not None and np.any(g.data[ind, :] < 0):  # Violating constraints
+              for constInd in range(g.data.shape[1]):
+                  fit -= b[i] * max(0, -g.data[ind, constInd])  # Apply penalty for violation
+          fitness[ind] = fit
+      # Add the fitness for the current objective to the dataset
+      fitnessSet[obj] = xr.DataArray(fitness, dims=['chromosome'], coords={'chromosome': np.arange(len(data))})
   return fitnessSet
 
-
-def feasibleFirst(rlz,**kwargs):
+def feasibleFirst(rlz, **kwargs):
   r"""
-    Efficient Parameter-less Feasible First Penalty Fitness method
-    This method is designed such that:
-    For minimization Problems:
-    1.  As the objective function decreases (comes closer to the min value), the fitness value increases
-    2.  As the objective function increases (away from the min value), the fitness value decreases
-    3.  As the solution violates the constraints the fitness should decrease and hence the solution is less favored by the algorithm.
-    4.  For the violating solutions, the fitness starts from the worst solution in the population
-        (i.e., max objective in minimization problems and min objective in maximization problems)
+  Efficient Parameter-less Feasible First Penalty Fitness method
+  This method is designed for minimization problems. For maximization, the fitness values are negated.
+  For minimization problems:
+  1.  As the objective decreases, the fitness value increases.
+  2.  As the objective increases, the fitness value decreases.
+  3.  If a solution violates constraints, the fitness decreases, making it less favorable.
+  For maximization problems, the objective value is negated, inverting the trends.
+  Reference: Deb, Kalyanmoy. "An efficient constraint handling method for genetic algorithms."
 
-    For maximization problems the objective value is multiplied by -1 and hence the previous trends are inverted.
-    A great quality of this fitness is that if the objective value is equal for multiple solutions it selects the furthest from constraint violation.
-
-    Reference: Deb, Kalyanmoy. "An efficient constraint handling method for genetic algorithms." Computer methods in applied mechanics and engineering 186.2-4 (2000): 311-338.
-
-    .. math::
-
-    fitness = \[ \\begin{cases}
-                  -obj & g_j(x)\\geq 0 \\forall j \\
-                  -obj_{worst} - \\Sigma_{j=1}^{J}<g_j(x)> & otherwise \\
-                  \\end{cases}
-              \];
-
-    @ In, rlz, xr.Dataset, containing the evaluation of a certain
-              set of individuals (can be the initial population for the very first iteration,
-              or a population of offsprings)
-    @ In, kwargs, dict, dictionary of parameters for this fitness method:
-          objVar, string, the name of the objective variable
-          'constraintFunction', xr.Dataarray, containing all constraint functions (explicit and implicit) evaluations for the whole population
-    @ Out, fitness, xr.DataArray, the fitness function of the given objective corresponding to a specific chromosome.
+  .. math::
+  fitness = \[ \\begin{cases}
+                -obj & g_j(x)\\geq 0 \\forall j \\
+                -obj_{worst} - \\Sigma_{j=1}^{J}<g_j(x)> & otherwise \\
+                \\end{cases}
+            \];
+  @ In, rlz, xr.Dataset, containing the evaluation of a set of individuals
+  @ In, kwargs, dict, dictionary of parameters:
+        objVar, list of strings, the names of objective variables
+        'constraintFunction', xr.DataArray, containing all constraint evaluations for the population
+        'constraintNum', int, number of constraints
+        'a', list of floats, scaling factors for the objectives
+        'b', list of floats, penalty factors for constraint violations
+        'type', list of strings, indicating 'min' or 'max' for each objective
+  @ Out, fitnessSet, xr.Dataset, the fitness function for the given population.
   """
-  objVar = [kwargs['objVar']] if isinstance(kwargs['objVar'], str) == True else kwargs['objVar']
-  a = [1.0]*len(objVar) if kwargs['a'] == None else kwargs['a']
-  if kwargs['constraintNum'] == 0:
-    pen = kwargs['b']
-  else:
-    g = kwargs['constraintFunction']
-    penalty = kwargs['b']
-    pen = [penalty[i:i+len(g['Constraint'].data)] for i in range(0, len(penalty), len(g['Constraint'].data))]
-  objPen = dict(map(lambda i,j : (i,j), objVar, pen))
-
-  for i in range(len(objVar)):
-    data = np.atleast_1d(rlz[objVar][objVar[i]].data)
-    worstObj = max(data)
-    fitness = []
-    for ind in range(data.size):
-      if kwargs['constraintNum'] == 0 or np.all(g.data[ind, :]>=0):
-        fit=(a[i]*data[ind])
-      else:
-        fit = a[i]*worstObj
-        for constInd,_ in enumerate(g['Constraint'].data):
-          fit = a[i]*fit + objPen[objVar[i]][constInd]*(max(0,-1*g.data[ind, constInd])) #NOTE: objPen[objVar[i]][constInd] is "objective & Constraint specific penalty."
-      if len(kwargs['type']) == 1:
-        fitness.append(-1*fit)
-      else:
-        fitness.append(fit)
-
-    fitness = xr.DataArray(np.array(fitness),
-                          dims=['chromosome'],
-                          coords={'chromosome': np.arange(len(data))})
-    if i == 0:
-      fitnessSet = fitness.to_dataset(name = objVar[i])
-    else:
-      fitnessSet[objVar[i]] = fitness
-
+  objVar = kwargs['objVar']
+  a = [1.0] * len(objVar) if kwargs.get('a') is None else kwargs['a']  # Scaling factors for objectives
+  b = [10.0] * len(objVar) if kwargs.get('b') is None else kwargs['b']  # Penalty scaling factors
+  constraintNum = kwargs['constraintNum']
+  g = kwargs['constraintFunction'] if constraintNum > 0 else None  # Constraint evaluations
+  fitnessSet = xr.Dataset()
+  for i, obj in enumerate(objVar):
+      data = np.atleast_1d(rlz[obj].data)
+      worstObj = max(data)  # Worst objective value for penalizing violating solutions
+      fitness = np.zeros(data.shape)
+      for ind in range(data.size):
+          if constraintNum == 0 or np.all(g.data[ind, :] >= 0):  # Feasible solutions
+              fit = a[i] * data[ind]
+          else:  # Penalize constraint violations
+              fit = a[i] * worstObj  # Start with the worst objective value
+              for constInd in range(g.data.shape[1]):
+                  violation = max(0, -g.data[ind, constInd])
+                  fit = b[i] * violation
+          # Adjust fitness for maximization problems
+          if kwargs['type'][i] == 'max':
+              fitness[ind] = -fit
+          else:
+              fitness[ind] = fit
+      # Add the fitness for the current objective to the dataset
+      fitnessSet[obj] = xr.DataArray(fitness, dims=['chromosome'], coords={'chromosome': np.arange(len(data))})
   return fitnessSet
 
-def logistic(rlz,**kwargs):
+def logistic(rlz, **kwargs):
+  r"""
+  Logistic fitness method for multi-objective optimization with constraint handling.
+  For minimization problems:
+  1. As the objective decreases, the fitness value increases.
+  2. As the objective increases, the fitness value decreases.
+  3. If the solution violates the constraints, the fitness decreases, making it less favorable.
+  For maximization problems, the objective value is negated, inverting the trends.
+  math::
+    fitness = \frac{1}{1 + e^{-scale(x - shift)}} - penalty terms for constraint violations.
+  @ In, rlz, xr.Dataset, containing the evaluation of a set of individuals
+  @ In, kwargs, dict, dictionary of parameters:
+        objVar, list of strings or single string, name(s) of the objective variable(s)
+        scale, list of floats, scaling coefficient(s) for the objective function (default = 1.0 for each objective)
+        shift, list of floats, coefficient(s) for shifting the objective value (default = 0.0 for each objective)
+        constraintFunction, xr.DataArray, measuring the severity of the constraint violation
+        penalty, list of floats, penalties for constraint violations (default = 1.0 for each objective)
+        type, list of strings, indicating 'min' or 'max' for each objective
+  @ Out, fitnessSet, xr.Dataset, the fitness function for the given population.
   """
-    Logistic fitness method
-    .. math::
-
-    fitness = \frac{1}{1+e^{-a(x-b)}}
-
-    @ In, rlz, xr.Dataset, containing the evaluation of a certain
-              set of individuals (can be the initial population for the very first iteration,
-              or a population of offsprings)
-    @ In, kwargs, dict, dictionary of parameters for this fitness method:
-          objVar, string, the name of the objective variable
-          a, float, linear coefficient for the objective function (default = 1.0)
-          penalty, float, measuring the severity of the constraint violation. (default = 1.0)
-          b, float, linear coefficient for the penalty measure. (default = 1.0)
-    @ Out, fitness, xr.DataArray, the fitness function of the given objective corresponding to a specific chromosome.
-  """
-  if kwargs['a'] == None:
-    a = [1.0]
-  else:
-    a = kwargs['a']
-  if kwargs['b'] == None:
-    b = [0.0]
-  else:
-    b = kwargs['b']
-  if isinstance(kwargs['objVar'], str) == True:
-    objVar = [kwargs['objVar']]
-  else:
-    objVar = kwargs['objVar']
-  for i in range(len(objVar)):
-    val = rlz[objVar][objVar[i]].data
-    data = np.atleast_1d(rlz[objVar][objVar[i]].data)
-    denom = 1.0 + np.exp(-a[0] * (val - b[0]))
-    fitness = 1.0 / denom
-    fitness = xr.DataArray(fitness.data,
-                           dims=['chromosome'],
-                           coords={'chromosome': np.arange(len(data))})
-    if i == 0:
-      fitnessSet = fitness.to_dataset(name = objVar[i])
-    else:
-      fitnessSet[objVar[i]] = fitness
-
+  objVar = kwargs['objVar']
+  scale = kwargs.get('scale', [1.0] * len(objVar))  # Scaling factors for objectives
+  shift = kwargs.get('shift', [0.0] * len(objVar))  # Shifting value for each objective
+  penalty = kwargs.get('penalty', [1.0] * len(objVar))  # Penalty for constraint violations
+  g = kwargs.get('constraintFunction', None)  # Constraint evaluations (if any)
+  fitnessSet = xr.Dataset()
+  for i, obj in enumerate(objVar):
+      data = np.atleast_1d(rlz[obj].data)  # Objective values
+      fitness = np.zeros(data.shape)
+      for ind in range(data.size):
+          # Base logistic fitness calculation
+          denom = 1.0 + np.exp(-scale[i] * (data[ind] - shift[i]))
+          fit = 1.0 / denom
+          # Apply penalties for constraint violations, if any
+          if g is not None and np.any(g.data[ind, :] < 0):  # Constraint violation
+              for constInd in range(g.data.shape[1]):
+                  fit -= penalty[i] * max(0, -g.data[ind, constInd])
+          # Adjust for maximization problems by negating the fitness value
+          if kwargs['type'][i] == 'max':
+              fit = 1.0 - fit  # Adjust the logistic fitness for maximization
+          fitness[ind] = fit
+      # Store fitness in the dataset
+      fitnessSet[obj] = xr.DataArray(fitness, dims=['chromosome'], coords={'chromosome': np.arange(len(data))})
   return fitnessSet
-
 
 __fitness = {}
 __fitness['invLinear'] = invLinear
