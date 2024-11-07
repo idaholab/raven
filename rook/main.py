5fcfa6c526f6c763a66d116a0988c10522530131
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2024-11-07 16:54:10 -0700

    Adding --longest-jobs for compatibility.

--- rook/main.py
+++ rook/main.py
@@ -68,6 +68,8 @@ class NoColors:
 parser = argparse.ArgumentParser(description="Test Runner")
 parser.add_argument('-j', '--jobs', dest='number_jobs', type=int, default=1,
                     help='Specifies number of tests to run simultaneously (default: 1)')
+parser.add_argument('--longest-jobs',dest='longest_jobs', type=int, default=0,
+                    help='Only for compatibility')
 parser.add_argument('--re', dest='test_re_raw', default='.*',
                     help='Only tests with this regular expression inside will be run')
 parser.add_argument('-l', dest='load_average', type=float, default=-1.0,
