03d84e34830edecd2f4a5b6255d67888e6904a88
Congjian Wang <congjian.wang@inl.gov>
2024-11-26 10:11:35 -0700

    add partially mapped crossover

--- ravenframework/Optimizers/crossOverOperators/crossovers.py
+++ ravenframework/Optimizers/crossOverOperators/crossovers.py
@@ -15,9 +15,13 @@
   Implementation of crossovers for crossover process of Genetic Algorithm
   currently the implemented crossover algorithms are:
   1.  OnePoint Crossover
+  2.  TwoPoints Crossover
+  3.  Uniform Crossover
+  4.  TwoPoints Partially Mapped Crossover (PMX)
 
   Created June,16,2020
-  @authors: Mohammad Abdo, Diego Mandelli, Andrea Alfonsi
+  Last update July,8,2024
+  @authors: Mohammad Abdo, Diego Mandelli, Andrea Alfonsi, Juan Luque-Gutierrez
 """
 
 import numpy as np
@@ -151,11 +155,57 @@ def twoPointsCrossover(parents, **kwargs):
 
   return children
 
+def partiallyMappedCrossover(parents, **kwargs):
+  """
+    Method designed to perform a two point partially mapped crossover (MPX) on 2 parents:
+    Partition each parents in three sequences (A,B,C):
+    parent1 = A1 B1 C1
+    parent2 = A2 B2 C2
+    Then:
+    children1 = A1* B2 C1*
+    children2 = A2* B1 C2*
+    Children should have the same elements as their parents, but in different order.
+    This crossover preserves the genes in a chromosome.
+    @ In, parents, xr.DataArray, parents involved in the mating process
+    @ In, kwargs, dict, dictionary of parameters for this mutation method:
+          parents, 2D array, parents in the current mating process.
+          Shape is nParents x len(chromosome) i.e, number of Genes/Vars
+          crossoverProb, float, crossoverProb determines when child takes genes from a specific parent, default is random
+          points, integer, point at which the cross over happens, default is random
+    @ Out, children, xr.DataArray, children resulting from the crossover. Shape is nParents x len(chromosome) i.e, number of Genes/Vars
+  """
+  nParents, nGenes = np.shape(parents)
+  children = xr.DataArray(np.zeros((int(2*comb(nParents,2)), np.shape(parents)[1])),
+                          dims = ['chromosome', 'Gene'],
+                          coords = {'chromosome': np.arange(int(2*comb(nParents, 2))),
+                                    'Gene':parents.coords['Gene'].values})
+  parentPairs = list(combinations(parents, 2))
+  index = 0
+  if nGenes <= 2:
+    ValueError('The number of genes should be >= 3')
+  for couples in parentPairs:
+    [loc1, loc2] = randomUtils.randomChoice(list(range(1, nGenes)), size = 2, replace=False, engine=None)
+    if loc1 > loc2:
+      locL = loc2
+      locU = loc1
+    else:
+      locL = loc1
+      locU = loc2
+    parent1 = couples[0]
+    parent2 = couples[1]
+    children1, children2 = twoPointsPMXMethod(parent1, parent2, locL, locU)
+
+    children[index] = children1
+    children[index + 1] = children2
+    index = index + 2
+
+  return children
+
 __crossovers = {}
 __crossovers['onePointCrossover']  = onePointCrossover
 __crossovers['twoPointsCrossover'] = twoPointsCrossover
 __crossovers['uniformCrossover']   = uniformCrossover
-
+__crossovers['partiallyMappedCrossover'] = partiallyMappedCrossover 
 
 def returnInstance(cls, name):
   """
@@ -215,3 +265,51 @@ def uniformCrossoverMethod(parent1,parent2,crossoverProb):
       children2[pos] = parent2[pos]
 
   return children1,children2
+
+def twoPointsPMXMethod(parent1, parent2, locL, locU):
+  """
+    Method designed to perform a two point Partially Mapped Crossover (PMX) on 2 arrays:
+    Partition each array into three sequences (A, B, C):
+    parent1 = A1 B1 C1
+    parent2 = A2 B2 C2
+    We map the values contained in B1 to B2.
+    Then:
+    children1 = X B2 X
+    children2 = X B1 X
+    We verify if the values in A and C are found in B for each children. If so, we 
+    replace such values for the ones in the map.
+    children1 = A1* B2 C1*
+    children2 = A2* B1 C2*
+    Children should have the same elements as their parents, but in different order. 
+    @ In, parent1, first array
+    @ In, parent2, second array
+    @ In, locL: first location
+    @ In, LocU: second location
+    @ Out, children1: first generated array
+    @ Out, children2: second generated array
+  """
+  
+  size = len(parent1)
+  
+  children1 = parent1.copy(deep=True)
+  children2 = parent2.copy(deep=True)
+
+  seqB1 = parent1.values[locL:locU]
+  seqB2 = parent2.values[locL:locU]
+
+  children1[locL:locU] = seqB2
+  children2[locL:locU] = seqB1
+
+  # Determine mapping relationship
+  mapping1 = {parent2.values[i]: parent1.values[i] for i in range(locL, locU)}
+  mapping2 = {parent1.values[i]: parent2.values[i] for i in range(locL, locU)}
+
+  for i in list(range(locL)) + list(range(locU, size)):
+    if children1.values[i] in mapping1:
+      while children1.values[i] in mapping1:
+        children1.values[i] = mapping1[children1.values[i]]
+    if children2.values[i] in mapping2:
+      while children2.values[i] in mapping2:
+        children2.values[i] = mapping2[children2.values[i]]
+
+  return children1, children2
