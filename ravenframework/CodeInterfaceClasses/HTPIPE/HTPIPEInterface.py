5f121be884509bac2d5c8771a4b7779c54454d78
Andrea Alfonsi - NuCube <aalfonsi@nucube.energy>
2025-03-28 10:47:46 -0600

    Update ravenframework/CodeInterfaceClasses/HTPIPE/HTPIPEInterface.py

--- ravenframework/CodeInterfaceClasses/HTPIPE/HTPIPEInterface.py
+++ ravenframework/CodeInterfaceClasses/HTPIPE/HTPIPEInterface.py
@@ -1,4 +1,4 @@
-# Copyright 2017 University of Rome La Sapienza and Battelle Energy Alliance, LLC
+# Copyright 2025 NuCube Energy and Battelle Energy Alliance, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
